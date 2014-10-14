2e6d11d4ba895c36a8159ae57c506fc6177e7178
Pedro M. Baeza <pedro.baeza@gmail.com>
2014-10-14 08:16:11 +0200

    [IMP] quality_control: Some improvement

--- quality_control/models/quality_control.py
+++ quality_control/models/quality_control.py
@@ -44,7 +44,7 @@ class QcProofMethod(orm.Model):
     _columns = {
         'name': fields.char('Name', size=100, required=True, select="1",
                             translate=True),
-        'active': fields.boolean('Active', select="1"),
+        'active': fields.boolean('Active'),
     }
 
     _defaults = {
@@ -61,11 +61,10 @@ class QcPosibleValue(orm.Model):
     _columns = {
         'name': fields.char('Name', size=200, required=True, select="1",
                             translate=True),
-        'active': fields.boolean('Active', select="1"),
+        'active': fields.boolean('Active'),
         'ok': fields.boolean('Correct answer',
-                             help="When this field is True, the answer\n"
-                                  " is correct, When is False the answer\n"
-                                  " is not correct."),
+                             help="When this field is marked, the answer "
+                                  "is considered correct."),
     }
 
     _defaults = {
@@ -114,7 +113,7 @@ class QcProof(orm.Model):
         'name': fields.char('Name', size=200, required=True, select="1",
                             translate=True),
         'ref': fields.char('Code', size=30, select="1"),
-        'active': fields.boolean('Active', select="1"),
+        'active': fields.boolean('Active'),
         'synonym_ids': fields.one2many('qc.proof.synonym', 'proof_id',
                                        'Synonyms'),
         'type': fields.selection([('qualitative', 'Qualitative'),
@@ -128,7 +127,7 @@ class QcProof(orm.Model):
     }
 
     _defaults = {
-        'active': lambda *a: True,
+        'active': True,
     }
 
     _sql_constraints = [
