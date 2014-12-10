3971774967e320ec11fc66d8dba001122dd2c071
Pedro M. Baeza <pedro.baeza@gmail.com>
2014-12-10 23:35:21 +0100

    [IMP] Total refactorization of quality control modules with new API, README files, and new concepts. WIP of quality_control_tolerance
    Merge branch '8.0' of github.com:odoomrp/odoomrp-wip into 8.0
    
    [FIX] quality_control_stock:
    * File missing
    * Triggers for product category

---
+++ quality_control_stock_oca/README.rst
@@ -0,0 +1,10 @@
+Stock extension for quality control
+===================================
+
+This module defines triggers that creates inspections when stock moves are done.
+
+It also adds some shortcuts on picking and lots to these inspections.
+
+Contributors
+------------
+* Pedro M. Baeza <pedro.baeza@serviciobaeza.com>
---
+++ quality_control_stock_oca/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+
+from . import qc_trigger
+from . import qc_inspection
+from . import stock_picking_type
+from . import stock_picking
+from . import stock_production_lot
