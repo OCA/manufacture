a96c85dae79ec7efd4282cbf5f7fa9d7b58c1ac2
Pedro M. Baeza <pedro.baeza@gmail.com>
2014-12-10 23:35:21 +0100

    [IMP] Total refactorization of quality control modules with new API, README files, and new concepts. WIP of quality_control_tolerance
    Merge branch '8.0' of github.com:odoomrp/odoomrp-wip into 8.0
    
    [FIX] quality_control_stock:
    * File missing
    * Triggers for product category

---
+++ quality_control_stock_oca/models/qc_trigger.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import models, fields
+
+
+class QcTrigger(models.Model):
+    _inherit = 'qc.trigger'
+
+    picking_type = fields.Many2one(
+        comodel_name="stock.picking.type", readonly=True)
