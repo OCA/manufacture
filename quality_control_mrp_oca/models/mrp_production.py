26d7f85bd5733187c8008d7d999abdd4f426e899
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-01-05 05:18:56 +0100

    [FIX] quality_control_mrp: Some problems [IMP] quality_control_mrp: Smartbutton to inspections

--- quality_control_mrp_oca/models/mrp_production.py
+++ quality_control_mrp_oca/models/mrp_production.py
@@ -9,27 +9,15 @@ class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
     @api.one
-    @api.depends('qc_inspections', 'qc_inspections.state')
+    @api.depends('qc_inspections')
     def _count_inspections(self):
         self.created_inspections = len(self.qc_inspections)
-        self.passed_inspections = len([x for x in self.qc_inspections if
-                                       x.state == 'success'])
-        self.failed_inspections = len([x for x in self.qc_inspections if
-                                       x.state == 'failed'])
-        self.done_inspections = (self.passed_inspections +
-                                 self.failed_inspections)
 
     qc_inspections = fields.One2many(
-        comodel_name='qc.inspection', inverse_name='picking', copy=False,
+        comodel_name='qc.inspection', inverse_name='production', copy=False,
         string='Inspections', help="Inspections related to this production.")
     created_inspections = fields.Integer(
         compute="_count_inspections", string="Created inspections")
-    done_inspections = fields.Integer(
-        compute="_count_inspections", string="Done inspections")
-    passed_inspections = fields.Integer(
-        compute="_count_inspections", string="Inspections OK")
-    failed_inspections = fields.Integer(
-        compute="_count_inspections", string="Inspections failed")
 
     @api.v7
     def action_produce(self, cr, uid, production_id, production_qty,
