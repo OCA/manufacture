3dd10b98b68f562e1351a0ed97eced3fda6a6f1b
Sébastien BEAU <sebastien.beau@akretion.com>
2015-03-22 12:22:53 +0100

    [IMP] update description using OCA template

--- mrp_production_autocomplete/__openerp__.py
+++ mrp_production_autocomplete/__openerp__.py
@@ -23,7 +23,7 @@
 {
     'name': 'Production AutoComplete',
     'version': '0.1',
-    'author': 'Akretion',
+    'author': 'Akretion,Odoo Community Association (OCA)',
     'maintener': 'Akretion',
     'category': 'Manufaturing',
     'summary': "Set as Done Manufacturing Orders with cron",
@@ -33,19 +33,50 @@
     'description': """
 Production AutoComplete
 ==========================
+
 This module will set in done all manufacturing Order for the product
 which have the option "Complete Manuf. Order" tic.
 The validation is done by a scheduled action.
 
-A case of use is when for example you have an external system to manage the
-production, and this system can not trigger when the manufacturing order have
-been done. In this case you can automatically set the production order in done
-and then you can still use odoo for computing the consomation of goods and for
-processing the move for the picking.
+A case of use is when you have an external system to manage the production,
+but this system can not trigger when the manufacturing order have been done.
+So as you still want to manage the consumation of goods and the processing of
+move for the picking, You can automatically set the production order in done
+with a schedule action.
+
+
+Configuration
+=============
+
+To use this module, you need to go to:
+* Sale > Product
+    - You can tic the box "Complete Manuf. Order" to select autocomplete product
+* Setting > Scheduler > Scheduled Actions
+    - You can confirgure the cron "Manuf. Order Auto Complete"
+
+Usage
+=====
+
+Nothing to do manufacturing order will be set as done automatically
+
+
+Credits
+=======
 
 Contributors
 ------------
 * David BEAL <david.beal@akretion.com>
+
+Maintainer
+----------
+.. image:: http://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+      :target: http://odoo-community.org
+      This module is maintained by the OCA.
+      OCA, or the Odoo Community Association, is a nonprofit organization
+      whose mission is to support the collaborative development of Odoo features
+      and promote its widespread use.
+      To contribute to this module, please visit http://odoo-community.org.
 
 """,
     'website': 'http://www.akretion.com/',
