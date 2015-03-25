45447c02e053f23cc8bbd32b301c9cdda5bf4c9e
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-03-25 21:35:06 +0100

    [IMP] Expand AUTHORS

--- mrp_bom_note/__openerp__.py
+++ mrp_bom_note/__openerp__.py
@@ -19,7 +19,9 @@
 {
     "name": "MRP BoM Notes",
     "version": "1.0",
-    "author": "OdooMRP team",
+    "author": "OdooMRP team,"
+              "AvanzOSC,"
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza",
     "website": "http://www.odoomrp.com",
     "contributors": [
         "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
