087d9e1bd9bef83ba7e0def32f1b7f696400b986
oihane <oihanecruce@gmail.com>
2015-04-01 18:19:08 +0200

    [MOD] Smartbuttons all in one div

--- quality_control_stock_oca/models/qc_trigger.py
+++ quality_control_stock_oca/models/qc_trigger.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
+# (c) 2014 Serv. Tec. Avanzados - Pedro M. Baeza
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
 from openerp import models, fields
 
 
@@ -9,4 +9,4 @@ class QcTrigger(models.Model):
     _inherit = 'qc.trigger'
 
     picking_type = fields.Many2one(
-        comodel_name="stock.picking.type", readonly=True)
+        comodel_name="stock.picking.type", readonly=True, ondelete="cascade")
--- quality_control_stock_oca/models/stock_picking_type.py
+++ quality_control_stock_oca/models/stock_picking_type.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
+# (c) 2014 Serv. Tec. Avanzados - Pedro M. Baeza
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
 from openerp import models, api
 
 
@@ -11,10 +11,10 @@ class StockPickingType(models.Model):
     @api.multi
     def _create_qc_trigger(self):
         qc_trigger = {
-            # Synchronize all translations
             'name': self.complete_name,
             'company_id': self.warehouse_id.company_id.id,
             'picking_type': self.id,
+            'partner_selectable': True,
         }
         return self.env['qc.trigger'].sudo().create(qc_trigger)
 
@@ -33,9 +33,3 @@ class StockPickingType(models.Model):
                 [('picking_type', '=', self.id)])
             qc_trigger.name = self.complete_name
         return res
-
-    @api.multi
-    def unlink(self):
-        self.env['qc.trigger'].sudo().search(
-            [('picking_type', '=', self.id)]).unlink()
-        return super(StockPickingType, self).unlink()
