562897f866a2d9d1e090bb64b34e7bfa9278b63a
oihane <oihanecruce@gmail.com>
2015-04-01 18:19:08 +0200

    [MOD] Smartbuttons all in one div

--- quality_control_stock_oca/__openerp__.py
+++ quality_control_stock_oca/__openerp__.py
@@ -1,45 +1,31 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-# Copyright (c)
-#    2014 Serv. Tec. Avanzados - Pedro M. Baeza (http://www.serviciosbaeza.com)
-#    2014 AvanzOsc (http://www.avanzosc.es)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# (c) 2014 Serv. Tec. Avanzados - Pedro M. Baeza
+# (c) 2014 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     "name": "Quality control - Stock",
-    "version": "1.0",
-    "author": "OdooMRP team,"
-              "AvanzOSC,"
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza",
+    "version": "8.0.1.0.0",
+    "category": "Quality control",
+    "license": "AGPL-3",
+    "author": "OdooMRP team, "
+              "AvanzOSC, "
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+              "Odoo Community Association (OCA)",
     "website": "http://www.odoomrp.com",
     "contributors": [
-        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com",
+        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
+        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
     ],
-    "category": "Quality control",
     "depends": [
-        'quality_control',
-        'stock',
+        "quality_control",
+        "stock",
     ],
     "data": [
-        'data/quality_control_stock_data.xml',
-        'views/qc_inspection_view.xml',
-        'views/stock_picking_view.xml',
-        'views/stock_production_lot_view.xml',
+        "data/quality_control_stock_data.xml",
+        "views/qc_inspection_view.xml",
+        "views/stock_picking_view.xml",
+        "views/stock_production_lot_view.xml",
     ],
     "post_init_hook": "post_init_hook",
     "installable": True,
