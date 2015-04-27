e14103e76d030d707b74a3ff70a156ec0fd28b2c
oihane <oihanecruce@gmail.com>
2015-04-27 09:33:10 +0200

    [MOD] Changed email

--- mrp_bom_note/__openerp__.py
+++ mrp_bom_note/__openerp__.py
@@ -26,7 +26,7 @@
     "contributors": [
         "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
         "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
-        "Ana Juaristi <ajuaristio@gmail.com>"
+        "Ana Juaristi <anajuaristi@avanzosc.es>"
     ],
     "category": "Tools",
     "depends": [
