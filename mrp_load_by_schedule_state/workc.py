4167c8b09e289a9c46ff779f56b815b097c886c8
Sébastien BEAU <sebastien.beau@akretion.com>
2015-05-05 12:15:13 +0200

    [REF] start to refactor ordering management

--- mrp_load_by_schedule_state/workc.py
+++ mrp_load_by_schedule_state/workc.py
@@ -20,26 +20,37 @@
 ##############################################################################
 
 from openerp.osv import orm, fields
-
+from openerp.addons.mrp_workcenter_workorder_link.mrp import STATIC_STATES
 
 COMPLEX_WORK_ORDER_FIELDS = [
     'message_follower_ids',
     'message_is_follower',
     'message_unread',
-    'criterious_output',
 ]
 
 
-class MrpWorkcenterOrdering(orm.Model):
-    _name = 'mrp.workcenter.ordering'
-    _description = "Workcenter Ordering"
-    _order = 'sequence'
+class MrpWorkcenterOrderingKey(orm.Model):
+    _name = 'mrp.workcenter.ordering.key'
+    _description = "Workcenter Ordering Key"
+
+    _columns = {
+        'name': fields.char('Name'),
+        'field_ids': fields.one2many(
+            'mrp.workcenter.ordering.field',
+            'ordering_key_id',
+            'Fields')
+    }
+
+class MrpWorkcenterOrderingField(orm.Model):
+    _name = 'mrp.workcenter.ordering.field'
+    _description = "Workcenter Ordering Field"
+    _order = 'sequence ASC'
 
     _columns = {
         'sequence': fields.integer(
             'Sequence'),
-        'workcenter_id': fields.many2one(
-            'mrp.workcenter',
+        'ordering_key_id': fields.many2one(
+            'mrp.workcenter.ordering.key',
             'Workcenter'),
         'field_id': fields.many2one(
             'ir.model.fields',
@@ -50,7 +61,8 @@ class MrpWorkcenterOrdering(orm.Model):
         'ttype': fields.related(
             'field_id', 'ttype',
             type='char',
-            string='Type'),
+            string='Type',
+            readonly=True),
         'order': fields.selection(
             [('asc', 'Asc'), ('desc', 'Desc')],
             string='Order'),
@@ -61,17 +73,47 @@ class MrpWorkcenterOrdering(orm.Model):
     }
 
 
+class MrpRoutingWorkcenter(orm.Model):
+    _inherit = 'mrp.routing.workcenter'
+
+    _columns = {
+        'priority': fields.integer('Priority'),
+    }
+
+
+class MrpBom(orm.Model):
+    _inherit = 'mrp.bom'
+
+    def _prepare_production_line(self, wc_use, bom, factor, level):
+        res = super(MrpBom, self)._prepare_production_line(
+            wc_use, bom, factor, level)
+        res['priority'] = wc_use.priority
+        return res
+
+
+class MrpProductionWorkcenterLine(orm.Model):
+    _inherit = 'mrp.production.workcenter.line'
+    _order = 'date_planned, sequence'
+
+    _columns = {
+        'priority': fields.integer('Priority'),
+    }
+
+
 class MrpWorkcenter(orm.Model):
     _inherit = 'mrp.workcenter'
 
     _columns = {
-        'unable_load': fields.float('Unable'),
-        'todo_load': fields.float('Todo'),
-        'scheduled_load': fields.float('Scheduled'),
-        'ordering_field_ids': fields.one2many(
-            'mrp.workcenter.ordering',
+        'workcenter_line_ids': fields.one2many(
+            'mrp.routing.workcenter',
             'workcenter_id',
-            string='Ordering fields',
+            'Work Centers'),
+        'waiting_load': fields.float('Waiting (h)'),
+        'todo_load': fields.float('Todo (h)'),
+        'scheduled_load': fields.float('Scheduled (h)'),
+        'ordering_key_id': fields.many2one(
+            'mrp.workcenter.ordering.key',
+            string='Ordering Key',
             help="Allow to define Work Orders ordering priority"),
     }
 
@@ -111,4 +153,21 @@ class MrpWorkcenter(orm.Model):
             if len(col) > 3 and col[-4:] == 'load':
                 vals.update({col: 0})
         MrpWorkCter_m.write(cr, uid, workcenter_ids, vals, context=context)
+        return True
+
+    def button_order_workorders_in_workcenter(
+            self, cr, uid, ids, context=None):
+        for elm in self.browse(cr, uid, ids, context=context):
+            ProdLine_m = self.pool['mrp.production.workcenter.line']
+            order_by = ['%s %s' % (row.field_id.name, row.order)
+                        for row in elm.ordering_field_ids]
+            prod_line_ids = ProdLine_m.search(cr, uid, [
+                ('state', 'not in', STATIC_STATES),
+                ('workcenter_id', '=', elm.id), ],
+                order=', '.join(order_by), context=context)
+            count = 1
+            for prod_line_id in prod_line_ids:
+                vals = {'sequence': count}
+                count += 1
+                ProdLine_m.write(cr, uid, prod_line_id, vals, context=context)
         return True
