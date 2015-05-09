68de8f0756a9152ebd18c990f093df36755cf73d
Sébastien BEAU <sebastien.beau@akretion.com>
2015-05-09 17:43:19 +0200

    [IMP] add filter on domain for workcenter, forbid to change the workcenter of a done operation, forbid to change from a different workcenter group

--- mrp_load/transient/mrp.py
+++ mrp_load/transient/mrp.py
@@ -20,6 +20,8 @@
 ##############################################################################
 
 from openerp.osv import orm, fields
+from lxml import etree
+from openerp.tools.translate import _
 
 
 class SwitchWorkcenter(orm.TransientModel):
@@ -39,3 +41,37 @@ class SwitchWorkcenter(orm.TransientModel):
         vals = {'workcenter_id': switch_workc.workcenter_id.id}
         MrpProdWorkcLine_m.write(cr, uid, active_ids, vals, context=context)
         return True
+
+    def fields_view_get(self, cr, uid, view_id=None, view_type='form',
+                        context=None, toolbar=False, submenu=False):
+        res = super(SwitchWorkcenter, self).fields_view_get(
+            cr, uid, view_id=view_id, view_type=view_type, context=context,
+            toolbar=toolbar, submenu=submenu)
+        if context is None:
+            context = {}
+        if context.get('active_ids') and view_type == 'form':
+            line_obj = self.pool['mrp.production.workcenter.line']
+            group_id = False
+            for line in line_obj.browse(cr, uid, context['active_ids'],
+                                        context=context):
+                if line.state == 'done':
+                    raise orm.except_orm(
+                        _('User Error'),
+                        _('You can not change the workcenter of a done '
+                          'operation'))
+                elif not group_id:
+                    group_id = line.workcenter_id.workcenter_group_id.id
+                elif group_id != line.workcenter_id.workcenter_group_id.id:
+                    raise orm.except_orm(
+                        _('User Error'),
+                        _('You can only swith workcenter of the same group'))
+            root = etree.fromstring(res['arch'])
+            field = root.find(".//field[@name='workcenter_id']")
+            field.set(
+                'domain',
+                "[('workcenter_group_id', '=', %s)]"\
+                % group_id
+                )
+            orm.setup_modifiers(field, root)
+            res['arch'] = etree.tostring(root, pretty_print=True)
+        return res
