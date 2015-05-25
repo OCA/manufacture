9139f362e45e79ec8813312bd0fb63eb41f4e4ba
InakiZabala <zabalainaki@gmail.com>
2015-05-25 10:57:20 +0200

    [IMP] mrp_bom_version: renamed module mrp_bom_state to mrp_bom_version
    [IMP] mrp_bom_version: fix flake8
    
    [IMP]mrp_bom_version: fix travis

---
+++ models/res_config.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import models, fields
+
+
+class MrpConfigSettings(models.TransientModel):
+    _inherit = 'mrp.config.settings'
+
+    group_mrp_bom_state = fields.Boolean(
+        string='Allow to re-edit BoMs',
+        implied_group='mrp_bom_version.group_mrp_bom_version',
+        help='The active state may be passed back to state draft')
