25041631f46595d8d9fb44852c9398b1b1cdd99a
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2015-09-10 12:11:35 +0200

    [IMP] mrp_production_project_estimated_cost: Show "Estimated Average Cost" field in mrp.production tree view. [IMP] mrp_production_real_cost: Show "Total Real Cost" field in mrp.production tree view.

--- mrp_production_project_estimated_cost/models/mrp_production.py
+++ mrp_production_project_estimated_cost/models/mrp_production.py
@@ -151,7 +151,7 @@ class MrpProduction(models.Model):
     @api.model
     def _prepare_cost_analytic_line(
             self, journal, name, production, product, general_account=None,
-            workorder=None, qty=1, std_cost=0, avg_cost=0):
+            workorder=None, qty=1, std_cost=0, avg_cost=0, amount=0):
         """
         Prepare the vals for creating an analytic entry for stimated cost
         :param journal: Journal of the entry
@@ -190,7 +190,7 @@ class MrpProduction(models.Model):
             'date': analytic_line_obj._get_default_date(),
             'product_id': product and product.id or False,
             'unit_amount': qty,
-            'amount': 0,
+            'amount': amount,
             'product_uom_id': product.uom_id.id,
             'general_account_id': general_account.id,
             'estim_std_cost': -qty * (std_cost or
