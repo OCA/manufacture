44681ee71f264f97c58c90cc8befc7c01db0a1be
oihane <oihanecruce@gmail.com>
2015-10-26 12:31:55 +0100

    [MIG] quality_control_mrp: migrated from odoomrp/odoomrp-wip
    [IMP] quality_control_mrp: required changes
    * Limit access by groups in views
    * Use api.model

--- quality_control_mrp_oca/models/qc_inspection.py
+++ quality_control_mrp_oca/models/qc_inspection.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
+# (c) 2014 Serv. Tec. Avanzados - Pedro M. Baeza
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
 from openerp import models, fields, api
 
 
--- quality_control_mrp_oca/tests/__init__.py
+++ quality_control_mrp_oca/tests/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# (c) 2014 Serv. Tec. Avanzados - Pedro M. Baeza
+# (c) 2015 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from . import models
+from . import test_quality_control_mrp
