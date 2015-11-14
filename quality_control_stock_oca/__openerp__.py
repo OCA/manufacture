4eaf880d0c6438fc35d0663c0635442db6e9fb29
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-11-14 01:53:55 +0100

    [IMP] quality_control_*: Make use of qty field
    Making it a regular field, you can put any manual quantity in inspections

--- quality_control_stock_oca/__openerp__.py
+++ quality_control_stock_oca/__openerp__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Quality control - Stock",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.0.1",
     "category": "Quality control",
     "license": "AGPL-3",
     "author": "OdooMRP team, "
