18e112e65c51af9b6dee6fca0dd02a23d6088df7
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-12-09 14:47:43 +0100

    [FIX] mrp_operations_extension: Hook import for migration script

--- mrp_operations_extension/migrations/8.0.2.0.0/post-migration.py
+++ mrp_operations_extension/migrations/8.0.2.0.0/post-migration.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # (c) 2015 Serv. Tecnol. Avanzados - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp.addons.mrp_operations_extension import (
+from openerp.addons.mrp_operations_extension.hooks import (
     create_default_routing_workcenter_line)
 
 
