a30fe955bb54382677ff3e235edf69b5203f8082
Oihane Crucelaegui <oihanecruce@gmail.com>
2016-01-14 19:24:56 +0100

    [MIG] Make mrp_bom_note installable in v9

--- mrp_bom_note/models/mrp_bom.py
+++ mrp_bom_note/models/mrp_bom.py
@@ -1,7 +1,6 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2015 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp import models, fields
 
