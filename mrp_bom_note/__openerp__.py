ac402dc3898014fa22d4738174a334b638f80083
Oihane Crucelaegui <oihanecruce@gmail.com>
2016-01-14 19:24:56 +0100

    [MIG] Make mrp_bom_note installable in v9

--- mrp_bom_note/__openerp__.py
+++ mrp_bom_note/__openerp__.py
@@ -1,24 +1,10 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2015 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    "name": "MRP - BoM Notes",
-    "version": "8.0.1.0.0",
+    "name": "Notes in Bill of Materials",
+    "version": "9.0.1.0.0",
     "author": "OdooMRP team,"
               "AvanzOSC,"
               "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
@@ -36,5 +22,5 @@
     "data": [
         "views/mrp_bom_view.xml",
     ],
-    'installable': False
+    'installable': True
 }
