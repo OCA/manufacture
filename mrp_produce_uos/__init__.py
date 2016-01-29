e8829848191717c4f265c2ff512bb2840d387477
Alex Comba <alex.comba@agilebg.com>
2016-01-29 15:02:45 +0100

    Avoid models folder since there's no need to update product_uos_qty on move_created_ids2. This is already done by split method of stock.move (see file stock/stock.py as reference) updating product_uos_qty on move_created_ids

--- mrp_produce_uos/__init__.py
+++ mrp_produce_uos/__init__.py
@@ -2,5 +2,4 @@
 # (c) 2015 Alex Comba - Agile Business Group
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from . import models
 from . import wizard
--- mrp_produce_uos/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# (c) 2015 Alex Comba - Agile Business Group
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from . import mrp_production
