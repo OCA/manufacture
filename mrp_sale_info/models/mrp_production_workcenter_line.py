258cfb9095e09077e868a4c3bb8e7c4f99402b83
victor <victor.martin@elico-corp.com>
2016-02-16 17:39:02 +0800

    [FIX] mrp_sale_info: Issue "CI/runbot not successful on GitHub", fix mrp_production_workcenter_line.py file (there is no need to inherit from "mrp.production" model)

--- mrp_sale_info/models/mrp_production_workcenter_line.py
+++ mrp_sale_info/models/mrp_production_workcenter_line.py
@@ -6,14 +6,13 @@ from openerp import models, fields
 
 
 class MrpProductionWorkcenterLine(models.Model):
-    _name = "mrp.production.workcenter.line"
-    _inherit = ['mrp.production.workcenter.line', 'mrp.production']
+    _inherit = 'mrp.production.workcenter.line'
 
     sale_id = fields.Many2one(related='production_id.sale_id',
                               string='Sale order', readonly=True, store=True)
     sale_line = fields.Many2one(
         comodel_name='sale.order.line', string='Sale Line',
-        related='move_prod_id.procurement_id.sale_line_id')
+        related='production_id.sale_line')
     partner_id = fields.Many2one(related='sale_id.partner_id', readonly=True,
                                  string='Customer', store=True)
     commitment_date = fields.Datetime(related='sale_id.commitment_date',
