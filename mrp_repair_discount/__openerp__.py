451b832001978f799a201953c84be33a3fb479ea
Nicola Malcontenti <nicola.malcontenti@agilebg.com>
2016-03-01 09:31:22 +0100

    [MIG] Mig mrp_discount to v10 and added discount on fees lines

---
+++ mrp_repair_discount/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2015 Nicola Malcontenti - Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "MRP Repair Discount",
+    "version": "8.0.1.0.0",
+    "category": "Manufactoring",
+    "license": "AGPL-3",
+    "author": "Agile Business Group, Odoo Community Association (OCA)",
+    "website": "http://www.agilebg.com",
+    "contributors": [
+        "Nicola Malcontenti <nicola.malcontenti@gmail.com>",
+    ],
+    'depends': ['mrp_repair'],
+    "data": ["views/mrp_repair_view.xml"],
+    "installable": True,
+}
