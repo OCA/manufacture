dece5f11eb677be4e3e097ff114c582c4510aa05
Thibault Rey <rey.thibault@gmail.com>
2016-03-28 18:02:06 +0200

    [FIX] fix comment done by the community

--- mrp_workcenter_hierarchical/__openerp__.py
+++ mrp_workcenter_hierarchical/__openerp__.py
@@ -1,84 +1,17 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) All Rights Reserved 2015 Akretion
-#    @author David BEAL <david.beal@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
-
+# © 2016 Akretion (http://www.akretion.com)
+# David BEAL <david.beal@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'MRP Workcenter Hierarchical',
-    'version': '0.5',
+    'version': '9.0.1.0.0',
     'author': 'Akretion,Odoo Community Association (OCA)',
     'summary': "Organise Workcenters by section",
     'category': 'Manufacturing',
     'depends': [
         'mrp_operations',
     ],
-    'description': """
-.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
-    :alt: License
-
-MRP Workcenter Hierarchical
-===============================
-
-* Add the possibility to define a parent workcenter to a workcenter
-
-Configuration
-=============
-
-To visualize features offered by this module, you need to:
-
- * go to Settings > Configuration > Manufacturing
- * in the section Manufacturing Order / Planning,
-   check "Manage routings and work orders" and Validate
-
-Usage
-=====
-
-To use this module, you need to go to:
-
- * Manufacturing > Configuration > Workcenter Hierarchical
-
-
-Credits
-=======
-
-Contributors
-------------
-
-* David BEAL <david.beal@akretion.com>
-* Sébastien BEAU <sebastien.beau@akretion.com>
-
-Maintainer
-----------
-
-.. image:: http://odoo-community.org/logo.png
-   :alt: Odoo Community Association
-   :target: http://odoo-community.org
-
-This module is maintained by the OCA.
-
-OCA, or the Odoo Community Association, is a nonprofit organization
-whose mission is to support the collaborative development of Odoo features
-and promote its widespread use.
-
-To contribute to this module, please visit http://odoo-community.org.
-""",
     'website': 'http://www.akretion.com/',
     'data': [
         'workcenter_view.xml',
