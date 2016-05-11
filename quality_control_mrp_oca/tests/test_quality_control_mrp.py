ccb9291ac8b1279271f46800f93f37239dd50656
oihane <oihanecruce@gmail.com>
2016-05-11 12:03:30 +0200

    [IMP] quality_control_stock: if lot in reference, get its info quality_control_mrp: tests extended
    [IMP] quality_control_mrp: take product from manufacturing order
    
    [IMP] quality_control_mrp: removed not required code
    
    [FIX] quality_control_mrp: expected singleton error
    
    [FIX] quality_control_mrp: partial fabrications where creating more inspections than required

--- quality_control_mrp_oca/tests/test_quality_control_mrp.py
+++ quality_control_mrp_oca/tests/test_quality_control_mrp.py
@@ -10,16 +10,24 @@ class TestQualityControlMrp(TransactionCase):
     def setUp(self):
         super(TestQualityControlMrp, self).setUp()
         self.production_model = self.env['mrp.production']
+        self.inspection_model = self.env['qc.inspection']
         self.qc_trigger_model = self.env['qc.trigger']
         self.product = self.env.ref('product.product_product_4')
         self.test = self.env.ref('quality_control.qc_test_1')
         self.trigger = self.env.ref('quality_control_mrp.qc_trigger_mrp')
         self.production1 = self.production_model.create({
             'product_id': self.product.id,
+            'product_qty': 2.0,
             'product_uom': self.product.uom_id.id,
         })
         self.production1.action_confirm()
         self.production1.action_assign()
+        inspection_lines = (
+            self.inspection_model._prepare_inspection_lines(self.test))
+        self.inspection1 = self.inspection_model.create({
+            'name': 'Test Inspection',
+            'inspection_lines': inspection_lines,
+        })
 
     def test_inspection_create_for_product(self):
         self.product.qc_triggers = [(
@@ -78,3 +86,27 @@ class TestQualityControlMrp(TransactionCase):
             'consume_produce')
         self.assertEqual(self.production1.created_inspections, 1,
                          'Only one inspection must be created')
+
+    def test_inspection_with_partial_fabrication(self):
+        self.product.qc_triggers = [(
+            0, 0, {
+                'trigger': self.trigger.id,
+                'test': self.test.id,
+            }
+        )]
+        self.production1.action_produce(
+            self.production1.id, 1.0, 'consume_produce')
+        self.assertEqual(self.production1.created_inspections, 1,
+                         'Only one inspection must be created.')
+        self.production1.action_produce(
+            self.production1.id, 1.0, 'consume_produce')
+        self.assertEqual(self.production1.created_inspections, 2,
+                         'There must be only 2 inspections.')
+
+    def test_qc_inspection_mo(self):
+        self.inspection1.write({
+            'object_id': '%s,%d' % (self.production1._model,
+                                    self.production1.id),
+        })
+        self.assertEquals(self.inspection1.production,
+                          self.production1)
