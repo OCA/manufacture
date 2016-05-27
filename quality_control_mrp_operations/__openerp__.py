956f47ed6060f3b744efda78a892df8b3567be10
mike <viriyananda.michael@gmail.com>
2016-05-27 16:07:25 +0700

    [MIG]quality_control_mrp_operations from odoomrp-wip

--- quality_control_mrp_operations/__openerp__.py
+++ quality_control_mrp_operations/__openerp__.py
@@ -1,43 +1,23 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# (c) 2014 Serv. Tec. Avanzados - Pedro M. Baeza
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    "name": "Quality Control - MRP operations",
-    "version": "1.0",
-    "category": "Quality control",
-    "author": "OdooMRP team,"
-              "AvanzOSC,"
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza",
-    "website": "http://www.odoomrp.com",
-    "contributors": [
-        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
-        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
-        "Ana Juaristi <ajuaristio@gmail.com>"
+    'name': 'Quality Control - MRP Operations',
+    'version': '8.0.1.0.0',
+    'summary': 'Enables quality control for MRP operations',
+    'author': 'Serv. Tec. Avanzados,Odoo Community Association (OCA)',
+    'website': 'http://www.odoomrp.com',
+    'category': 'Quality control',
+    'depends': [
+        'mrp_operations_extension',
+        'quality_control',
     ],
-    "depends": [
-        "mrp_operations_extension",
-        "quality_control",
+    'data': [
+        'views/qc_inspection_view.xml',
+        'views/mrp_routing_operation_view.xml',
+        'views/mrp_production_workcenter_line_view.xml',
     ],
-    "data": [
-        "views/qc_inspection_view.xml",
-        "views/mrp_routing_operation_view.xml",
-        "views/mrp_production_workcenter_line_view.xml",
-    ],
-    "installable": True,
-    "auto_install": False,
+    'installable': True,
+    'license': 'AGPL-3',
 }
