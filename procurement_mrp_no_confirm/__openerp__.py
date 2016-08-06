22eb5e17f78e77cd0ce226fc07f1441dac9a9498
Andhitia Rama <andhitia.r@gmail.com>
2016-08-06 14:16:38 +0700

    Move from odoomrp/odoomrp-wip

--- procurement_mrp_no_confirm/__openerp__.py
+++ procurement_mrp_no_confirm/__openerp__.py
@@ -1,32 +1,19 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2015 AvanzOSC
+# © 2015 Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Procurement MRP no Confirm",
-    "version": "1.0",
+    "version": "8.0.1.0.0",
     "depends": ["mrp"],
     "author": "OdooMRP team,"
               "AvanzOSC,"
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza",
+              "Tecnativa,"
+              "Odoo Community Association (OCA)",
     "category": "MRP",
-    'data': ["data/mrp_production_workflow.xml"],
-    'demo': [],
-    'installable': True,
-    'auto_install': False,
-    'uninstall_hook': 'uninstall_hook',
+    "data": ["data/mrp_production_workflow.xml"],
+    "installable": True,
+    "auto_install": False,
+    "uninstall_hook": "uninstall_hook",
 }
--- procurement_mrp_no_confirm/tests/__init__.py
+++ procurement_mrp_no_confirm/tests/__init__.py
@@ -3,5 +3,4 @@
 # © 2015 Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import procurement_order
-from . import mrp_production
+from . import test_procurement
