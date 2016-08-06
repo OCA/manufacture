d455ec369f7624aa247591e9807ae313c467431f
Andhitia Rama <andhitia.r@gmail.com>
2016-08-06 10:15:11 +0700

    Move from odoomrp-wip

--- mrp_operations_start_without_material/tests/__init__.py
+++ mrp_operations_start_without_material/tests/__init__.py
@@ -2,8 +2,7 @@
 # © 2015 Daniel Campos
 # © 2015 Pedro M. Baeza
 # © 2015 Ana Juaristi
+# © 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import mrp_routing
-from . import mrp_production
-from . import mrp_bom
+from . import test_mrp_operation
