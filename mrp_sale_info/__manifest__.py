ed5039a287eb5c79632534da86456a70acfebc0a
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-10-06 14:57:42 +0200

    [MIG] Make modules uninstallable & rename manifest files

--- mrp_sale_info/__manifest__.py
+++ mrp_sale_info/__manifest__.py
@@ -14,7 +14,7 @@
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
-    "installable": True,
+    'installable': False,
     "depends": [
         "mrp_operations",
         "sale_mrp",
