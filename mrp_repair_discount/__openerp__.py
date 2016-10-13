7fb6bdaf5889e786bbb0b207b0d2b3cf712cc00f
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-10-14 01:21:24 +0200

    mrp_discount: New API + tests

--- mrp_repair_discount/__openerp__.py
+++ mrp_repair_discount/__openerp__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2015 Nicola Malcontenti - Agile Business Group
+# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
@@ -7,7 +8,9 @@
     "version": "8.0.1.0.0",
     "category": "Manufactoring",
     "license": "AGPL-3",
-    "author": "Agile Business Group, Odoo Community Association (OCA)",
+    "author": "Agile Business Group, "
+              "Tecnativa, "
+              "Odoo Community Association (OCA)",
     "website": "http://www.agilebg.com",
     "contributors": [
         "Nicola Malcontenti <nicola.malcontenti@gmail.com>",
--- mrp_repair_discount/models/mrp_repair.py
+++ mrp_repair_discount/models/mrp_repair.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 # © 2015 Nicola Malcontenti - Agile Business Group
+# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
-from openerp.osv import orm
+from openerp import api, fields, models
+from openerp.osv import fields as old_fields
+import openerp.addons.decimal_precision as dp
 
 
 class MrpRepairLine(models.Model):
@@ -11,14 +13,32 @@ class MrpRepairLine(models.Model):
 
     discount = fields.Float(string='Discount (%)')
 
+    @api.multi
+    def _amount_line(self, field_name, arg):
+        res = super(MrpRepairLine, self)._amount_line(field_name, arg)
+        for line in self.filtered('discount'):
+            price = res[line.id] * (1 - (line.discount or 0.0) / 100.0)
+            res[line.id] = price
+        return res
 
-class MrpRepair(orm.Model):
+    _columns = {
+        # Must be defined in old API so that we can call super in the compute
+        'price_subtotal': old_fields.function(
+            _amount_line, string='Subtotal',
+            digits_compute=dp.get_precision('Account')),
+    }
+
+
+class MrpRepair(models.Model):
     _inherit = 'mrp.repair'
 
-    def action_invoice_create(self, cr, uid, ids, group=False, context=None):
-        res = super(MrpRepair, self).action_invoice_create(
-            cr, uid, ids, group, context)
-        repair_obj = self.browse(cr, uid, ids, context=context)
-        for op in repair_obj.operations:
-            op.invoice_line_id.discount = op.discount
+    @api.multi
+    def action_invoice_create(self, group=False):
+        res = super(MrpRepair, self).action_invoice_create(group)
+        for repair in self:
+            operations = repair.operations.filtered('to_invoice')
+            for op in operations:
+                op.invoice_line_id.discount = op.discount
+            if operations:
+                repair.invoice_id.button_reset_taxes()
         return res
