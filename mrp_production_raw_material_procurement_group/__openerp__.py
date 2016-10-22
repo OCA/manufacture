b837c69bcba630d60ecff7d92994327745514e50
Andhitia Rama <andhitia.r@gmail.com>
2016-10-22 17:46:55 +0700

    mrp_production_raw_material_procurement_group

---
+++ mrp_production_raw_material_procurement_group/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Create Procurement Group On Manufacturing Order",
+    "version": "8.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "views/mrp_production_views.xml",
+    ],
+}
---
+++ mrp_production_raw_material_procurement_group/models/mrp_production.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class MrpProduction(models.Model):
+    _inherit = "mrp.production"
+
+    raw_material_procurement_group_id = fields.Many2one(
+        string="Raw Material Procurement Group",
+        comodel_name="procurement.group",
+        )
+
+    @api.model
+    def _make_consume_line_from_data(
+            self,
+            production, product, uom_id, qty,
+            uos_id, uos_qty):
+
+        move_id = super(MrpProduction, self)._make_consume_line_from_data(
+            production, product, uom_id, qty,
+            uos_id, uos_qty)
+        obj_move = self.env["stock.move"]
+        move = obj_move.browse(move_id)
+        if move.procure_method == "make_to_stock":
+            return move_id
+        if not production.raw_material_procurement_group_id:
+            return move_id
+        move.write(
+            {"group_id": production.raw_material_procurement_group_id.id})
+        return move_id
