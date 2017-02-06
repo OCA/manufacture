6bfe68cdb3cfa004b10540841907f7549f3bec93
MonsieurB <contact@ebii.fr>
2017-02-06 11:42:57 +0100

    migrate V10

--- mrp_production_note/models/mrp_production.py
+++ mrp_production_note/models/mrp_production.py
@@ -2,7 +2,7 @@
 # © 2015 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class MrpProduction(models.Model):
