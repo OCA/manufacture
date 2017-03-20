65fa8fa023d07f138754ec129f80ea1fb772ef4f
Rubén Cabrera <rcabrera@praxya.com>
2017-03-20 18:45:58 +0100

    PEP8 fixes

--- mrp_project/models/res_config.py
+++ mrp_project/models/res_config.py
@@ -2,24 +2,23 @@
 ##############################################################################
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
-from openerp import fields, models, _, api
+from openerp import fields, models, api
 
 
 class MrpConfigSettings(models.TransientModel):
     _inherit = 'mrp.config.settings'
     default_create_project = fields.Boolean(
-        string=_("Create projects from manufacturing orders"),
-        help=_("""Automatically create projects from manufacturing orders if a
-                 project is not assigned."""),
+        string="Create projects from manufacturing orders",
+        help="""Automatically create projects from manufacturing orders if a
+                 project is not assigned.""",
         default_model="mrp.production",
                  )
     default_create_tasks = fields.Boolean(
-        string=_("Create tasks for each work order in production"),
-        help=_(""" Automatically create tasks linked to manufacturing orders in
-               assigned project."""),
+        string="Create tasks for each work order in production",
+        help=""" Automatically create tasks linked to manufacturing orders in
+               assigned project.""",
         default_model="mrp.production"
     )
-
 
     def _get_parameter(self, key, default=False):
         param_obj = self.env['ir.config_parameter']
@@ -50,4 +49,3 @@ class MrpConfigSettings(models.TransientModel):
     @api.multi
     def set_parameter_cycle_bom(self):
         self._write_or_create_param('cycle.by.bom', self.cycle_by_bom)
-
