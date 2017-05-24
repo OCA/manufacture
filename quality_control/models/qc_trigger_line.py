272b44c042c88ecaeb08fd653da0ab2aa391d68b
lreficent <lois.rilo@eficent.com>
2017-05-24 17:24:55 +0200

    [9.0][MIG] quality_control

--- quality_control/models/qc_trigger_line.py
+++ quality_control/models/qc_trigger_line.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-from openerp import models, fields
+# Copyright 2010 NaN Projectes de Programari Lliure, S.L.
+# Copyright 2014 Serv. Tec. Avanzados - Pedro M. Baeza
+# Copyright 2014 Oihane Crucelaegui - AvanzOSC
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import fields, models
 
 
 def _filter_trigger_lines(trigger_lines):
