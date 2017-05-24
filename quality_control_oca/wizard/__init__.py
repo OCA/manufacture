3cbcbe3f31d5a743589dca7bb5ebf477fc6bd527
lreficent <lois.rilo@eficent.com>
2017-05-24 17:24:55 +0200

    [9.0][MIG] quality_control

--- quality_control_oca/wizard/__init__.py
+++ quality_control_oca/wizard/__init__.py
@@ -1,5 +1,8 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright 2010 NaN Projectes de Programari Lliure, S.L.
+# Copyright 2014 Serv. Tec. Avanzados - Pedro M. Baeza
+# Copyright 2014 Oihane Crucelaegui - AvanzOSC
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from . import qc_test_wizard
