55ad98678f748dbbbe578cff56f689bb8ae694ae
lreficent <lois.rilo@eficent.com>
2017-05-25 13:30:24 +0200

    [9.0][ADD] quality_control_issue

---
+++ quality_control_issue/models/qc_issue_stage.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models
+
+AVAILABLE_PRIORITIES = [
+    ('0', 'Normal'),
+    ('1', 'Low'),
+    ('2', 'High'),
+    ('3', 'Very High'),
+]
+
+
+class QualityControlIssueStage(models.Model):
+    _name = "qc.issue.stage"
+    _rec_name = 'name'
+    _order = "sequence, name, id"
+
+    @api.model
+    def default_get(self, fields):
+        """ Hack :  when going from the kanban view, creating a stage with a
+        qc team in context should not create a stage for the current team only.
+        """
+        ctx = dict(self.env.context)
+        if ctx.get('default_qc_team_id') and not ctx.get('some_context'):
+            ctx.pop('default_qc_team_id')
+        return super(QualityControlIssueStage,
+                     self.with_context(ctx)).default_get(fields)
+
+    name = fields.Char('Stage Name', required=True)
+    sequence = fields.Integer(
+        string='Sequence', help="Used to order stages. Lower is better.",
+        default=1)
+    qc_team_id = fields.Many2one(
+        comodel_name='qc.team', string='Quality Control Team',
+        ondelete='set null')
+    fold = fields.Boolean(
+        string='Folded in Pipeline', default=False,
+        help='This stage is folded in the kanban view when there are no '
+             'records in that stage to display.')
