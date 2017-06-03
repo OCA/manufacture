36371de94164090d0463c232cdf39e11172dc9c4
bima <bimajatiwijaya@gmail.com>
2017-06-03 21:57:15 +0700

    removed unused import api and _

--- mrp_bom_product_detail/models/mrp_bom_product_detail.py
+++ mrp_bom_product_detail/models/mrp_bom_product_detail.py
@@ -20,7 +20,7 @@
 #
 ##############################################################################
 
-from odoo import api, fields, models, _
+from odoo import fields, models
 
 
 class MrpBom(models.Model):
