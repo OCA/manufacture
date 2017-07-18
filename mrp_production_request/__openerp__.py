79e01279e65fb684578990c0d1718f522449cb1c
lreficent <lois.rilo@eficent.com>
2017-07-18 13:16:33 +0200

    [9.0][IMP] add a new field to show done qty.

--- mrp_production_request/__openerp__.py
+++ mrp_production_request/__openerp__.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "MRP Request",
+    "name": "MRP Production Request",
     "summary": "Allows you to use Manufacturing Request as a previous "
                "step to Manufacturing Orders for better manufacture "
                "planification.",
