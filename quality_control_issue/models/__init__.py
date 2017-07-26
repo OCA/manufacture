e16506f1893e84eebe88f0497dd0284cbe4b40f0
Jordi Ballester <jordi.ballester@eficent.com>
2017-07-26 18:46:05 +0200

    Several fixes: * add stage states * default values for relational fields must return a recordset. * do not allow to create qc issues from kanban quick create.

--- quality_control_issue/models/__init__.py
+++ quality_control_issue/models/__init__.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import qc_issue
+from . import qc_stage
 from . import qc_problem
 from . import qc_problem_group
-from . import qc_stage
 from . import qc_issue_stage
+from . import qc_issue
