9653e2bfb3544603a5c1c79e5c13a1a66d758704
Florian da Costa <florian.dacosta@akretion.com>
2017-07-27 12:28:54 +0200

    Add mrp_progress_button

---
+++ mrp_progress_button/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017 Akretion (http://www.akretion.com). All Rights Reserved
+# @author Florian DA COSTA <florian.dacosta@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Mrp Progress Button",
+    "summary": "Add a button on MO to make the MO state 'In Progress'",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "website": "https://akretion.com/",
+    "category": "Manufacturing",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["mrp"],
+    "data": ["views/production.xml"],
+}
