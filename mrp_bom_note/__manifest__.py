dd0cfc2133e579c5dd7dfaad7347eef01674e456
Bima <bimajatiwijaya@gmail.com>
2017-11-10 16:57:29 +0700

    [10.0] [MIG] mrp_bom_note (#202)
    Squashed commits:
    
    OCA Transbot updated translations from Transifex

--- mrp_bom_note/__manifest__.py
+++ mrp_bom_note/__manifest__.py
@@ -1,20 +1,15 @@
-# -*- coding: utf-8 -*-
 # © 2015 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     "name": "Notes in Bill of Materials",
-    "version": "9.0.1.0.0",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
     "author": "OdooMRP team,"
               "AvanzOSC,"
               "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
               "Odoo Community Association (OCA)",
     "website": "http://www.odoomrp.com",
-    "contributors": [
-        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
-        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
-        "Ana Juaristi <anajuaristi@avanzosc.es>",
-    ],
     "category": "Tools",
     "depends": [
         "mrp",
@@ -22,5 +17,5 @@
     "data": [
         "views/mrp_bom_view.xml",
     ],
-    'installable': False
+    'installable': True
 }
