cc6e48d168c33fdc4ec620c45c84e0fe9a94c9ec
Bima <bimajatiwijaya@gmail.com>
2017-11-10 16:57:29 +0700

    [10.0] [MIG] mrp_bom_note (#202)

--- mrp_bom_note/models/mrp_bom.py
+++ mrp_bom_note/models/mrp_bom.py
@@ -2,7 +2,7 @@
 # © 2015 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class MrpBom(models.Model):
