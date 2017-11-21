5193db6abc0243e4e64c561ad80cef9fb75e28cb
Simone Rubino <simone.rubino@agilebg.com>
2017-11-21 12:55:50 +0100

    [MIG] quality_control: Migration to 10.0

--- quality_control_oca/tests/__init__.py
+++ quality_control_oca/tests/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Oihane Crucelaegui - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import test_quality_control
