6be2e0e616c303f1549907abce7dab8a789a4e7c
Simone Rubino <simone.rubino@agilebg.com>
2017-11-21 12:55:50 +0100

    [MIG] quality_control: Migration to 10.0

--- quality_control_oca/__init__.py
+++ quality_control_oca/__init__.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2010 NaN Projectes de Programari Lliure, S.L.
-# Copyright 2014 Serv. Tec. Avanzados - Pedro M. Baeza
-# Copyright 2014 Oihane Crucelaegui - AvanzOSC
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import models
 from . import wizard
