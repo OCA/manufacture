79a5d5db3ddddf23cf6127d61c0052959cae822f
bima <bima@portcities.net>
2017-11-25 20:39:57 +0700

    [MIG] mrp_bom_product_detail : Migration to 10.0

--- mrp_bom_product_detail/models/__init__.py
+++ mrp_bom_product_detail/models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 # Copyright 2013 <http://www.libergia.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from . import models
+from . import mrp_bom_product_detail
---
+++ mrp_bom_product_detail/models/mrp_bom_product_detail.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2013 <http://www.libergia.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class MrpBom(models.Model):
+    _inherit = 'mrp.bom'
+
+    product_standard_price = fields.Float(related='product_id.standard_price',
+                                          string='Cost Price', readonly=True)
+    product_qty_available = fields.Float(related='product_id.qty_available',
+                                         string='Quantity On Hand',
+                                         readonly=True)
+
+
+class MrpBomLine(models.Model):
+    _inherit = 'mrp.bom.line'
+
+    product_standard_price = fields.Float(related='product_id.standard_price',
+                                          string='Cost Price', readonly=True)
+    product_qty_available = fields.Float(related='product_id.qty_available',
+                                         string='Quantity On Hand',
+                                         readonly=True)
