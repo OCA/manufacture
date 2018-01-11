e11c72d6a32417c2d1e410a2912eae18cd8212b1
lreficent <lois.rilo@eficent.com>
2018-01-11 15:59:11 +0100

    [10.0][MIG] mrp_production_putaway_strategy

--- mrp_production_putaway_strategy/models/mrp_production.py
+++ mrp_production_putaway_strategy/models/mrp_production.py
@@ -1,23 +1,26 @@
 # -*- coding: utf-8 -*-
-# © 2017 Eficent Business and IT Consulting Services S.L.
+# © 2017-18 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, models, _
+from odoo import api, models, _
 
 
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
-    @api.multi
-    def action_confirm(self):
-        for mo in self:
-            if not mo.move_prod_id:
-                location = self.env['stock.location'].get_putaway_strategy(
-                    mo.location_dest_id,  mo.product_id)
-                if location:
-                    message = _(
-                        'Applied Putaway strategy to finished products '
-                        'location %s.' % mo.location_dest_id.complete_name)
-                    mo.message_post(message, message_type='comment')
-                    mo.location_dest_id = location
-        return super(MrpProduction, self).action_confirm()
+    @api.model
+    def create(self, vals):
+        location_dest = self.env['stock.location'].browse(vals.get(
+            'location_dest_id'))
+        product = self.env['product.product'].browse(vals.get('product_id'))
+        location_id = location_dest.get_putaway_strategy(product)
+        if location_id:
+            vals['location_dest_id'] = location_id
+        mo = super(MrpProduction, self).create(vals)
+        if location_id:
+            message = _(
+                "Applied Putaway strategy to finished products.\n"
+                "Finished Products Location: %s." %
+                mo.location_dest_id.complete_name)
+            mo.message_post(message, message_type='comment')
+        return mo
