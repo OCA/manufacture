cd19a77d91f6034823773c84dae0fe164c5905c3
lreficent <lois.rilo@eficent.com>
2018-01-11 15:59:11 +0100

    [10.0][MIG] mrp_production_putaway_strategy

--- mrp_production_putaway_strategy/tests/test_mrp_production.py
+++ mrp_production_putaway_strategy/tests/test_mrp_production.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# © 2017 Eficent Business and IT Consulting Services S.L.
+# © 2017-18 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase
 
 
 class MrpProductionCase(TransactionCase):
@@ -39,15 +39,15 @@ class MrpProductionCase(TransactionCase):
 
         self.loc_production = self.env.ref(
             "stock.location_production")
-        self.product1 = self.env.ref("product.product_product_18")
+        self.product1 = self.env.ref("mrp.product_product_19")
         self.product1.categ_id = self.category
         self.bom1 = self.env.ref("mrp.mrp_bom_3")
 
     def _create_mo(self, product=False, bom=False, src_loc=False,
-                   dest_loc=False, qty=10.0, uom=False, move_prod_id=False):
+                   dest_loc=False, qty=10.0, uom=False):
         if not product:
             product = self.product1
-            uom = product.uom_id
+        uom = product.uom_id or uom
         if not bom:
             bom = self.bom1
         if not src_loc:
@@ -60,40 +60,15 @@ class MrpProductionCase(TransactionCase):
             "location_src_id": src_loc.id,
             "location_dest_id": dest_loc.id,
             "product_qty": qty,
-            "product_uom": uom.id,
-            "move_prod_id": move_prod_id.id if move_prod_id else False,
+            "product_uom_id": uom.id,
         }
         return self.env['mrp.production'].create(res)
 
     def test_putaway_strategy_01(self):
-        """Tests if the putaway strategy applies to a Manufacturing Order
-        without destination move."""
+        """Tests if the putaway strategy applies to a Manufacturing Order."""
         # Create MO
         mo = self._create_mo()
-        # Click confirm button
-        mo.signal_workflow("button_confirm")
-        for finished in mo.move_created_ids:
+        for finished in mo.move_finished_ids:
             self.assertEqual(
                 finished.location_dest_id, self.bin_loc_stock,
                 "Putaway strategy hasn't been applied.")
-
-    def test_putaway_strategy_02(self):
-        """Tests if the destination location is respected whenever a
-        destination move is set for the Manufactuing Order."""
-        # Create a destination transfer.
-        move = self.env['stock.move'].create({
-            "name": "Destination move for the test MO",
-            "product_id": self.product1.id,
-            "product_uom_qty": 10.0,
-            "product_uom": self.product1.uom_id.id,
-            "location_id": self.loc_stock.id,
-            "location_dest_id": self.bin_loc_stock.id,
-        })
-        # Create MO
-        mo = self._create_mo(move_prod_id=move)
-        # Click confirm button
-        mo.signal_workflow("button_confirm")
-        for finished in mo.move_created_ids:
-            self.assertEqual(
-                finished.location_dest_id, self.loc_stock,
-                "Destination move has not been respected.")
