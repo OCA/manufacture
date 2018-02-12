3b899e3c9fbb773568f7d4f7355c53fedee8ea4b
mpanarin <m.panarin@mobilunity.com>
2018-02-12 18:44:35 +0200

    [MIG] mrp_bom_location: Migration to Odoo 11

--- mrp_bom_location/__manifest__.py
+++ mrp_bom_location/__manifest__.py
@@ -1,16 +1,14 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "MRP BOM Location",
     "summary": "Adds location field to Bill of Materials and its components.",
-    "version": "9.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Manufacture",
-    "website": "https://odoo-community.org/",
+    "website": "https://github.com/OCA/manufacture",
     "author": "Eficent, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
-    "installable": True,
     "depends": [
         "mrp",
     ],
