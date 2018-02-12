234a48dd4e521943467dc9487024459dd2f09aa8
mpanarin <m.panarin@mobilunity.com>
2018-02-12 18:44:35 +0200

    [MIG] mrp_bom_location: Migration to Odoo 11

--- mrp_bom_location/models/mrp_bom.py
+++ mrp_bom_location/models/mrp_bom.py
@@ -1,23 +1,22 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class MrpBom(models.Model):
     _inherit = "mrp.bom"
 
     location_id = fields.Many2one(
-        comodel_name="stock.location", string="Location",
-        help="Set the preferred location for this BOM.",
-        domain=[('usage', '=', 'internal')])
+        related='picking_type_id.default_location_dest_id',
+        store=True,
+    )
 
 
-class MrpBom(models.Model):
+class MrpBomLine(models.Model):
     _inherit = "mrp.bom.line"
 
     location_id = fields.Many2one(
-        comodel_name="stock.location", string="Location",
-        help="Location which it is expected to get the products from.",
-        domain=[('usage', '=', 'internal')])
+        related='bom_id.picking_type_id.default_location_src_id',
+        store=True,
+    )
