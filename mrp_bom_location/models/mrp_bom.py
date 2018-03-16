a8caae396b31164a8866e495adf01dfd849b3fbf
mpanarin <m.panarin@mobilunity.com>
2018-03-16 16:24:54 +0200

    [IMP] make an onchange for location_id, fix readme

--- mrp_bom_location/models/mrp_bom.py
+++ mrp_bom_location/models/mrp_bom.py
@@ -1,24 +1,29 @@
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class MrpBom(models.Model):
     _inherit = "mrp.bom"
 
     location_id = fields.Many2one(
-        related='picking_type_id.default_location_dest_id',
-        readonly=True,
-        store=True,
+        string='Location',
+        comodel_name='stock.location',
     )
+
+    @api.onchange('picking_type_id')
+    def _onchange_picking_type_id(self):
+        if (self.picking_type_id and
+                self.picking_type_id.default_location_src_id):
+            self.location_id = self.picking_type_id.default_location_src_id
 
 
 class MrpBomLine(models.Model):
     _inherit = "mrp.bom.line"
 
     location_id = fields.Many2one(
-        related='bom_id.picking_type_id.default_location_src_id',
+        related='bom_id.location_id',
         readonly=True,
         store=True,
     )
