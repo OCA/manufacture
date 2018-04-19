5dd73afbf6857a7950bfb2e3221b0d6ab36a55b2
Abdulrhman Moahmmed <obeidgad@gmail.com>
2018-04-19 16:48:56 +0200

    mrp_production_partner_note

--- mrp_production_partner_note/tests/test_mrp_production_partner_note.py
+++ mrp_production_partner_note/tests/test_mrp_production_partner_note.py
@@ -2,7 +2,7 @@
 # © 2016 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp.tests import common
+from oddo.tests import common
 
 
 class TestMrpProductionPartnerNote(common.TransactionCase):
