24782de7c0fd6c43cb9b6f47a18318822f394997
hveficent <hector.villarreal@eficent.com>
2018-04-25 14:57:10 +0200

    [MIG] mrp_production_service: Migration to 11.0

--- mrp_production_service/__manifest__.py
+++ mrp_production_service/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
@@ -6,12 +5,13 @@
     "name": "MRP Production Service",
     "summary": "Creates procurement orders from manufacturing orders, for "
                "services included in the Bill of Materials",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Eficent,"
               "Odoo Community Association (OCA)",
     "website": "https://www.odoo-community.org",
     "category": "Warehouse Management",
-    "depends": ["mrp"],
+    "depends": ["mrp",
+                "subcontracted_service"],
     "license": "AGPL-3",
     'installable': True,
     'application': False,
--- mrp_production_service/tests/test_mrp_production_service.py
+++ mrp_production_service/tests/test_mrp_production_service.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
@@ -9,14 +8,31 @@ from odoo.tests.common import TransactionCase
 class TestMrpProductionService(TransactionCase):
     def setUp(self):
         super(TestMrpProductionService, self).setUp()
-        # Create products
-        product_model = self.env['product.product']
-        self.p1 = product_model.create({'name': '101'})
-        self.p2 = product_model.create({'name': '201P'})
-        self.p3 = product_model.create({'name': '202P'})
-        self.service = product_model.create({'name': 'Assembly Service',
-                                             'type': 'service'})
 
+        # Create products
+        self.obj_warehouse = self.env['stock.warehouse']
+        self.test_wh = self.obj_warehouse.create({
+            'name': 'Test WH',
+            'code': 'T',
+        })
+        self.supplier = self.env['res.partner'].create({
+            'name': 'Supplier',
+            'supplier': True,
+        })
+        self.product_model = self.env['product.product']
+        self.p1 = self.product_model.create({
+            'name': '101',
+            'type': 'product',
+        })
+        self.service = self.product_model.create({
+            'name': 'Galvanize Service',
+            'type': 'service',
+            'seller_ids': [(0, 0, {
+                'name': self.supplier.id,
+                'price': 100.0,
+            })]
+        })
+        self.service.property_subcontracted_service = True
         # Create bill of materials
         self.bom_model = self.env['mrp.bom']
         self.bom = self.bom_model.create({
@@ -27,16 +43,6 @@ class TestMrpProductionService(TransactionCase):
         })
         # Create bom lines
         self.bom_line_model = self.env['mrp.bom.line']
-        self.bom_line_model.create({
-            'bom_id': self.bom.id,
-            'product_id': self.p2.id,
-            'product_qty': 1,
-        })
-        self.bom_line_model.create({
-            'bom_id': self.bom.id,
-            'product_id': self.p3.id,
-            'product_qty': 1,
-        })
         self.bom_line_model.create({
             'bom_id': self.bom.id,
             'product_id': self.service.id,
@@ -54,7 +60,7 @@ class TestMrpProductionService(TransactionCase):
             'bom_id': self.bom.id
         })
 
-        procurement = self.env['procurement.order'].search(
+        procurement = self.env['purchase.order.line'].search(
             [('product_id', 'in',
               self.bom.bom_line_ids.mapped('product_id.id'))])
 
