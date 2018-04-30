fe0fc5593dc9c808b268f84947e6ebbf2b545a9f
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-04-30 08:41:02 -0500

    [ADD] mrp_barcode

---
+++ mrp_barcode/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "MRP Barcode",
+    "summary": "Scan barcodes in Manufacturing",
+    "version": "11.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "category": "MRP",
+    "website": "http://github.com/oca/manufacture",
+    "depends": [
+        "quality_mrp"
+    ],
+    "data": [
+        "views/mrp_view.xml",
+    ],
+    "installable": True,
+}
