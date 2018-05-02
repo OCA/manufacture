14519df50bf1bf30e30e423e11b5d830ea1ddfdc
Lois Rilo <lois.rilo@eficent.com>
2018-05-02 11:26:45 +0200

    [11.0][MIG] mrp_auto_assign

--- mrp_auto_assign/models/mrp_production.py
+++ mrp_auto_assign/models/mrp_production.py
@@ -1,11 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2017 Akretion (http://www.akretion.com). All Rights Reserved
 # @author Florian DA COSTA <florian.dacosta@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
-import logging
-_logger = logging.getLogger(__name__)
 
 
 class MrpProduction(models.Model):
