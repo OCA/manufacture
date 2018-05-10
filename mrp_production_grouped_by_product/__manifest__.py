d2116d2fb4e9586457e257e51d882ae654f7658d
David <david.vidal@tecnativa.com>
2018-05-10 20:22:27 +0200

    [ADD] mrp_production_grouped_by_product: New Module

---
+++ mrp_production_grouped_by_product/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2018 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Production Grouped By Product',
+    'version': '11.0.1.0.0',
+    'category': 'MRP',
+    'author': 'Tecnativa, '
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/oca/manufacture',
+    'license': 'AGPL-3',
+    'depends': [
+        'mrp',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
