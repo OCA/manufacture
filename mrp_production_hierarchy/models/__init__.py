fb68c2db700466e403f3a604eb62673447b20422
sebalix <seb@usr-src.org>
2018-05-18 15:15:42 +0200

    New module 'mrp_production_hierarchy'

---
+++ mrp_production_hierarchy/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from . import mrp_production
+from . import procurement_order
---
+++ mrp_production_hierarchy/tests/test_mrp_production_hierarchy.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 ABF OSIELL <http://osiell.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.addons.mrp.tests.common import TestMrpCommon
+
+
+class TestMrpProductionHierarchy(TestMrpCommon):
+
+    def setUp(self):
+        super(TestMrpProductionHierarchy, self).setUp()
+        self.bom_2.write({'type': 'normal'})
+        route_manufacture = self.env.ref('mrp.route_warehouse0_manufacture')
+        route_mto = self.env.ref('stock.route_warehouse0_mto')
+        (self.bom_1 | self.bom_2 | self.bom_3).mapped('product_id').write(
+            {'route_ids': [(6, 0, [route_manufacture.id, route_mto.id])]})
+
+    def test_production_hierarchy(self):
+        man_order = self.env['mrp.production'].create({
+            'name': 'MO-Test',
+            'product_id': self.product_6.id,
+            'product_uom_id': self.product_6.uom_id.id,
+            'product_qty': 2,
+            'bom_id': self.bom_3.id,
+        })
+        self.assertEqual(len(man_order.child_ids), 2)
+        for child in man_order.child_ids:
+            self.assertIn(child.product_id, [self.product_5, self.product_4])
+            for child2 in child.child_ids:
+                self.assertIn(child2.product_id, [self.product_4])
