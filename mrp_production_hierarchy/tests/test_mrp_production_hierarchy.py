f6a53a1a0148dca740e9fcc09e6a66197e446bd4
sebalix <seb@usr-src.org>
2018-05-22 14:21:11 +0200

    [FIX] mrp_production_hierarchy: Increase test coverage

--- mrp_production_hierarchy/tests/test_mrp_production_hierarchy.py
+++ mrp_production_hierarchy/tests/test_mrp_production_hierarchy.py
@@ -16,6 +16,7 @@ class TestMrpProductionHierarchy(TestMrpCommon):
             {'route_ids': [(6, 0, [route_manufacture.id, route_mto.id])]})
 
     def test_production_hierarchy(self):
+        # bom_3 (product_6) -> bom_2 (product_5) -> bom_1 (product_4)
         man_order = self.env['mrp.production'].create({
             'name': 'MO-Test',
             'product_id': self.product_6.id,
@@ -24,11 +25,17 @@ class TestMrpProductionHierarchy(TestMrpCommon):
             'bom_id': self.bom_3.id,
         })
         self.assertEqual(len(man_order.child_ids), 2)
+        self.assertIn(
+            self.bom_2.product_id, man_order.child_ids.mapped('product_id'))
         for child in man_order.child_ids:
             self.assertIn(child.product_id, [self.product_5, self.product_4])
             self.assertEqual(child.root_id, man_order)
             self.assertEqual(child.parent_id, man_order)
+            if child.product_id == self.bom_2.product_id:
+                self.assertTrue(child.child_ids)
+                self.assertTrue(child.open_production_tree())
             for child2 in child.child_ids:
                 self.assertIn(child2.product_id, [self.product_4])
                 self.assertEqual(child2.root_id, man_order)
                 self.assertEqual(child2.parent_id, child)
+        self.assertTrue(man_order.open_production_tree())
