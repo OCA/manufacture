67da2e9260e74afe0974760a63b6e737a1298d67
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-06-04 01:29:31 +0200

    [IMP] mrp_production_grouped_by_product: Time frames
    Introduce time frames for grouping.

--- mrp_production_grouped_by_product/models/mrp_production.py
+++ mrp_production_grouped_by_product/models/mrp_production.py
@@ -2,7 +2,8 @@
 # Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from dateutil.relativedelta import relativedelta
+from odoo import api, fields, models
 from odoo.tools import config
 
 
@@ -25,15 +26,54 @@ class MrpProduction(models.Model):
             self.move_finished_ids.move_dest_ids = vals['move_dest_ids']
         self.write(new_vals)
 
-    def _find_grouping_target(self, vals):
-        mo = self.env['mrp.production'].search([
+    def _get_grouping_target_domain(self, vals):
+        """Get the domain for searching manufacturing orders that can match
+        with the criteria we want to use.
+
+        :param vals: Values dictionary of the MO to be created.
+
+        :return: Odoo domain.
+        """
+        domain = [
             ('product_id', '=', vals['product_id']),
+            ('picking_type_id', '=', vals['picking_type_id']),
             ('bom_id', '=', vals.get('bom_id', False)),
             ('routing_id', '=', vals.get('routing_id', False)),
             ('company_id', '=', vals.get('company_id', False)),
             ('state', '=', 'confirmed'),
-        ], limit=1)
-        return mo
+        ]
+        if not vals.get('date_planned_finished'):
+            return domain
+        date = fields.Datetime.from_string(vals['date_planned_finished'])
+        pt = self.env['stock.picking.type'].browse(vals['picking_type_id'])
+        if date.hour < pt.mo_grouping_max_hour:
+            date_end = date.replace(
+                hour=pt.mo_grouping_max_hour, minute=0, second=0,
+            )
+        else:
+            date_end = date.replace(
+                day=date.day + 1, hour=pt.mo_grouping_max_hour, minute=0,
+                second=0,
+            )
+        date_start = date_end - relativedelta(days=pt.mo_grouping_interval)
+        domain += [
+            ('date_planned_finished', '>',
+             fields.Datetime.to_string(date_start)),
+            ('date_planned_finished', '<=',
+             fields.Datetime.to_string(date_end)),
+        ]
+        return domain
+
+    def _find_grouping_target(self, vals):
+        """Return the matching order for grouping.
+
+        :param vals: Values dictionary of the MO to be created.
+
+        :return: Target manufacturing order record (or empty record).
+        """
+        return self.env['mrp.production'].search(
+            self._get_grouping_target_domain(vals), limit=1,
+        )
 
     @api.model
     def create(self, vals):
