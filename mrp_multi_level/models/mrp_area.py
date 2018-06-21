0e350a46402fd731a48698b174bcfcfdca139c35
Lois Rilo <lois.rilo@eficent.com>
2018-06-21 12:05:41 +0200

    [11.0][REN] multi_level_mrp -> mrp_multi_level

---
+++ mrp_multi_level/models/mrp_area.py
@@ -0,0 +1,19 @@
+# © 2016 Ucamco - Wim Audenaert <wim.audenaert@ucamco.com>
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# - Jordi Ballester Alomar <jordi.ballester@eficent.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class MrpArea(models.Model):
+    _name = 'mrp.area'
+ 
+    name = fields.Char('Name')
+    warehouse_id = fields.Many2one(
+        comodel_name='stock.warehouse', string='Warehouse',
+        required=True)
+    location_id = fields.Many2one(
+        comodel_name='stock.location', string='Location',
+        required=True)
+    active = fields.Boolean(default=True)
