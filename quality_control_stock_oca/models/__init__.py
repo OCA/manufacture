a599afb20c7d5c652d426d6daf22f14bf2fbd70e
Simone Rubino <simone.rubino@agilebg.com>
2018-06-27 10:09:05 +0200

    [MIG] quality_control_stock: Migration to 10.0

--- quality_control_stock_oca/models/__init__.py
+++ quality_control_stock_oca/models/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-# (c) 2014 Serv. Tec. Avanzados - Pedro M. Baeza
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import qc_trigger
 from . import qc_inspection
