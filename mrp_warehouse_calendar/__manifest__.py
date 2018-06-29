43a363ff5175254ee963c4aec9ba6caddaecc684
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-06-29 15:45:49 +0200

    11.0 add mrp_warehouse_calendar

---
+++ mrp_warehouse_calendar/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "MRP Warehouse Calendar",
+    "summary": "Considers the warehouse calendars in manufacturing",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/stock-logistics-warehouse",
+    "author": "Eficent, "
+              "Odoo Community Association (OCA)",
+    "category": "Manufacturing",
+    "depends": [
+        "mrp",
+        "stock_warehouse_calendar",
+    ],
+    "installable": True,
+    'development_status': 'Beta',
+    'maintainers': ['jbeficent'],
+}
---
+++ mrp_warehouse_calendar/tests/__init__.py
@@ -0,0 +1,1 @@
+from . import test_mrp_warehouse_calendar
