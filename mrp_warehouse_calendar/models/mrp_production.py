7830c043933835382be98c0c361749cace5dc421
Saran L <saranp502@hotmail.com>
2018-08-05 17:15:59 +0200

    [MIG] mrp_warehouse_calendar: Migration to 12.0

--- mrp_warehouse_calendar/models/mrp_production.py
+++ mrp_warehouse_calendar/models/mrp_production.py
@@ -1,7 +1,7 @@
 # Copyright 2018 Eficent Business and IT Consulting Services, S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class MrpProduction(models.Model):
@@ -9,7 +9,7 @@ class MrpProduction(models.Model):
 
     @api.onchange('date_planned_start', 'product_id')
     def onchange_date_planned(self):
-        dt_planned = fields.Datetime.from_string(self.date_planned_start)
+        dt_planned = self.date_planned_start
         warehouse = self.picking_type_id.warehouse_id
         if warehouse.calendar_id and self.product_id.produce_delay:
             date_expected_finished = warehouse.calendar_id.plan_days(
@@ -19,7 +19,7 @@ class MrpProduction(models.Model):
     @api.multi
     def copy(self, default=None):
         mo = super(MrpProduction, self).copy(default=default)
-        dt_planned = fields.Datetime.from_string(mo.date_planned_start)
+        dt_planned = mo.date_planned_start
         warehouse = mo.picking_type_id.warehouse_id
         if warehouse.calendar_id and mo.product_id.produce_delay:
             date_expected = warehouse.calendar_id.plan_days(
