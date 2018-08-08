6a4eaa9cc16c54d9f7c0953495ad87b4a53ec01e
Simone Rubino <simone.rubino@agilebg.com>
2018-08-08 16:29:20 +0200

    [FIX] count_inspections methods should be able to manage recordsets (#293)

--- quality_control_stock/models/stock_production_lot.py
+++ quality_control_stock/models/stock_production_lot.py
@@ -11,24 +11,24 @@ class StockProductionLot(models.Model):
 
     @api.multi
     @api.depends('qc_inspections', 'qc_inspections.state')
-    def _count_inspections(self):
-        self.ensure_one()
-        self.created_inspections = len(self.qc_inspections)
-        self.passed_inspections = len([x for x in self.qc_inspections if
-                                       x.state == 'success'])
-        self.failed_inspections = len([x for x in self.qc_inspections if
-                                       x.state == 'failed'])
-        self.done_inspections = (self.passed_inspections +
-                                 self.failed_inspections)
+    def _compute_count_inspections(self):
+        for lot in self:
+            lot.created_inspections = len(lot.qc_inspections)
+            lot.passed_inspections = \
+                len([x for x in lot.qc_inspections if x.state == 'success'])
+            lot.failed_inspections = \
+                len([x for x in lot.qc_inspections if x.state == 'failed'])
+            lot.done_inspections = \
+                (lot.passed_inspections + lot.failed_inspections)
 
     qc_inspections = fields.One2many(
         comodel_name='qc.inspection', inverse_name='lot', copy=False,
         string='Inspections', help="Inspections related to this lot.")
     created_inspections = fields.Integer(
-        compute="_count_inspections", string="Created inspections")
+        compute="_compute_count_inspections", string="Created inspections")
     done_inspections = fields.Integer(
-        compute="_count_inspections", string="Done inspections")
+        compute="_compute_count_inspections", string="Done inspections")
     passed_inspections = fields.Integer(
-        compute="_count_inspections", string="Inspections OK")
+        compute="_compute_count_inspections", string="Inspections OK")
     failed_inspections = fields.Integer(
-        compute="_count_inspections", string="Inspections failed")
+        compute="_compute_count_inspections", string="Inspections failed")
