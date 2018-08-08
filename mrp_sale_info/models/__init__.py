b7f1a6942c4af12b5b367115cf8c0677c1d7dbc8
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-08-08 16:48:09 +0200

    [MIG] mrp_sale_info: Migration to 11.0

--- mrp_sale_info/models/__init__.py
+++ mrp_sale_info/models/__init__.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# © 2016 Antiun Ingenieria S.L. - Javier Iniesta
+# Copyright 2016 Antiun Ingenieria S.L. - Javier Iniesta
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import mrp_production
-from . import mrp_production_workcenter_line
+from . import mrp_workorder
