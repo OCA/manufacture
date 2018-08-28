738f7f5cf04895fd2452932bb78baa75504e7b88
Florian da Costa <florian.dacosta@akretion.com>
2018-08-28 11:21:29 +0200

    Take draft PO quantities into account for forecast stock in mrp_mto_with_stock

---
+++ mrp_mto_with_stock_purchase/models/mrp_production.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    @api.model
+    def _get_incoming_qty_waiting_validation(self, move):
+        qty = super(MrpProduction, self)._get_incoming_qty_waiting_validation(
+            move)
+        location_ids = self.env['stock.location'].search(
+            [('id', 'child_of', move.location_id.id)])
+        picking_types = self.env['stock.picking.type'].search(
+            [('default_location_dest_id', 'in',
+              location_ids.ids)])
+        orders = self.env['purchase.order'].search(
+            [('picking_type_id', 'in', picking_types.ids),
+             ('state', 'in', ['draft', 'sent', 'to approve'])])
+        po_lines = self.env['purchase.order.line'].search(
+            [('order_id', 'in', orders.ids),
+             ('product_qty', '>', 0.0),
+             ('product_id', '=', move.product_id.id)])
+        for line in po_lines:
+            qty_uom = line.product_uom._compute_quantity(
+                line.product_qty, move.product_uom)
+            qty += qty_uom
+        return qty
---
+++ mrp_mto_with_stock_purchase/readme/CONFIGURE.rst
@@ -0,0 +1,1 @@
+* This module is installed automatically when mrp_mto_with_stock and purchase module are installed
