4fbfc6080ab1218a5941f94cda67c3f54aee3dc5
mreficent <miquel.raich@eficent.com>
2018-08-29 17:35:25 +0200

    [MIG] mrp_bom_component_menu: Migration to 11.0

--- mrp_bom_component_menu/__init__.py
+++ mrp_bom_component_menu/__init__.py
@@ -1,4 +1,1 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
-#                (<http://www.eficent.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
