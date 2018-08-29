e475cf1faa6d11e307de25e3b3a21083606bcdad
mreficent <miquel.raich@eficent.com>
2018-08-29 17:35:25 +0200

    [MIG] mrp_bom_component_menu: Migration to 11.0

--- mrp_bom_component_menu/__manifest__.py
+++ mrp_bom_component_menu/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 #                (<http://www.eficent.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "MRP BOM Component Menu",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Manufacturing",
     "website": "https://github.com/OCA/manufacture",
     "author": "Eficent,"
