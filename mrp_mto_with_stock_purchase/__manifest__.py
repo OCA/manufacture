52a823ec248a85638d87f9b2a6b0511cbf6d35ca
Florian <florian.dacosta@akretion.com>
2018-09-10 16:15:47 +0200

    [10.0][mrp_mto_with_stock] Take draft PO quantities into account for forecast (#301)

---
+++ mrp_mto_with_stock_purchase/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "MRP MTO with Stock Purchase",
+    "summary": "Module needed to make mrp_mto_with_stock module compatible "
+               "with purchase module.",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/manufacture",
+    "category": "Manufacturing",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "mrp_mto_with_stock",
+        "purchase"
+    ],
+    "auto_install": True,
+}
