8dc8023900c7330a7705bc49dbb103e1a0333019
Lois Rilo <lois.rilo@eficent.com>
2018-09-13 15:55:07 +0200

    [11.0][MIG] mrp_production_request

--- mrp_production_request/models/__init__.py
+++ mrp_production_request/models/__init__.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from . import mrp_production_request
 from . import mrp_production
-from . import procurement
+from . import procurement_rule
 from . import product
 from . import stock_move
+from . import stock_warehouse_orderpoint
--- mrp_production_request/models/mrp_production.py
+++ mrp_production_request/models/mrp_production.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -13,7 +12,16 @@ class MrpProduction(models.Model):
         string="Manufacturing Request", copy=False, readonly=True)
 
     def _generate_finished_moves(self):
+        """`move_dest_ids` is a One2many fields in mrp.production, thus we
+        cannot indicate the same destination move in several MOs (which most
+        probably would be the case with MRs).
+        Storing them on the MR and writing them on the finished moves as it
+        would happen if they were present in the MO, is the best workaround
+        without changing the standard data model."""
         move = super(MrpProduction, self)._generate_finished_moves()
-        mr_proc = self.mrp_production_request_id.procurement_id
-        if mr_proc and mr_proc.move_dest_id:
-            move.write({"move_dest_id": mr_proc.move_dest_id.id})
+        request = self.mrp_production_request_id
+        if request and request.move_dest_ids:
+            move.write({
+                'move_dest_ids': [(4, x.id) for x in request.move_dest_ids],
+            })
+        return move
