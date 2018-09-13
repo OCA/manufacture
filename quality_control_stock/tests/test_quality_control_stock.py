260a39a2c58f27cf6811e430edb96f068e9b8f4c
Ignacio Ales <ignacio.ales@guadaltech.es>
2018-09-13 10:57:38 +0200

    [MIG] quality_control_stock: Migration to 11.0

--- quality_control_stock/tests/test_quality_control_stock.py
+++ quality_control_stock/tests/test_quality_control_stock.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Oihane Crucelaegui - AvanzOSC
 # Copyright 2018 Simone Rubino - Agile Business Group
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
@@ -14,7 +13,7 @@ class TestQualityControl(TransactionCase):
         self.inspection_model = self.env['qc.inspection']
         self.qc_trigger_model = self.env['qc.trigger']
         self.picking_type_model = self.env['stock.picking.type']
-        self.product = self.env.ref('product.product_product_4')
+        self.product = self.env.ref('product.product_product_2')
         self.partner1 = self.env.ref('base.res_partner_2')
         self.partner2 = self.env.ref('base.res_partner_4')
         self.test = self.env.ref('quality_control.qc_test_1')
@@ -29,7 +28,7 @@ class TestQualityControl(TransactionCase):
             'inspection_lines': inspection_lines,
         })
         self.trigger = self.qc_trigger_model.search(
-            [('picking_type', '=', self.picking_type.id)])
+            [('picking_type_id', '=', self.picking_type.id)])
         self.lot = self.env['stock.production.lot'].create({
             'name': 'Lot for tests',
             'product_id': self.product.id,
@@ -41,6 +40,7 @@ class TestQualityControl(TransactionCase):
             'product_uom_qty': 2.0,
             'location_id': self.picking_type.default_location_src_id.id,
             'location_dest_id': self.location_dest.id,
+            'quantity_done': 1.0
         }
         self.picking1 = self.picking_model \
             .with_context(default_picking_type_id=self.picking_type.id) \
@@ -52,15 +52,22 @@ class TestQualityControl(TransactionCase):
             })
         self.picking1.action_confirm()
         self.picking1.force_assign()
-        self.picking1.do_prepare_partial()
-        for line in self.picking1.pack_operation_ids.filtered(
+        sequence = 10
+        for line in self.picking1.move_lines.filtered(
                 lambda r: r.product_id == self.product):
             line.write({
-                'pack_lot_ids': [(0, 0, {
+                'move_line_ids': [(0, 0, {
                     'lot_id': self.lot.id,
-                    'qty': 2.0
-                })]
+                    'product_uom_qty': 1.0,
+                    'qty_done': 1.0,
+                    'product_uom_id': line.product_uom.id,
+                    'product_id': line.product_id.id,
+                    'location_id': line.location_id.id,
+                    'location_dest_id': line.location_dest_id.id,
+                })],
+                'sequence': sequence
             })
+            sequence += 10
 
     def test_inspection_create_for_product(self):
         self.product.qc_triggers = [(
@@ -72,7 +79,7 @@ class TestQualityControl(TransactionCase):
         self.picking1.do_transfer()
         self.assertEqual(self.picking1.created_inspections, 1,
                          'Only one inspection must be created')
-        inspection = self.picking1.qc_inspections[:1]
+        inspection = self.picking1.qc_inspections_ids[:1]
         self.assertEqual(inspection.qty, 2.0)
         self.assertEqual(inspection.test, self.test,
                          'Wrong test picked when creating inspection.')
@@ -91,7 +98,7 @@ class TestQualityControl(TransactionCase):
         self.picking1.do_transfer()
         self.assertEqual(self.picking1.created_inspections, 1,
                          'Only one inspection must be created')
-        self.assertEqual(self.picking1.qc_inspections[:1].test, self.test,
+        self.assertEqual(self.picking1.qc_inspections_ids[:1].test, self.test,
                          'Wrong test picked when creating inspection.')
 
     def test_inspection_create_for_category(self):
@@ -104,7 +111,7 @@ class TestQualityControl(TransactionCase):
         self.picking1.do_transfer()
         self.assertEqual(self.picking1.created_inspections, 1,
                          'Only one inspection must be created')
-        self.assertEqual(self.picking1.qc_inspections[:1].test, self.test,
+        self.assertEqual(self.picking1.qc_inspections_ids[:1].test, self.test,
                          'Wrong test picked when creating inspection.')
 
     def test_inspection_create_for_product_partner(self):
@@ -118,7 +125,7 @@ class TestQualityControl(TransactionCase):
         self.picking1.do_transfer()
         self.assertEqual(self.picking1.created_inspections, 1,
                          'Only one inspection must be created')
-        self.assertEqual(self.picking1.qc_inspections[:1].test, self.test,
+        self.assertEqual(self.picking1.qc_inspections_ids[:1].test, self.test,
                          'Wrong test picked when creating inspection.')
 
     def test_inspection_create_for_template_partner(self):
@@ -132,7 +139,7 @@ class TestQualityControl(TransactionCase):
         self.picking1.do_transfer()
         self.assertEqual(self.picking1.created_inspections, 1,
                          'Only one inspection must be created')
-        self.assertEqual(self.picking1.qc_inspections[:1].test, self.test,
+        self.assertEqual(self.picking1.qc_inspections_ids[:1].test, self.test,
                          'Wrong test picked when creating inspection.')
 
     def test_inspection_create_for_category_partner(self):
@@ -146,7 +153,7 @@ class TestQualityControl(TransactionCase):
         self.picking1.do_transfer()
         self.assertEqual(self.picking1.created_inspections, 1,
                          'Only one inspection must be created')
-        self.assertEqual(self.picking1.qc_inspections[:1].test, self.test,
+        self.assertEqual(self.picking1.qc_inspections_ids[:1].test, self.test,
                          'Wrong test picked when creating inspection.')
 
     def test_inspection_create_for_product_wrong_partner(self):
@@ -201,11 +208,11 @@ class TestQualityControl(TransactionCase):
         self.picking1.do_transfer()
         self.assertEqual(self.picking1.created_inspections, 1,
                          'Only one inspection must be created')
-        self.assertEqual(self.picking1.qc_inspections[:1].test, self.test,
+        self.assertEqual(self.picking1.qc_inspections_ids[:1].test, self.test,
                          'Wrong test picked when creating inspection.')
         self.assertEqual(self.lot.created_inspections, 1,
                          'Only one inspection must be created')
-        self.assertEqual(self.lot.qc_inspections[:1].test, self.test,
+        self.assertEqual(self.lot.qc_inspections_ids[:1].test, self.test,
                          'Wrong test picked when creating inspection.')
 
     def test_picking_type(self):
@@ -215,7 +222,7 @@ class TestQualityControl(TransactionCase):
             'sequence_id': self.sequence.id
         })
         trigger = self.qc_trigger_model.search(
-            [('picking_type', '=', picking_type.id)])
+            [('picking_type_id', '=', picking_type.id)])
         self.assertEqual(len(trigger), 1,
                          'One trigger must have been created.')
         self.assertEqual(trigger.name, picking_type.name,
@@ -228,34 +235,37 @@ class TestQualityControl(TransactionCase):
 
     def test_qc_inspection_picking(self):
         self.inspection1.write({
+            'name': self.picking1.move_lines[:1]._name + "inspection",
             'object_id': '%s,%d' % (self.picking1._name,
                                     self.picking1.id),
         })
-        self.assertEquals(self.inspection1.picking,
-                          self.picking1)
+        self.assertEqual(self.inspection1.picking_id,
+                         self.picking1)
 
     def test_qc_inspection_stock_move(self):
         self.inspection1.write({
+            'name': self.picking1.move_lines[:1]._name + "inspection",
             'object_id': '%s,%d' % (self.picking1.move_lines[:1]._name,
                                     self.picking1.move_lines[:1].id),
         })
         self.inspection1.onchange_object_id()
-        self.assertEquals(self.inspection1.picking,
-                          self.picking1)
-        self.assertEquals(self.inspection1.lot,
-                          self.picking1.move_lines[:1].lot_ids[:1])
-        self.assertEquals(self.inspection1.product,
-                          self.picking1.move_lines[:1].product_id)
-        self.assertEquals(self.inspection1.qty,
-                          self.picking1.move_lines[:1].product_qty)
+        self.assertEqual(self.inspection1.picking_id,
+                         self.picking1)
+        self.assertEqual(self.inspection1.lot_id,
+                         self.lot)
+        self.assertEqual(self.inspection1.product_id,
+                         self.picking1.move_lines[:1].product_id)
+        self.assertEqual(self.inspection1.qty,
+                         self.picking1.move_lines[:1].product_qty)
 
     def test_qc_inspection_lot(self):
         self.inspection1.write({
+            'name': self.picking1.move_lines[:1]._name + "inspection",
             'object_id': '%s,%d' % (self.lot._name,
                                     self.lot.id),
         })
         self.inspection1.onchange_object_id()
-        self.assertEquals(self.inspection1.lot,
-                          self.lot)
-        self.assertEquals(self.inspection1.product,
-                          self.lot.product_id)
+        self.assertEqual(self.inspection1.lot_id,
+                         self.lot)
+        self.assertEqual(self.inspection1.product_id,
+                         self.lot.product_id)
