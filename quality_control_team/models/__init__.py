257328f956ef97eb1879a78d8d04280dbe73f872
Lois Rilo <lois.rilo@eficent.com>
2018-10-18 11:37:47 +0200

    [11.0][MIG] quality_control_team

--- quality_control_team/models/__init__.py
+++ quality_control_team/models/__init__.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from . import qc_team
 from . import res_partner
 from . import res_user
