90ca9e948671e47bfd086a5d6daa6956288c357e
Lois Rilo <lois.rilo@eficent.com>
2018-10-24 17:10:56 +0200

    [11.0][MIG] quality_control_issue

--- quality_control_issue/models/__init__.py
+++ quality_control_issue/models/__init__.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from . import stock_scrap
 from . import qc_stage
 from . import qc_problem
