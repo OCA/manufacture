d36f1cfe4709b44d0e864a1a9745391ae813c521
Lois Rilo <lois.rilo@eficent.com>
2018-10-24 10:16:42 +0200

    add aleph objects to copyright headers

--- quality_control_issue/models/qc_issue.py
+++ quality_control_issue/models/qc_issue.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Aleph Objects, Inc. (https://www.alephobjects.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import api, fields, models, _
