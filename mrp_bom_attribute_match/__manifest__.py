9bf60871a27338e8bc1ab6e3362ff3ec0a39f602
Iván Todorovich <ivan.todorovich@gmail.com>
2018-10-25 09:29:17 +0200

    [14.0][UPD] mrp_bom_attribute_match: updates from v15

--- mrp_bom_attribute_match/__manifest__.py
+++ mrp_bom_attribute_match/__manifest__.py
@@ -4,13 +4,10 @@
     "category": "Manufacturing",
     "author": "Ilyas, Ooops, Odoo Community Association (OCA)",
     "summary": "Dynamic BOM component based on product attribute",
-    "depends": ["mrp_account", "web_domain_field"],
+    "depends": ["mrp"],
     "license": "AGPL-3",
     "website": "https://github.com/OCA/manufacture",
     "data": [
         "views/mrp_bom_views.xml",
     ],
-    "qweb": [],
-    "application": False,
-    "installable": True,
 }
