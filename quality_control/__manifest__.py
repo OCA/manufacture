2fcf05cdae23b3d399c12a2e2eeecea04bd30e58
oihane <oihanecruce@gmail.com>
2018-12-19 13:31:14 +0100

    [FIX] quality_control: created new notebook page to avoid activate variants + permissions (#325)

--- quality_control/__manifest__.py
+++ quality_control/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Quality control",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.1.0",
     "category": "Quality control",
     "license": "AGPL-3",
     "summary": "Generic infrastructure for quality tests.",
