cb8718a77b423908aea29b11e41673eee4e19b9e
Adrià Gil Sorribes <adria.gil@eficent.com>
2018-12-21 15:53:36 +0100

    [11.0][ADD] Add the module mrp_production_originating_user

---
+++ mrp_stock_orderpoint_manual_procurement/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MRP Stock Orderpoint Manual Procurement",
+    "summary": "Updates the value of MO Responsible and keeps track"
+               "of changes regarding this field",
+    "version": "11.0.1.0.0",
+    "author": "Eficent, "
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/manufacture",
+    "category": "MRP",
+    "depends": [
+        "mrp",
+        "stock_orderpoint_manual_procurement",
+    ],
+    "data": [],
+    "license": "AGPL-3",
+    'installable': True,
+    'application': False,
+    'auto_install': True
+}
