735146ddeefb1f3ab5d2d2c4f8fe59d7a925f684
Lois Rilo <lois.rilo@eficent.com>
2019-01-15 12:16:22 +0100

    [11.0][REW] mrp_stock_orderpoint_manual_procurement:  * add a specific field `requested_by`.  * receive information by context.  * Add 2 test cases.

--- mrp_stock_orderpoint_manual_procurement/models/procurement.py
+++ mrp_stock_orderpoint_manual_procurement/models/procurement.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2019 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import models
@@ -7,17 +7,12 @@ from odoo import models
 class ProcurementRule(models.Model):
     _inherit = 'procurement.rule'
 
-    def _prepare_mo_vals(self, product_id, product_qty,
-                         product_uom, location_id, name,
-                         origin, values, bom):
-
-        res = super(ProcurementRule, self)._prepare_mo_vals(product_id,
-                                                            product_qty,
-                                                            product_uom,
-                                                            location_id, name,
-                                                            origin, values,
-                                                            bom)
-        requested_uid = values.get('requested_uid', None)
+    def _prepare_mo_vals(self, product_id, product_qty, product_uom,
+                         location_id, name, origin, values, bom):
+        res = super(ProcurementRule, self)._prepare_mo_vals(
+            product_id, product_qty, product_uom, location_id, name,
+            origin, values, bom)
+        requested_uid = self.env.context.get('requested_uid')
         if requested_uid:
-            res.update({'user_id': requested_uid.id})
+            res.update({'requested_by': requested_uid.id})
         return res
