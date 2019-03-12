7e94d7f53d5c931b4be063b56e2ad373042f4f26
Akim Juillerat <akim.juillerat@camptocamp.com>
2019-03-12 16:10:30 +0100

    [MIG] repair_refurbish (mrp_repair_refurbish): Migration to 12.0

---
+++ repair_refurbish/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2016-18 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MRP Repair Refurbish",
+    "summary": "Create refurbished products during repair",
+    "version": "12.0.1.0.1",
+    "category": "Manufacturing",
+    "website": "https://github.com/OCA/manufacture",
+    "author": "Eficent, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        'repair',
+    ],
+    "data": [
+        "views/repair_view.xml",
+        "data/stock_data.xml",
+        "views/product_template_view.xml",
+        "views/product_product_view.xml",
+    ],
+}
