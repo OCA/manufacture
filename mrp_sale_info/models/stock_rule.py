0a601de19d0a5747911ab2bd649d2ac88beb2e80
Ruben Dario Bravo <rubenred18@gmail.com>
2019-04-14 17:02:58 -0400

    [FIX] mrp_sale_info: fix E501

--- mrp_sale_info/models/stock_rule.py
+++ mrp_sale_info/models/stock_rule.py
@@ -8,7 +8,17 @@ from odoo import models
 class StockRule(models.Model):
     _inherit = 'stock.rule'
 
-    def _prepare_mo_vals(self, product_id, product_qty, product_uom, location_id, name, origin, values, bom):
+    def _prepare_mo_vals(
+        self,
+        product_id,
+        product_qty,
+        product_uom,
+        location_id,
+        name,
+        origin,
+        values,
+        bom
+    ):
         res = super(StockRule, self)._prepare_mo_vals(
             product_id,
             product_qty,
