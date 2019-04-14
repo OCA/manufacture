52a3c6b76c812e220b783d5267570a4e37a7491a
Ruben Dario Bravo <rubenred18@gmail.com>
2019-04-14 12:08:57 -0400

    [MIG] mrp_sale_info: Migration to 12.0

--- mrp_sale_info/models/__init__.py
+++ mrp_sale_info/models/__init__.py
@@ -1,5 +1,7 @@
 # Copyright 2016 Antiun Ingenieria S.L. - Javier Iniesta
+# Copyright 2019 Rubén Bravo <rubenred18@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import mrp_production
 from . import mrp_workorder
+from . import stock_rule
