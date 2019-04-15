489be54c95f153f3ec97de6428c8d762d80bd6c6
Ruben Dario Bravo <rubenred18@gmail.com>
2019-04-15 09:34:46 -0400

    [MIG] mrp_bom_component_menu: Migration to 12.0

--- mrp_bom_component_menu/__manifest__.py
+++ mrp_bom_component_menu/__manifest__.py
@@ -1,9 +1,10 @@
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 #                (<http://www.eficent.com>)
+# Copyright 2019 Rubén Bravo <rubenred18@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "MRP BOM Component Menu",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Manufacturing",
     "website": "https://github.com/OCA/manufacture",
     "author": "Eficent,"
