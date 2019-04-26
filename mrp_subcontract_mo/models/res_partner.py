591a3571e0c0ecb902ba7eb8895041a7bc1127b9
remi-filament <30716308+remi-filament@users.noreply.github.com>
2019-04-26 12:52:05 +0200

    Add write function when selecting an existing partner as subcontractor

--- mrp_subcontract_mo/models/res_partner.py
+++ mrp_subcontract_mo/models/res_partner.py
@@ -13,7 +13,7 @@ class ResPartner(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         # Overloads existing method from base module, with the following:
-        # - Automatically creates subcontractor location is partner is a
+        # - Automatically creates subcontractor location if partner is a
         #     subcontractor
 
         # Returns:
@@ -30,3 +30,31 @@ class ResPartner(models.Model):
                     'partner_id': partner.id,
                     })
         return partners
+
+    @api.multi
+    def write(self, vals):
+        # Overloads existing method from base module, with the following:
+        # - Automatically creates subcontractor location iF partner is a
+        #     subcontractor and location does not already exist
+
+        # Returns:
+        #     [partners] -- As per original create method
+        if vals.get('subcontractor') is True:
+            for partner in self:
+                # Check if corresponding location already exists
+                subcos_location = self.env.ref(
+                    "mrp_subcontract_mo.stock_location_subcontractor")
+                # Retrieves selected subcontractor internal location
+                if not self.env['stock.location'].search(
+                        [('location_id', '=', subcos_location.id),
+                         ('usage', '=', 'internal'),
+                         ('partner_id', '=', partner.id)],
+                        count=True) > 0:
+                    # Create corresponding location
+                    self.env['stock.location'].create({
+                        'location_id': subcos_location.id,
+                        'name': partner.name,
+                        'usage': 'internal',
+                        'partner_id': partner.id,
+                        })
+        return super(ResPartner, self).write(vals)
