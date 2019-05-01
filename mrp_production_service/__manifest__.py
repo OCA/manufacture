458e4e48d6c8a84b99df84846076e5acf863c966
Ruben Dario Bravo <rubenred18@gmail.com>
2019-05-01 07:49:12 -0400

    [MIG] mrp_production_service: Migration to 12.0

--- mrp_production_service/__manifest__.py
+++ mrp_production_service/__manifest__.py
@@ -1,11 +1,12 @@
 # Copyright 2016 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
+# Copyright 2019 Rubén Bravo <rubenred18@gmail.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "MRP Production Service",
     "summary": "Creates procurement orders from manufacturing orders, for "
                "services included in the Bill of Materials",
-    "version": "11.0.1.0.1",
+    "version": "12.0.1.0.1",
     "author": "Eficent,"
               "Odoo Community Association (OCA)",
     "website": "https://www.odoo-community.org",
