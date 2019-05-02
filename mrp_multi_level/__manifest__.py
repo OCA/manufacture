503ffb1e92fdf99b49915bf5a54219b29a2dc185
Lois Rilo <lois.rilo@eficent.com>
2019-05-02 13:00:48 +0200

    [11.0][IMP] mrp_multi_level:  * Able to run MRP only for selected areas.  * Clean logging messages.

--- mrp_multi_level/__manifest__.py
+++ mrp_multi_level/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     'name': 'MRP Multi Level',
-    'version': '11.0.2.1.0',
+    'version': '11.0.2.2.0',
     'development_status': 'Beta',
     'license': 'AGPL-3',
     'author': 'Ucamco, '
