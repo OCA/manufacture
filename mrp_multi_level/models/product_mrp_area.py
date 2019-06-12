c1100215bcc5d088b7016f2b0b5a33af02e1103d
Lois Rilo <lois.rilo@eficent.com>
2019-06-12 15:54:55 +0200

    [11.0][FIX] mrp_multi_level: * remove duplicated fields in view * fix issue using this module together with procurement_auto_create_group.

--- mrp_multi_level/models/product_mrp_area.py
+++ mrp_multi_level/models/product_mrp_area.py
@@ -149,7 +149,8 @@ class ProductMRPArea(models.Model):
 
     @api.multi
     def _compute_supply_method(self):
-        group_obj = self.env['procurement.group']
+        group_obj = self.env['procurement.group'].with_context(
+            no_auto_create_group=True)
         for rec in self:
             proc_loc = rec.location_proc_id or rec.mrp_area_id.location_id
             values = {
