f6fc474abd2e919f897ca21b3f95fa4ac4a5d389
mreficent <miquel.raich@eficent.com>
2019-07-03 12:53:43 +0200

    [ADD] mrp_bom_tracking

---
+++ mrp_bom_tracking/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "MRP BoM Tracking",
+    "version": "11.0.1.0.0",
+    "author": "Eficent, Odoo Community Association (OCA)",
+    "summary": "Add track visibility to some fields of mrp boms.",
+    "website": "https://github.com/OCA/manufacture",
+    "category": "Manufacturing",
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "views/bom_template.xml",
+    ],
+    "license": "LGPL-3",
+    'installable': True,
+}
