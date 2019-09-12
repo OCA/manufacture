72bf6b7e0b68b4dd14158f9ea23d48293e6a8ee4
ahenriquez <ahenriquez@eficent.com>
2019-09-12 13:33:18 +0200

    [FIX]repair_refurbish. Set correct destination location in stock move lines

--- repair_refurbish/models/stock_move.py
+++ repair_refurbish/models/stock_move.py
@@ -14,3 +14,16 @@ class StockMove(models.Model):
                 vals['location_dest_id'] = self.env.context[
                     'force_refurbish_location_dest_id']
         return super().create(vals_list)
+
+
+class StockMoveLine(models.Model):
+
+    _inherit = 'stock.move.line'
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        if 'force_refurbish_location_dest_id' in self.env.context:
+            for vals in vals_list:
+                vals['location_dest_id'] = self.env.context[
+                    'force_refurbish_location_dest_id']
+        return super().create(vals_list)
