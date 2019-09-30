05213e3db9936b3e17087a768cb408583196ff12
Lois Rilo <lois.rilo@eficent.com>
2019-09-30 12:01:31 +0200

    [13.0][MIG] mrp_bom_component_menu

--- mrp_bom_component_menu/__manifest__.py
+++ mrp_bom_component_menu/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
-#                (<http://www.eficent.com>)
+# Copyright 2017-19 Eficent Business and IT Consulting Services, S.L.
+#     (<https://www.eficent.com>)
 # Copyright 2019 Rubén Bravo <rubenred18@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "MRP BOM Component Menu",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Manufacturing",
     "website": "https://github.com/OCA/manufacture",
     "author": "Eficent,"
