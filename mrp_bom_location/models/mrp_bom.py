2a27c7a8249fb0019a96ce104d8d145975f13673
sudhir-erpharbor <sudhir@erpharbor.com>
2019-10-11 16:57:21 +0530

    [MIG] Migrated the module mrp_bom_location to v13.

--- mrp_bom_location/models/mrp_bom.py
+++ mrp_bom_location/models/mrp_bom.py
@@ -7,22 +7,15 @@ from odoo import api, fields, models
 class MrpBom(models.Model):
     _inherit = "mrp.bom"
 
-    location_id = fields.Many2one(
-        string='Location',
-        comodel_name='stock.location',
-    )
+    location_id = fields.Many2one(string="Location", comodel_name="stock.location")
 
-    @api.onchange('picking_type_id')
+    @api.onchange("picking_type_id")
     def _onchange_picking_type_id(self):
-        if (self.picking_type_id and
-                self.picking_type_id.default_location_src_id):
+        if self.picking_type_id and self.picking_type_id.default_location_src_id:
             self.location_id = self.picking_type_id.default_location_src_id
 
 
 class MrpBomLine(models.Model):
     _inherit = "mrp.bom.line"
 
-    location_id = fields.Many2one(
-        related='bom_id.location_id',
-        store=True,
-    )
+    location_id = fields.Many2one(related="bom_id.location_id", store=True)
