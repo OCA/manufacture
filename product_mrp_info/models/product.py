73c2f43d3fd94bb411c7f31e1234f369384397fe
sudhir-erpharbor <sudhir@erpharbor.com>
2019-10-11 15:05:54 +0530

    [MIG] product_mrp_info: Migration to 13.0
    Co-Authored-By: Héctor Villarreal Ortega <hector.villarreal@forgeflow.com>

--- product_mrp_info/models/product.py
+++ product_mrp_info/models/product.py
@@ -1,26 +1,26 @@
-# Copyright 2019 Eficent Business and IT Consulting Services S.L.
-# - Lois Rilo Antelo <lois.rilo@eficent.com>
+# Copyright 2019 ForgeFlow S.L.
+# - Lois Rilo Antelo <lois.rilo@forgeflow.com>
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     mo_count = fields.Integer(
-        string="# Manufacturing Orders",
-        compute="_compute_mo_count",
+        string="# Manufacturing Orders", compute="_compute_mo_count",
     )
 
-    @api.multi
     def _compute_mo_count(self):
         read_group_res = self.env["mrp.production"].read_group(
             [("product_id", "in", self.mapped("product_variant_ids").ids)],
-            ["product_id"], ["product_id"])
-        mapped_data = dict(
-            [(data["product_id"][0], data["product_id_count"]) for data in
-             read_group_res])
+            ["product_id"],
+            ["product_id"],
+        )
+        mapped_data = {
+            data["product_id"][0]: data["product_id_count"] for data in read_group_res
+        }
         for rec in self:
             count = 0
             for variant in rec.mapped("product_variant_ids"):
@@ -39,20 +39,19 @@ class ProductProduct(models.Model):
     _inherit = "product.product"
 
     mo_count = fields.Integer(
-        string="# Manufacturing Orders",
-        compute="_compute_mo_count",
+        string="# Manufacturing Orders", compute="_compute_mo_count",
     )
 
     def _compute_mo_count(self):
         read_group_res = self.env["mrp.production"].read_group(
-            [("product_id", "in", self.ids)], ["product_id"], ["product_id"])
-        mapped_data = dict(
-            [(data["product_id"][0], data["product_id_count"]) for data in
-             read_group_res])
+            [("product_id", "in", self.ids)], ["product_id"], ["product_id"]
+        )
+        mapped_data = {
+            data["product_id"][0]: data["product_id_count"] for data in read_group_res
+        }
         for product in self:
             product.mo_count = mapped_data.get(product.id, 0)
 
-    @api.multi
     def action_view_mrp_productions(self):
         product_ids = self.ids
         action = self.env.ref("mrp.act_product_mrp_production").read()[0]
