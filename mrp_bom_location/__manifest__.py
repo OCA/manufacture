6806d5a7272f4090e89406dec972b1f420439073
sudhir-erpharbor <sudhir@erpharbor.com>
2019-10-11 16:57:21 +0530

    [MIG] Migrated the module mrp_bom_location to v13.

--- mrp_bom_location/__manifest__.py
+++ mrp_bom_location/__manifest__.py
@@ -3,18 +3,13 @@
 {
     "name": "MRP BOM Location",
     "summary": "Adds location field to Bill of Materials and its components.",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.1",
     "category": "Manufacture",
     "website": "https://github.com/OCA/manufacture",
     "author": "Eficent, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
-    "depends": [
-        "mrp",
-    ],
-    "data": [
-        "views/mrp_view.xml",
-        "views/report_mrpbomstructure.xml",
-    ],
-    "installable": True
+    "depends": ["mrp"],
+    "data": ["views/mrp_view.xml", "views/report_mrpbomstructure.xml"],
+    "installable": True,
 }
