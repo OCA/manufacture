6c5dc04298b798ff014d971d711e56e672bfbfc0
sudhir-erpharbor <sudhir@erpharbor.com>
2019-10-11 15:05:54 +0530

    [MIG] product_mrp_info: Migration to 13.0
    Co-Authored-By: Héctor Villarreal Ortega <hector.villarreal@forgeflow.com>

--- product_mrp_info/__manifest__.py
+++ product_mrp_info/__manifest__.py
@@ -1,23 +1,19 @@
-# Copyright 2019 Eficent Business and IT Consulting Services S.L.
+# Copyright 2019 ForgeFlow S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Product MRP Info",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "development_status": "Beta",
     "license": "LGPL-3",
-    "author": "Eficent, Odoo Community Association (OCA)",
-    "maintainers": ["lreficent"],
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "maintainers": ["LoisRForgeFlow"],
     "summary": "Adds smart button in product form view linking to "
-               "manufacturing order list.",
+    "manufacturing order list.",
     "website": "https://github.com/OCA/manufacture",
     "category": "Manufacturing",
-    "depends": [
-        "mrp",
-    ],
-    "data": [
-        "views/product_views.xml",
-    ],
+    "depends": ["mrp"],
+    "data": ["views/product_views.xml"],
     "installable": True,
     "application": False,
 }
--- product_mrp_info/tests/test_product_mrp_info.py
+++ product_mrp_info/tests/test_product_mrp_info.py
@@ -1,11 +1,10 @@
-# Copyright 2019 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2019 ForgeFlow S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from odoo.tests.common import TransactionCase
 
 
 class TestProductMrpInfo(TransactionCase):
-
     def setUp(self):
         super().setUp()
         self.pt_obj = self.env["product.template"]
@@ -14,57 +13,66 @@ class TestProductMrpInfo(TransactionCase):
         self.bom_obj = self.env["mrp.bom"]
         self.boml_obj = self.env["mrp.bom.line"]
 
-        self.manufacture_route = self.env.ref(
-            "mrp.route_warehouse0_manufacture")
+        self.manufacture_route = self.env.ref("mrp.route_warehouse0_manufacture")
 
-        self.attribute = self.env["product.attribute"].create({
-            "name": "Test Attribute",
-            "create_variant": "always",
-        })
-        self.value1 = self.env["product.attribute.value"].create({
-            "name": "Value 1",
-            "attribute_id": self.attribute.id,
-        })
-        self.value2 = self.env["product.attribute.value"].create({
-            "name": "Value 2",
-            "attribute_id": self.attribute.id,
-        })
-        self.product = self.pt_obj.create({
-            "name": "Test Template",
-            "attribute_line_ids": [(0, 0, {
-                "attribute_id": self.attribute.id,
-                "value_ids": [(6, 0, [self.value1.id, self.value2.id])]
-            })],
-            "route_ids": [(6, 0, self.manufacture_route.ids)],
-        })
+        self.attribute = self.env["product.attribute"].create(
+            {"name": "Test Attribute", "create_variant": "always"}
+        )
+        self.value1 = self.env["product.attribute.value"].create(
+            {"name": "Value 1", "attribute_id": self.attribute.id}
+        )
+        self.value2 = self.env["product.attribute.value"].create(
+            {"name": "Value 2", "attribute_id": self.attribute.id}
+        )
+        self.product = self.pt_obj.create(
+            {
+                "name": "Test Template",
+                "attribute_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "attribute_id": self.attribute.id,
+                            "value_ids": [(6, 0, [self.value1.id, self.value2.id])],
+                        },
+                    )
+                ],
+                "route_ids": [(6, 0, self.manufacture_route.ids)],
+            }
+        )
         self.variant_1 = self.product.product_variant_ids[0]
         self.variant_2 = self.product.product_variant_ids[1]
-        self.bom = self.bom_obj.create({
-            "product_tmpl_id": self.product.id,
-            "product_qty": 1.0,
-        })
+        self.bom = self.bom_obj.create(
+            {"product_tmpl_id": self.product.id, "product_qty": 1.0}
+        )
         # Create 3 MO's
-        self.mo_1 = self.mo_obj.create({
-            "name": "MO ABC",
-            "product_id": self.variant_1.id,
-            "product_uom_id": self.variant_1.uom_id.id,
-            "product_qty": 2,
-            "bom_id": self.bom.id,
-        })
-        self.mo_2 = self.mo_obj.create({
-            "name": "MO XYZ",
-            "product_id": self.variant_1.id,
-            "product_uom_id": self.variant_1.uom_id.id,
-            "product_qty": 3,
-            "bom_id": self.bom.id,
-        })
-        self.mo_3 = self.mo_obj.create({
-            "name": "MO QWE",
-            "product_id": self.variant_2.id,
-            "product_uom_id": self.variant_2.uom_id.id,
-            "product_qty": 6,
-            "bom_id": self.bom.id,
-        })
+        self.mo_1 = self.mo_obj.create(
+            {
+                "name": "MO ABC",
+                "product_id": self.variant_1.id,
+                "product_uom_id": self.variant_1.uom_id.id,
+                "product_qty": 2,
+                "bom_id": self.bom.id,
+            }
+        )
+        self.mo_2 = self.mo_obj.create(
+            {
+                "name": "MO XYZ",
+                "product_id": self.variant_1.id,
+                "product_uom_id": self.variant_1.uom_id.id,
+                "product_qty": 3,
+                "bom_id": self.bom.id,
+            }
+        )
+        self.mo_3 = self.mo_obj.create(
+            {
+                "name": "MO QWE",
+                "product_id": self.variant_2.id,
+                "product_uom_id": self.variant_2.uom_id.id,
+                "product_qty": 6,
+                "bom_id": self.bom.id,
+            }
+        )
 
     def test_01_mo_counters(self):
         self.assertEqual(len(self.product.product_variant_ids), 2)
