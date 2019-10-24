765e2ab41c00bdc95306a2f570f51a52bb7887d0
David Beal <david.beal@akretion.com>
2019-10-24 11:11:11 +0200

    ADD module mrp_component_consumption_gap

---
+++ mrp_component_consumption_gap/__manifest__.py
@@ -0,0 +1,21 @@
+# © 2019 David BEAL @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{
+    "name": "MRP Component Consumption Gap",
+    "version": "12.0.1.0.0",
+    "category": "MRP",
+    "license": "AGPL-3",
+    "author": "Akretion, "
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/manufacture/tree/12.0",
+    "depends": [
+        "mrp",
+        "bi_sql_editor",
+    ],
+    "data": [
+        "data/query.xml",
+    ],
+    "installable": True,
+}
---
+++ mrp_component_consumption_gap/readme/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+Display a pivot table from product which make appears
+disparity between theoritical component consumption and 
+real component consumption in the manufacturing orders.
+
+This module is particularly usefull in food industry.
