6f8af359d159c8d605aa81941bce847e1b27a744
sudhir-erpharbor <sudhir@erpharbor.com>
2019-11-04 18:46:38 +0530

    [MIG] Migrated mrp_production_putaway_strategy to v13.

--- mrp_production_putaway_strategy/models/mrp_production.py
+++ mrp_production_putaway_strategy/models/mrp_production.py
@@ -12,7 +12,7 @@ class MrpProduction(models.Model):
         location_dest = self.env['stock.location'].browse(vals.get(
             'location_dest_id'))
         product = self.env['product.product'].browse(vals.get('product_id'))
-        location_id = location_dest.get_putaway_strategy(product)
+        location_id = location_dest._get_putaway_strategy(product)
         if location_id:
             vals['location_dest_id'] = location_id.id
         mo = super(MrpProduction, self).create(vals)
@@ -21,5 +21,5 @@ class MrpProduction(models.Model):
                 "Applied Putaway strategy to finished products.\n"
                 "Finished Products Location: %s." %
                 mo.location_dest_id.complete_name)
-            mo.message_post(message, message_type='comment')
+            mo.message_post(body=message, message_type='comment')
         return mo
