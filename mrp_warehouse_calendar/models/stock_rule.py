acd510366a4eb8da6e74e9646a880a74f5a745b5
Lois Rilo <lois.rilo@forgeflow.com>
2019-12-17 12:09:20 +0100

    [IMP] mrp_warehouse_calendar: black, isort

--- mrp_warehouse_calendar/models/stock_rule.py
+++ mrp_warehouse_calendar/models/stock_rule.py
@@ -5,22 +5,19 @@ from odoo import fields, models
 
 
 class StockRule(models.Model):
-    _inherit = 'stock.rule'
+    _inherit = "stock.rule"
 
     def _get_date_planned(self, product_id, values):
-        date_planned = super(StockRule, self)._get_date_planned(
-            product_id, values)
-        picking_type = self.picking_type_id or \
-            values['warehouse_id'].manu_type_id
+        date_planned = super(StockRule, self)._get_date_planned(product_id, values)
+        picking_type = self.picking_type_id or values["warehouse_id"].manu_type_id
         # We force the date planned to be at the beginning of the day.
         # So no work intervals are found in planned date.
-        dt_planned = fields.Datetime.to_datetime(
-            values['date_planned']).replace(hour=0)
+        dt_planned = fields.Datetime.to_datetime(values["date_planned"]).replace(hour=0)
         warehouse = picking_type.warehouse_id
         if warehouse.calendar_id and product_id.produce_delay:
-            lead_days = values['company_id'].manufacturing_lead + \
-                product_id.produce_delay
-            date_expected = warehouse.calendar_id.plan_days(
-                -1 * lead_days, dt_planned)
+            lead_days = (
+                values["company_id"].manufacturing_lead + product_id.produce_delay
+            )
+            date_expected = warehouse.calendar_id.plan_days(-1 * lead_days, dt_planned)
             date_planned = date_expected
         return date_planned
