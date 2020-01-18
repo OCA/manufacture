19a8f73a62d9eb7254833e226f2c220de4d6995f
Laurent Bélorgey <lb@lalieutenante.com>
2020-01-18 15:37:14 +0100

    [MIG] mrp_production_note: Migration to 12.0

--- mrp_production_note/__init__.py
+++ mrp_production_note/__init__.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
 # © 2015 Oihane Crucelaegui - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
 
 from . import models
