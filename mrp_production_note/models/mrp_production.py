efc5e05dbb667e756ddb19437fc617a618c1279e
Laurent Bélorgey <lb@lalieutenante.com>
2020-01-18 15:37:14 +0100

    [MIG] mrp_production_note: Migration to 12.0

--- mrp_production_note/models/mrp_production.py
+++ mrp_production_note/models/mrp_production.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 # © 2015 Oihane Crucelaegui - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import fields, models
 
