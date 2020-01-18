9798f3d2f57f858eb8d6297b94fc1bb99c73f2e6
Laurent Bélorgey <lb@lalieutenante.com>
2020-01-18 22:54:50 +0100

    [MIG] mrp_bom_note: Migration to 12.0

--- mrp_bom_note/__init__.py
+++ mrp_bom_note/__init__.py
@@ -1,4 +1,4 @@
 # © 2015 Oihane Crucelaegui - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
 
 from . import models
