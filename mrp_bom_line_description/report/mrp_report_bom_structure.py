7adae368b772a23bbcac8384dfa917df7294ad4f
Francisco Fernández @PlanetaTIC <ffernandez@planetatic.com>
2020-02-10 16:48:13 +0100

    [FIX] mrp_bom_line_description: fix PDF when product repeated in BoM
    depends on https://github.com/odoo/odoo/pull/34618

--- mrp_bom_line_description/report/mrp_report_bom_structure.py
+++ mrp_bom_line_description/report/mrp_report_bom_structure.py
@@ -17,13 +17,7 @@ class ReportBomStructure(models.AbstractModel):
             res_line['description'] = line and line.description
         return res
 
-    def _get_pdf_line(self, bom_id, product_id=False, qty=1, child_bom_ids=[],
-                      unfolded=False):
-        res = super(ReportBomStructure, self)._get_pdf_line(
-            bom_id, product_id, qty, child_bom_ids, unfolded)
-        lines = self.env['mrp.bom.line'].search([('bom_id', '=', bom_id)])
-        lines = {l.product_id.display_name: l for l in lines}
-        for res_line in res['lines']:
-            line = lines.get(res_line['name'])
-            res_line['description'] = line and line.description
+    def _get_line_vals(self, bom_line):
+        res = super(ReportBomStructure, self)._get_line_vals(bom_line)
+        res['description'] = bom_line.get('description')
         return res
