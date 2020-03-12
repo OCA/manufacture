912b86cba9c2942cf12e2733d60ca9b4e7849213
ps-tubtim <pimolnats@ecosoft.co.th>
2020-03-12 15:47:16 +0700

    [IMP] product_quick_bom: black, isort

--- product_quick_bom/tests/test_quick_bom.py
+++ product_quick_bom/tests/test_quick_bom.py
@@ -1,8 +1,9 @@
 #   Copyright (C) 2015 Akretion (http://www.akretion.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests.common import TransactionCase
 import logging
+
+from odoo.tests.common import TransactionCase
 
 _logger = logging.getLogger(__name__)
 
@@ -15,9 +16,7 @@ class TestQuickBom(TransactionCase):
         self.prd_1 = self.env.ref("mrp.product_product_computer_desk_bolt")
         self.prd_2 = self.env.ref("mrp.product_product_computer_desk_screw")
         self.prd_3 = self.env.ref("mrp.product_product_wood_ply")
-        self.tmpl_4 = self.env.ref(
-            "product.product_delivery_02_product_template"
-        )
+        self.tmpl_4 = self.env.ref("product.product_delivery_02_product_template")
 
     def test_create_bom(self):
         self.tmpl_4.button_create_bom()
