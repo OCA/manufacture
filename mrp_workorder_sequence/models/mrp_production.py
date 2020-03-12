46e066960281907d5e7c245815e30d41280b6b28
ps-tubtim <pimolnats@ecosoft.co.th>
2020-03-12 15:40:50 +0700

    [MIG] mrp_workorder_sequence: Migration to 13.0

--- mrp_workorder_sequence/models/mrp_production.py
+++ mrp_workorder_sequence/models/mrp_production.py
@@ -1,13 +1,12 @@
-# Copyright 2019 Eficent Business and IT Consulting Services S.L.
+# Copyright 2019-20 ForgeFlow S.L. (https://www.forgeflow.com)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import api, models
+from odoo import models
 
 
 class MrpProduction(models.Model):
     _inherit = "mrp.production"
 
-    @api.multi
     def _reset_work_order_sequence(self):
         for rec in self:
             current_sequence = 1
@@ -15,7 +14,6 @@ class MrpProduction(models.Model):
                 work.sequence = current_sequence
                 current_sequence += 1
 
-    @api.multi
     def _generate_workorders(self, exploded_boms):
         res = super()._generate_workorders(exploded_boms)
         self._reset_work_order_sequence()
