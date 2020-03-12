9a2555829d44012ca737814ebd43bab881b1a10e
ps-tubtim <pimolnats@ecosoft.co.th>
2020-03-12 13:31:15 +0700

    [IMP] mrp_production_note: black, isort

--- mrp_production_note/__manifest__.py
+++ mrp_production_note/__manifest__.py
@@ -3,19 +3,15 @@
 
 {
     "name": "Notes in production orders",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Manufacturing",
     "license": "AGPL-3",
     "author": "OdooMRP team, "
-              "AvanzOSC, "
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
-              "Odoo Community Association (OCA)",
+    "AvanzOSC, "
+    "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+    "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/manufacture",
-    "depends": [
-        "mrp",
-    ],
-    "data": [
-        "views/mrp_production_view.xml",
-    ],
+    "depends": ["mrp"],
+    "data": ["views/mrp_production_view.xml"],
     "installable": True,
 }
