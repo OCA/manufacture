2cd087623adabf8456fd07d51162da32973084fe
ps-tubtim <pimolnats@ecosoft.co.th>
2020-03-16 17:32:22 +0700

    [IMP] quality_control: black, isort

--- quality_control_oca/models/qc_trigger.py
+++ quality_control_oca/models/qc_trigger.py
@@ -9,17 +9,19 @@ from odoo import fields, models
 
 
 class QcTrigger(models.Model):
-    _name = 'qc.trigger'
-    _description = 'Quality control trigger'
+    _name = "qc.trigger"
+    _description = "Quality control trigger"
 
-    name = fields.Char(string='Name', required=True,
-                       translate=True)
-    active = fields.Boolean(string='Active', default=True)
+    name = fields.Char(string="Name", required=True, translate=True)
+    active = fields.Boolean(string="Active", default=True)
     company_id = fields.Many2one(
-        comodel_name='res.company', string='Company',
-        default=lambda self: self.env['res.company']._company_default_get(
-            'qc.test'))
+        comodel_name="res.company",
+        string="Company",
+        default=lambda self: self.env["res.company"]._company_default_get("qc.test"),
+    )
     partner_selectable = fields.Boolean(
-        string='Selectable by partner', default=False, readonly=True,
-        help='This technical field is to allow to filter by partner in'
-        ' triggers')
+        string="Selectable by partner",
+        default=False,
+        readonly=True,
+        help="This technical field is to allow to filter by partner in" " triggers",
+    )
