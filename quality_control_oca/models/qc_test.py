5d9e7ba71c44f5a8f0dca4cd445875488d65682e
ps-tubtim <pimolnats@ecosoft.co.th>
2020-03-16 17:32:22 +0700

    [IMP] quality_control: black, isort

--- quality_control_oca/models/qc_test.py
+++ quality_control_oca/models/qc_test.py
@@ -5,7 +5,8 @@
 # Copyright 2017 Simone Rubino - Agile Business Group
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, exceptions, fields, models, _
+from odoo import _, api, exceptions, fields, models
+
 import odoo.addons.decimal_precision as dp
 
 
@@ -13,92 +14,107 @@ class QcTest(models.Model):
     """
     A test is a group of questions along with the values that make them valid.
     """
-    _name = 'qc.test'
-    _description = 'Quality control test'
+
+    _name = "qc.test"
+    _description = "Quality control test"
 
     @api.multi
     def object_selection_values(self):
         return set()
 
-    @api.onchange('type')
+    @api.onchange("type")
     def onchange_type(self):
-        if self.type == 'generic':
+        if self.type == "generic":
             self.object_id = False
 
-    active = fields.Boolean('Active', default=True)
-    name = fields.Char(
-        string='Name', required=True, translate=True)
+    active = fields.Boolean("Active", default=True)
+    name = fields.Char(string="Name", required=True, translate=True)
     test_lines = fields.One2many(
-        comodel_name='qc.test.question', inverse_name='test',
-        string='Questions', copy=True)
+        comodel_name="qc.test.question",
+        inverse_name="test",
+        string="Questions",
+        copy=True,
+    )
     object_id = fields.Reference(
-        string='Reference object', selection=object_selection_values,)
-    fill_correct_values = fields.Boolean(
-        string='Pre-fill with correct values')
+        string="Reference object", selection=object_selection_values,
+    )
+    fill_correct_values = fields.Boolean(string="Pre-fill with correct values")
     type = fields.Selection(
-        [('generic', 'Generic'),
-         ('related', 'Related')],
-        string='Type', required=True, default='generic')
-    category = fields.Many2one(
-        comodel_name='qc.test.category', string='Category')
+        [("generic", "Generic"), ("related", "Related")],
+        string="Type",
+        required=True,
+        default="generic",
+    )
+    category = fields.Many2one(comodel_name="qc.test.category", string="Category")
     company_id = fields.Many2one(
-        comodel_name='res.company', string='Company',
-        default=lambda self: self.env['res.company']._company_default_get(
-            'qc.test'))
+        comodel_name="res.company",
+        string="Company",
+        default=lambda self: self.env["res.company"]._company_default_get("qc.test"),
+    )
 
 
 class QcTestQuestion(models.Model):
     """Each test line is a question with its valid value(s)."""
-    _name = 'qc.test.question'
-    _description = 'Quality control question'
-    _order = 'sequence, id'
 
-    @api.constrains('ql_values')
+    _name = "qc.test.question"
+    _description = "Quality control question"
+    _order = "sequence, id"
+
+    @api.constrains("ql_values")
     def _check_valid_answers(self):
         for tc in self:
-            if (tc.type == 'qualitative' and tc.ql_values and
-                    not tc.ql_values.filtered('ok')):
+            if (
+                tc.type == "qualitative"
+                and tc.ql_values
+                and not tc.ql_values.filtered("ok")
+            ):
                 raise exceptions.ValidationError(
-                    _("Question '%s' is not valid: "
-                      "you have to mark at least one value as OK.")
-                    % tc.name_get()[0][1])
+                    _(
+                        "Question '%s' is not valid: "
+                        "you have to mark at least one value as OK."
+                    )
+                    % tc.name_get()[0][1]
+                )
 
-    @api.constrains('min_value', 'max_value')
+    @api.constrains("min_value", "max_value")
     def _check_valid_range(self):
         for tc in self:
-            if tc.type == 'quantitative' and tc.min_value > tc.max_value:
+            if tc.type == "quantitative" and tc.min_value > tc.max_value:
                 raise exceptions.ValidationError(
-                    _("Question '%s' is not valid: "
-                      "minimum value can't be higher than maximum value.")
-                    % tc.name_get()[0][1])
+                    _(
+                        "Question '%s' is not valid: "
+                        "minimum value can't be higher than maximum value."
+                    )
+                    % tc.name_get()[0][1]
+                )
 
-    sequence = fields.Integer(
-        string='Sequence', required=True, default="10")
-    test = fields.Many2one(comodel_name='qc.test', string='Test')
-    name = fields.Char(
-        string='Name', required=True, translate=True)
+    sequence = fields.Integer(string="Sequence", required=True, default="10")
+    test = fields.Many2one(comodel_name="qc.test", string="Test")
+    name = fields.Char(string="Name", required=True, translate=True)
     type = fields.Selection(
-        [('qualitative', 'Qualitative'),
-         ('quantitative', 'Quantitative')], string='Type', required=True)
+        [("qualitative", "Qualitative"), ("quantitative", "Quantitative")],
+        string="Type",
+        required=True,
+    )
     ql_values = fields.One2many(
-        comodel_name='qc.test.question.value', inverse_name="test_line",
-        string='Qualitative values', copy=True)
-    notes = fields.Text(string='Notes')
-    min_value = fields.Float(string='Min',
-                             digits=dp.get_precision('Quality Control'))
-    max_value = fields.Float(string='Max',
-                             digits=dp.get_precision('Quality Control'),)
-    uom_id = fields.Many2one(comodel_name='uom.uom', string='Uom')
+        comodel_name="qc.test.question.value",
+        inverse_name="test_line",
+        string="Qualitative values",
+        copy=True,
+    )
+    notes = fields.Text(string="Notes")
+    min_value = fields.Float(string="Min", digits=dp.get_precision("Quality Control"))
+    max_value = fields.Float(string="Max", digits=dp.get_precision("Quality Control"),)
+    uom_id = fields.Many2one(comodel_name="uom.uom", string="Uom")
 
 
 class QcTestQuestionValue(models.Model):
-    _name = 'qc.test.question.value'
-    _description = 'Possible values for qualitative questions.'
+    _name = "qc.test.question.value"
+    _description = "Possible values for qualitative questions."
 
-    test_line = fields.Many2one(
-        comodel_name="qc.test.question", string="Test question")
-    name = fields.Char(
-        string='Name', required=True, translate=True)
+    test_line = fields.Many2one(comodel_name="qc.test.question", string="Test question")
+    name = fields.Char(string="Name", required=True, translate=True)
     ok = fields.Boolean(
-        string='Correct answer?',
-        help="When this field is marked, the answer is considered correct.")
+        string="Correct answer?",
+        help="When this field is marked, the answer is considered correct.",
+    )
