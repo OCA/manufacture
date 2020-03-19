5258715df7bb048231ec7b30f7768aae0c5604d1
ahenriquez <ahenriquez@eficent.com>
2020-03-19 13:23:01 +0100

    [UPT]rebranding

--- repair_refurbish/models/stock_move.py
+++ repair_refurbish/models/stock_move.py
@@ -1,5 +1,5 @@
-# Copyright 2019 Camptocamp SA
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+# Copyright 2020 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import api, models
 
 
