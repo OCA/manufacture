8d4be106a8d9e97b0d4374c2d862dbc1daa5a712
ahenriquez <ahenriquez@eficent.com>
2020-03-19 13:23:01 +0100

    [UPT]rebranding

--- repair_refurbish/models/product_product.py
+++ repair_refurbish/models/product_product.py
@@ -1,4 +1,4 @@
-# Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
+# Copyright 2020 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
--- repair_refurbish/tests/test_repair_refurbish.py
+++ repair_refurbish/tests/test_repair_refurbish.py
@@ -1,5 +1,5 @@
-# Copyright 2018 Eficent Business and IT Consulting Services S.L.
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# Copyright 2020 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests.common import TransactionCase
 
