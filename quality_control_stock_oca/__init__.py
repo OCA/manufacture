d680f23fd2f08720b614cc357406846fa63f8120
ps-tubtim <pimolnats@ecosoft.co.th>
2020-03-20 15:28:15 +0700

    [IMP] quality_control_stock: black, isort

--- quality_control_stock_oca/__init__.py
+++ quality_control_stock_oca/__init__.py
@@ -9,6 +9,6 @@ from odoo import api, SUPERUSER_ID
 def post_init_hook(cr, registry):
     # Create QC triggers
     env = api.Environment(cr, SUPERUSER_ID, {})
-    picking_type_ids = env['stock.picking.type'].sudo().search([])
+    picking_type_ids = env["stock.picking.type"].sudo().search([])
     for picking_type_id in picking_type_ids:
         picking_type_id.sudo()._create_qc_trigger()
--- quality_control_stock_oca/models/stock_picking_type.py
+++ quality_control_stock_oca/models/stock_picking_type.py
@@ -6,18 +6,18 @@ from odoo import api, models
 
 
 class StockPickingType(models.Model):
-    _inherit = 'stock.picking.type'
+    _inherit = "stock.picking.type"
 
     @api.multi
     def _create_qc_trigger(self):
         for picking_type in self:
             qc_trigger = {
-                'name': picking_type.name,
-                'company_id': picking_type.warehouse_id.company_id.id,
-                'picking_type_id': picking_type.id,
-                'partner_selectable': True
+                "name": picking_type.name,
+                "company_id": picking_type.warehouse_id.company_id.id,
+                "picking_type_id": picking_type.id,
+                "partner_selectable": True,
             }
-            self.env['qc.trigger'].sudo().create(qc_trigger)
+            self.env["qc.trigger"].sudo().create(qc_trigger)
 
     @api.model_create_multi
     def create(self, val_list):
@@ -28,10 +28,11 @@ class StockPickingType(models.Model):
     @api.multi
     def write(self, vals):
         res = super(StockPickingType, self).write(vals)
-        if vals.get('name') or vals.get('warehouse_id'):
-            qc_trigger_model = self.env['qc.trigger'].sudo()
+        if vals.get("name") or vals.get("warehouse_id"):
+            qc_trigger_model = self.env["qc.trigger"].sudo()
             for rec in self:
                 qc_triggers = qc_trigger_model.search(
-                    [('picking_type_id', '=', rec.id)])
-                qc_triggers.write({'name': rec.name})
+                    [("picking_type_id", "=", rec.id)]
+                )
+                qc_triggers.write({"name": rec.name})
         return res
