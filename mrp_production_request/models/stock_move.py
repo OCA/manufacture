6c2f6fe827576b5d14aac8cee8d75f147041fbf3
Chandresh Thakkar <chandresh.thakkar.serpentcs@gmail.com>
2020-04-03 16:51:57 +0530

    [ADD] Added module with pre-commit changes only.

--- mrp_production_request/models/stock_move.py
+++ mrp_production_request/models/stock_move.py
@@ -8,15 +8,13 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     created_mrp_production_request_id = fields.Many2one(
-        comodel_name='mrp.production.request',
-        string='Created Production Request',
+        comodel_name="mrp.production.request", string="Created Production Request",
     )
 
     @api.model
     def create(self, vals):
-        if 'production_id' in vals:
-            production = self.env['mrp.production'].browse(
-                vals['production_id'])
+        if "production_id" in vals:
+            production = self.env["mrp.production"].browse(vals["production_id"])
             if production.mrp_production_request_id:
-                vals['propagate'] = False
+                vals["propagate"] = False
         return super().create(vals)
