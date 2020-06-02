636904241ef7c4d50d7398c4bdb260025eb583b0
Matt Taylor <matt454357@gmail.com>
2020-06-02 12:41:01 -0600

    [FIX] mrp_multi_level: Check BoM Line Attrs
    add a call to mrp.bom.line._skip_bom_line() method
    fixes #505

--- mrp_multi_level/tests/test_mrp_multi_level.py
+++ mrp_multi_level/tests/test_mrp_multi_level.py
@@ -309,3 +309,30 @@ class TestMrpMultiLevel(TestMrpMultiLevelCommon):
         self.assertEqual(mrp_invs[0].to_procure, 130)
         # Net needs = 18, available on-hand = 3 -> 15
         self.assertEqual(mrp_invs[1].to_procure, 15)
+
+    def test_12_bom_line_attribute_value_skip(self):
+        """Check for the correct demand on components of a product with
+        multiple variants"""
+        product_4b_demand = self.mrp_inventory_obj.search(
+            [("product_mrp_area_id.product_id", "=", self.product_4b.id)]
+        )
+        self.assertTrue(product_4b_demand)
+        self.assertTrue(product_4b_demand.to_procure)
+        # No demand or supply for AV-12 or AV-21
+        av_12_supply = self.mrp_inventory_obj.search(
+            [("product_mrp_area_id.product_id", "=", self.av_12.id)]
+        )
+        self.assertFalse(av_12_supply)
+        av_21_supply = self.mrp_inventory_obj.search(
+            [("product_mrp_area_id.product_id", "=", self.av_21.id)]
+        )
+        self.assertFalse(av_21_supply)
+        # Supply for AV-11 and AV-22
+        av_11_supply = self.mrp_inventory_obj.search(
+            [("product_mrp_area_id.product_id", "=", self.av_11.id)]
+        )
+        self.assertTrue(av_11_supply)
+        av_22_supply = self.mrp_inventory_obj.search(
+            [("product_mrp_area_id.product_id", "=", self.av_22.id)]
+        )
+        self.assertTrue(av_22_supply)
