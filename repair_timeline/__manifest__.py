801e155a7f68a3dd82c9ae4df6efbb725bb41103
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-08-28 20:09:17 -0300

    [ADD] repair_timeline

---
+++ repair_timeline/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2021 - TODAY, Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Repair Timeline',
+    'summary': """
+        Add timeline view""",
+    'category': 'Manufacturing',
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo,Odoo Community Association (OCA)',
+    'maintainers': ['marcelsavegnago'],
+    'website': 'https://github.com/oca/manufacture',
+    'images': ['static/description/banner.png'],
+    'depends': [
+        'base_repair',
+        'web_timeline'
+    ],
+    'data': [
+        'views/repair_order.xml'
+    ],
+}
