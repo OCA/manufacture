c88fe3360df15baa9fdac9841c8a441d93d6ba61
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2020-09-21 17:02:17 +0200

    [12.0][ADD] mrp_production_show_post_inventory
    this module removes the need to have debug mode enabled to display
    the post inventory button on manufacturing orders

---
+++ mrp_production_show_post_inventory/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 Camptocamp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Production Show Post Inventory',
+    'version': '12.0.1.0.0',
+    'category': 'MRP',
+    'author': 'Camptocamp, '
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/oca/manufacture',
+    'license': 'AGPL-3',
+    'depends': [
+        'mrp',
+    ],
+    'data': [
+        'views/mrp_production.xml',
+    ],
+    'installable': True,
+}
