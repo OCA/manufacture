ccaddbd3ec023ef0fd7d726b690b623127505510
David Beal <david.beal@akretion.com>
2020-10-17 19:02:26 +0200

    mrp_work_hierachical: pre-commit execution

--- mrp_workcenter_hierarchical/model/workcenter.py
+++ mrp_workcenter_hierarchical/model/workcenter.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2016 Akretion (http://www.akretion.com)
 # David BEAL <david.beal@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -7,30 +6,30 @@ from openerp import api, fields, models
 
 
 class MrpWorkcenter(models.Model):
-    _inherit = 'mrp.workcenter'
-    _order = 'parent_left'
+    _inherit = "mrp.workcenter"
+    _order = "parent_left"
     _parent_store = True
 
-    parent_id = fields.Many2one('mrp.workcenter', string='Parent')
-    child_ids = fields.One2many(
-        'mrp.workcenter',
-        'parent_id',
-        string='Children')
+    parent_id = fields.Many2one("mrp.workcenter", string="Parent")
+    child_ids = fields.One2many("mrp.workcenter", "parent_id", string="Children")
     parent_level_1_id = fields.Many2one(
-        'mrp.workcenter',
-        compute='_compute_parent_level',
-        string='Parent Level 1',
-        store=True)
+        "mrp.workcenter",
+        compute="_compute_parent_level",
+        string="Parent Level 1",
+        store=True,
+    )
     parent_level_2_id = fields.Many2one(
-        'mrp.workcenter',
-        compute='_compute_parent_level',
-        string='Parent Level 2',
-        store=True)
+        "mrp.workcenter",
+        compute="_compute_parent_level",
+        string="Parent Level 2",
+        store=True,
+    )
     parent_level_3_id = fields.Many2one(
-        'mrp.workcenter',
-        compute='_compute_parent_level',
-        string='Parent Level 3',
-        store=True)
+        "mrp.workcenter",
+        compute="_compute_parent_level",
+        string="Parent Level 3",
+        store=True,
+    )
     parent_left = fields.Integer(select=True)
     parent_right = fields.Integer(select=True)
 
@@ -45,18 +44,17 @@ class MrpWorkcenter(models.Model):
         return ids
 
     @api.multi
-    @api.depends('parent_id.parent_id.parent_id', 'child_ids')
+    @api.depends("parent_id.parent_id.parent_id", "child_ids")
     def _compute_parent_level(self):
         def get_next_level(parent_ids, workcenter):
-            return parent_ids and parent_ids.pop(0) or (
-                workcenter.child_ids and workcenter.id
-                or workcenter.parent_id.id)
+            return (
+                parent_ids
+                and parent_ids.pop(0)
+                or (workcenter.child_ids and workcenter.id or workcenter.parent_id.id)
+            )
 
         for workcenter in self:
             parent_ids = workcenter._get_parent_ids()
-            workcenter.parent_level_1_id = get_next_level(
-                parent_ids, workcenter)
-            workcenter.parent_level_2_id = get_next_level(
-                parent_ids, workcenter)
-            workcenter.parent_level_3_id = get_next_level(
-                parent_ids, workcenter)
+            workcenter.parent_level_1_id = get_next_level(parent_ids, workcenter)
+            workcenter.parent_level_2_id = get_next_level(parent_ids, workcenter)
+            workcenter.parent_level_3_id = get_next_level(parent_ids, workcenter)
