b3e2f4761e6b308779b22b7fa23007d34bc2493e
Marc Poch <mpoch@planetatic.com>
2020-12-09 11:43:12 +0100

    [ADD] mrp_production_unreserve:line Permits to unreserve lines of mrp.production

---
+++ mrp_production_unreserve_line/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2020 PlanetaTIC - Marc Poch <mpoch@planetatic.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{
+    'name': 'MRP Production Unreserve Line',
+    'summary': 'Permits to unreserve lines of mrp production',
+    'version': '12.0.1.0.0',
+    'category': 'Stock',
+    'author': 'PlanetaTIC'
+              ' Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/stock-logistics-workflow',
+    'license': 'AGPL-3',
+    'depends': [
+        'mrp',
+        'stock_picking_unreserve_line',
+    ],
+    'data': [
+        'views/stock_move_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False
+}
