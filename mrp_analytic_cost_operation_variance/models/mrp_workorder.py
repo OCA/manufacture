6c024425c23abc30489794a20bff122d067248fa
Chandresh Thakkar <chandresh.thakkar.serpentcs@gmail.com>
2020-12-31 01:59:08 +0530

    [IMP] Stored original duration estimation in a new field.

--- mrp_analytic_cost_operation_variance/models/mrp_workorder.py
+++ mrp_analytic_cost_operation_variance/models/mrp_workorder.py
@@ -8,3 +8,13 @@ class MRPWorkorder(models.Model):
     _inherit = "mrp.workorder"
 
     duration_estimated = fields.Float(string="Duration (Estimated)", readonly=True)
+
+
+class MrpProduction(models.Model):
+    _inherit = "mrp.production"
+
+    def action_confirm(self):
+        res = super(MrpProduction, self).action_confirm()
+        for order in self.workorder_ids:
+            order.duration_estimated = order.duration_expected
+        return res
