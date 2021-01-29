93e9bed1042860841e5d373639d255d334a5a9ad
davidborromeo <david.borromeo@forgeflow.com>
2021-01-29 10:24:50 +0100

    [IMP] mrp_warehouse_calendar: black, isort, prettier

--- mrp_warehouse_calendar/__manifest__.py
+++ mrp_warehouse_calendar/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": "Considers the warehouse calendars in manufacturing",
     "version": "13.0.1.0.0",
     "license": "LGPL-3",
-    "website": "https://github.com/manufacture",
+    "website": "https://github.com/OCA/manufacture",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "category": "Manufacturing",
     "depends": ["mrp", "stock_warehouse_calendar"],
