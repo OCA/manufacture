881320bae017dc3f95a477f459b4580dbd6a91d9
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-02-02 23:10:20 -0300

    [ADD] base_repair_config

---
+++ base_repair_config/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 - TODAY, Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Base Repair Config',
+    'summary': """
+        Provides general settings for the Maintenance App""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo,Odoo Community Association (OCA)',
+    'maintainers': ['marcelsavegnago'],
+    'images': ['static/description/banner.png'],
+    'website': 'https://github.com/oca/manufacture',
+    'depends': [
+        'repair',
+    ],
+    'data': [
+        'views/res_config_settings.xml',
+    ]
+}
