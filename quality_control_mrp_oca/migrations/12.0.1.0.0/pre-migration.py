13b68ba371d02408888db93c2b6d1d645644d000
Carlos Roca <carlos.roca@tecnativa.com>
2021-02-23 16:33:38 +0100

    quality_control_mrp: black, isort

--- quality_control_mrp_oca/migrations/12.0.1.0.0/pre-migration.py
+++ quality_control_mrp_oca/migrations/12.0.1.0.0/pre-migration.py
@@ -1,10 +1,9 @@
 from openupgradelib import openupgrade
 
 field_renames = [
-    ("mrp.production", "mrp_production", "qc_inspections",
-     "qc_inspections_ids"),
+    ("mrp.production", "mrp_production", "qc_inspections", "qc_inspections_ids"),
     ("qc.inspection", "qc_inspection", "production", "production_id"),
-    ("qc.inspection.line", "qc_inspection_line", "production", "production_id")
+    ("qc.inspection.line", "qc_inspection_line", "production", "production_id"),
 ]
 
 
