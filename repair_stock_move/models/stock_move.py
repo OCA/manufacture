bc193218917d9cc0a50341220556fd34fd5bbf74
Mateu Griful <mateu.griful@forgeflow.com>
2021-03-01 15:22:24 +0100

    [ADD] repair_stock_move

---
+++ repair_stock_move/models/stock_move.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2021 ForgeFlow S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+
+from odoo import fields, models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    repair_line_id = fields.Many2one(
+        comodel_name="repair.line",
+        string="Repair Line",
+    )
