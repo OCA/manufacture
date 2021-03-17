10a02c3edf76b4d0ada1a9ae7ffd1559bea18d28
Lois Rilo <lois.rilo@forgeflow.com>
2021-03-17 11:03:41 +0100

    [FIX] correct manifest website links

--- mrp_planned_order_matrix/__manifest__.py
+++ mrp_planned_order_matrix/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": "Allows to create fixed planned orders on a grid view.",
     "version": "13.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "website": "https://github.com/OCA/manufacture",
     "category": "Warehouse Management",
     "depends": ["mrp_multi_level", "web_widget_x2many_2d_matrix", "date_range"],
     "data": ["wizards/mrp_planned_order_wizard_view.xml"],
