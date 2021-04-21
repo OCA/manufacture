8398d83acff2d10758b822afa129896b00591545
Kevin Khao <kevinkhao@gmail.com>
2021-04-21 14:20:33 +0200

    [IMP] mrp_sale_info: black, isort, prettier

--- mrp_sale_info/tests/test_mrp_sale_info.py
+++ mrp_sale_info/tests/test_mrp_sale_info.py
@@ -8,28 +8,38 @@ class TestMrpSaleInfo(common.SavepointCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.product = cls.env["product.product"].create({
-            "name": "Test mrp_sale_info product",
-            "type": "product",
-            "route_ids": [
-                (4, cls.env.ref("mrp.route_warehouse0_manufacture").id),
-                (4, cls.env.ref("stock.route_warehouse0_mto").id),
-            ],
-        })
-        cls.bom = cls.env["mrp.bom"].create({
-            "product_tmpl_id": cls.product.product_tmpl_id.id,
-        })
+        cls.product = cls.env["product.product"].create(
+            {
+                "name": "Test mrp_sale_info product",
+                "type": "product",
+                "route_ids": [
+                    (4, cls.env.ref("mrp.route_warehouse0_manufacture").id),
+                    (4, cls.env.ref("stock.route_warehouse0_mto").id),
+                ],
+            }
+        )
+        cls.bom = cls.env["mrp.bom"].create(
+            {
+                "product_tmpl_id": cls.product.product_tmpl_id.id,
+            }
+        )
         cls.partner = cls.env["res.partner"].create({"name": "Test client"})
-        cls.sale_order = cls.env["sale.order"].create({
-            "partner_id": cls.partner.id,
-            "order_line": [
-                (0, 0, {
-                    "product_id": cls.product.id,
-                    "product_uom_qty": 1,
-                    "price_unit": 1,
-                }),
-            ]
-        })
+        cls.sale_order = cls.env["sale.order"].create(
+            {
+                "partner_id": cls.partner.id,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": cls.product.id,
+                            "product_uom_qty": 1,
+                            "price_unit": 1,
+                        },
+                    ),
+                ],
+            }
+        )
 
     def test_mrp_sale_info(self):
         prev_productions = self.env["mrp.production"].search([])
