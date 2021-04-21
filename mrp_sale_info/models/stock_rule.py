17dbd125e277a11c728cd54df01d6510ee8fb5e0
Kevin Khao <kevinkhao@gmail.com>
2021-04-21 14:20:33 +0200

    [IMP] mrp_sale_info: black, isort, prettier

--- mrp_sale_info/models/stock_rule.py
+++ mrp_sale_info/models/stock_rule.py
@@ -6,14 +6,23 @@ from odoo import models
 
 
 class StockRule(models.Model):
-    _inherit = 'stock.rule'
+    _inherit = "stock.rule"
 
-    def _prepare_mo_vals(self, product_id, product_qty, product_uom,
-                         location_id, name, origin, values, bom):
+    def _prepare_mo_vals(
+        self,
+        product_id,
+        product_qty,
+        product_uom,
+        location_id,
+        name,
+        origin,
+        values,
+        bom,
+    ):
         res = super()._prepare_mo_vals(
-            product_id, product_qty, product_uom, location_id, name,
-            origin, values, bom
+            product_id, product_qty, product_uom, location_id, name, origin, values, bom
         )
-        res["source_procurement_group_id"] = values.get(
-            "group_id").id if values.get('group_id', False) else False
+        res["source_procurement_group_id"] = (
+            values.get("group_id").id if values.get("group_id", False) else False
+        )
         return res
