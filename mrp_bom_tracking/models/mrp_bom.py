912af977705f3d0e6457c779e07045ccebf06950
cubells <vicent@vcubells.net>
2021-05-06 19:07:25 +0200

    Wrong model reference

--- mrp_bom_tracking/models/mrp_bom.py
+++ mrp_bom_tracking/models/mrp_bom.py
@@ -89,7 +89,7 @@ class MrpBomLine(models.Model):
                         'product_qty') or lines.product_qty
                     product_uom_id = values.get('product_uom_id')
                     if product_uom_id:
-                        product_uom_id = self.env["product.uom"].browse(
+                        product_uom_id = self.env["uom.uom"].browse(
                             product_uom_id)
                     product_uom_id = product_uom_id or lines.product_uom_id
                     bom.message_post_with_view(
