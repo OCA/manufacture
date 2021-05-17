c44a2e4c0ad58b5328fab06a81582e1d02bed129
Chandresh Thakkar OSI <cthakkar@opensourceintegrators.com>
2021-05-17 20:13:09 +0530

    [ADD] product_cost_rollup_to_bom: update the standard cost of a product

---
+++ product_cost_rollup_to_bom/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2021, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import product
+from . import mrp_bom
+from . import res_config_settings
