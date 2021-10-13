726ccec7e8f00b33a7aeea7a592f797eba4e9192
Jordi Ballester <jordi.ballester@forgeflow.com>
2021-10-13 13:55:52 +0200

    improve usage of planned orders. - The description of the planned order includes the topmost requirement that caused the planned order. This makes it easier to trace, for example, what planned orders has a given sales order generated. The description of the planned order is passed on to the Manufacturing Orders / Purchase Orders / Transfers as the origin, so it can be possible to search for PO's / MO's that were originated as a result of a given sales order, for example.
    - The MRP Inventory tree view is improved so as to add a button to allow you to
      jump to the planned orders.

--- mrp_multi_level/wizards/mrp_inventory_procure.py
+++ mrp_multi_level/wizards/mrp_inventory_procure.py
@@ -83,8 +83,8 @@ class MrpInventoryProcure(models.TransientModel):
                     item.qty,
                     item.uom_id,
                     item.location_id,
-                    "MRP: " + str(self.env.user.login),  # name?
-                    "MRP: " + str(self.env.user.login),  # origin?
+                    "MRP: " + item.planned_order_id.name or str(self.env.user.login),
+                    "MRP: " + item.planned_order_id.name or str(self.env.user.login),
                     item.mrp_inventory_id.company_id,
                     values,
                 )
