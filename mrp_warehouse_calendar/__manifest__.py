dedfcc220e84a2424dfaddd7a30cd5ff2e0e1c8d
Joan Mateu Jordi <joan.mateu@forgeflow.com>
2021-10-26 13:54:57 +0200

    [MIG] mrp_warehouse_calendar: Migration to 15.0

--- mrp_warehouse_calendar/__manifest__.py
+++ mrp_warehouse_calendar/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2018-19 ForgeFlow S.L. (https://www.forgeflow.com)
+# Copyright 2021 ForgeFlow S.L. (https://www.forgeflow.com)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "MRP Warehouse Calendar",
     "summary": "Considers the warehouse calendars in manufacturing",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/manufacture",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
