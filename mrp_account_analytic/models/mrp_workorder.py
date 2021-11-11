7fb8c812f9a36884eafe87daf88800e945938eca
Daniel Reis <dreis@opensourceintegrators.com>
2021-11-11 17:51:50 +0000

    [FIX] mrp_account_analytic: set UoM on created Analytic Items

--- mrp_account_analytic/models/mrp_workorder.py
+++ mrp_account_analytic/models/mrp_workorder.py
@@ -8,12 +8,14 @@ from odoo import api, fields, models
 class MrpWorkcenterProductivity(models.Model):
     _inherit = "mrp.workcenter.productivity"
 
-    def _prepare_mrp_workorder_analytic_item(self):
+    def _prepare_mrp_workorder_analytic_item(self, duration):
         """
         Prepare additional values for Analytic Items created.
         For compatibility with analytic_activity_cost
         """
         self.ensure_one()
+        duration_hours = duration / 60
+        amount = duration_hours * self.workcenter_id.costs_hour
         return {
             "name": "{} / {}".format(self.production_id.name, self.workorder_id.name),
             "account_id": self.production_id.analytic_account_id.id,
@@ -21,26 +23,36 @@ class MrpWorkcenterProductivity(models.Model):
             "company_id": self.company_id.id,
             "manufacturing_order_id": self.production_id.id,
             "workorder_id": self.workorder_id.id,
-            "unit_amount": self.duration / 60,  # convert minutes to hours
-            "amount": -self.duration / 60 * self.workcenter_id.costs_hour,
+            "unit_amount": duration_hours,
+            "amount": -amount,
         }
 
-    def generate_mrp_work_analytic_line(self):
-        AnalyticLine = self.env["account.analytic.line"].sudo()
-        for timelog in self:
-            line_vals = timelog._prepare_mrp_workorder_analytic_item()
+    def generate_mrp_work_analytic_line(self, duration):
+        if duration and self.production_id.analytic_account_id:
+            line_vals = self._prepare_mrp_workorder_analytic_item(duration=duration)
+            AnalyticLine = self.env["account.analytic.line"].sudo()
             analytic_line = AnalyticLine.create(line_vals)
             analytic_line.on_change_unit_amount()
 
     @api.model
     def create(self, vals):
         timelog = super().create(vals)
-        if vals.get("date_end"):
-            timelog.generate_mrp_work_analytic_line()
+        if timelog.duration:
+            timelog.generate_mrp_work_analytic_line(duration=timelog.duration)
         return timelog
 
     def write(self, vals):
-        res = super().write(vals)
-        if vals.get("date_end"):
-            self.generate_mrp_work_analytic_line()
-        return res
+        for timelog in self:
+            old_duration = timelog.duration
+            super(MrpWorkcenterProductivity, timelog).write(vals)
+            new_duration = timelog.duration
+            diff_duration = round(new_duration - old_duration, 2)
+            if diff_duration:
+                timelog.generate_mrp_work_analytic_line(duration=diff_duration)
+        return True
+
+    def unlink(self):
+        # Setting time spent to zero will remove Time Logs
+        for timelog in self:
+            timelog.generate_mrp_work_analytic_line(duration=-timelog.duration)
+        return super().unlink()
--- mrp_account_analytic/models/stock_move.py
+++ mrp_account_analytic/models/stock_move.py
@@ -8,75 +8,57 @@ from odoo import api, models
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    def _prepare_mrp_raw_material_analytic_line(self):
+    def _prepare_mrp_raw_material_analytic_line(self, qty):
         """
         Prepare additional values for Analytic Items created.
         """
         self.ensure_one()
-        move = self
-        mrp_order = move.raw_material_production_id
+        mrp_order = self.raw_material_production_id
         return {
-            "date": move.date,
-            "name": "{} / {}".format(mrp_order.name, move.product_id.display_name),
+            "date": self.date,
+            "name": "{} / {}".format(mrp_order.name, self.product_id.display_name),
             "ref": mrp_order.name,
             "account_id": mrp_order.analytic_account_id.id,
             "manufacturing_order_id": mrp_order.id,
             "company_id": mrp_order.company_id.id,
-            "stock_move_id": move.id,
-            "product_id": move.product_id.id,
-            "unit_amount": move.quantity_done,
+            "stock_move_id": self.id,
+            "product_id": self.product_id.id,
+            "product_uom_id": self.product_uom.id,
+            "unit_amount": qty,
         }
 
-    def generate_mrp_raw_analytic_line(self):
+    def generate_mrp_raw_analytic_line(self, qty):
         """
-        Generate Analytic Lines.
-        One Analytic Item for each Stock Move line.
-        If the Stock Move is updated, the existing Analytic Item is updated.
+        Generate Analytic Lines for each consumption
         """
-        AnalyticLine = self.env["account.analytic.line"].sudo()
-        existing_items = AnalyticLine.search([("stock_move_id", "in", self.ids)])
-        for move in self.filtered("raw_material_production_id.analytic_account_id"):
-            line_vals = move._prepare_mrp_raw_material_analytic_line()
-            if move in existing_items.mapped("stock_move_id"):
-                analytic_line = existing_items.filtered(
-                    lambda x: x.stock_move_id == move
-                )
-                analytic_line.write(line_vals)
-                analytic_line.on_change_unit_amount()
-            elif line_vals.get("unit_amount"):
-                analytic_line = AnalyticLine.create(line_vals)
-                analytic_line.on_change_unit_amount()
-
-    def write(self, vals):
-        """When material is consumed, generate Analytic Items"""
-        res = super().write(vals)
-        if vals.get("quantity_done"):
-            self.generate_mrp_raw_analytic_line()
-        return res
-
-    @api.model
-    def create(self, vals):
-        qty_done = vals.get("quantity_done")
-        res = super().create(vals)
-        if qty_done:
-            res.generate_mrp_raw_analytic_line()
-        return res
+        if qty and self.raw_material_production_id.analytic_account_id:
+            line_vals = self._prepare_mrp_raw_material_analytic_line(qty=qty)
+            AnalyticLine = self.env["account.analytic.line"].sudo()
+            analytic_line = AnalyticLine.create(line_vals)
+            analytic_line.on_change_unit_amount()
 
 
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
-    def write(self, vals):
-        qty_done = vals.get("qty_done")
-        res = super().write(vals)
-        if qty_done:
-            self.mapped("move_id").generate_mrp_raw_analytic_line()
-        return res
-
     @api.model
     def create(self, vals):
         qty_done = vals.get("qty_done")
         res = super().create(vals)
-        if qty_done:
-            res.mapped("move_id").generate_mrp_raw_analytic_line()
+        res.move_id.generate_mrp_raw_analytic_line(qty=qty_done)
         return res
+
+    def write(self, vals):
+        for move_line in self:
+            old_qty = move_line.qty_done
+            super(StockMoveLine, move_line).write(vals)
+            new_qty = move_line.qty_done
+            diff_qty = new_qty - old_qty
+            if diff_qty:
+                move_line.move_id.generate_mrp_raw_analytic_line(qty=diff_qty)
+        return True
+
+    def unlink(self):
+        for move_line in self:
+            move_line.move_id.generate_mrp_raw_analytic_line(qty=-move_line.qty_done)
+        return super().unlink()
