0ab2c892904804716419335d5d73fd22aa437b87
clementmbr <clement.mombereau@akretion.com.br>
2021-11-12 17:34:39 +0100

    [IMP] stock_picking_product_kit_helper: black, isort, prettier

--- stock_picking_product_kit_helper/__manifest__.py
+++ stock_picking_product_kit_helper/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Stock Picking Product Kit Helper",
     "summary": "Set quanity in picking line based on product kit quantity",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Stock",
     "website": "https://github.com/OCA/manufacture",
     "author": "Ecosoft, Odoo Community Association (OCA)",
