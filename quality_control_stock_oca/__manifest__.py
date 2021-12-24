33a8e1bae7c2e989324e2a422e859cc0c8856b0a
kpo <kpo@frontware.com>
2021-12-24 14:43:15 +0700

    [add] quality_control_stock_oca

--- quality_control_stock_oca/__manifest__.py
+++ quality_control_stock_oca/__manifest__.py
@@ -1,25 +1,25 @@
 # Copyright 2014 Serv. Tec. Avanzados - Pedro M. Baeza
 # Copyright 2014 Oihane Crucelaegui - AvanzOSC
+# Copyright 2018 Simone Rubino - Agile Business Group
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "MRP extension for quality control (OCA)",
-    "version": "14.0.2.0.0",
+    "name": "Quality control - Stock (OCA)",
+    "version": "13.0.1.0.1",
     "category": "Quality control",
     "license": "AGPL-3",
-    "author": "OdooMRP team, "
-    "AvanzOSC, "
-    "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
-    "Agile Business Group, "
-    "Odoo Community Association (OCA),"
+    "author": "OdooMRP team, AvanzOSC, Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+    "Agile Business Group, Odoo Community Association (OCA),"
     "frontware international",
     "website": "https://github.com/OCA/manufacture",
-    "depends": ["quality_control_oca", "quality_control_stock_oca", "mrp"],
+    "depends": ["quality_control_oca", "stock"],
     "data": [
-        "data/quality_control_mrp_data.xml",
+        "security/ir.model.access.csv",
         "views/qc_inspection_view.xml",
-        "views/mrp_production_view.xml",
+        "views/stock_picking_view.xml",
+        "views/stock_production_lot_view.xml",
     ],
+    "post_init_hook": "post_init_hook",
     "installable": True,
     "auto_install": True,
     "description":"""
