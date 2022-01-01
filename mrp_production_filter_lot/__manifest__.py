9ee8e1c7a467c253a89951d45413edf88742a268
Simone Rubino <simone.rubino88@gmail.com>
2022-01-01 13:59:40 +0100

    [ADD] mrp_production_filter_lot
    Co-authored-by: Simone Rubino <sir@takobi.online>

---
+++ mrp_production_filter_lot/__manifest__.py
@@ -0,0 +1,22 @@
+#  Copyright 2022 Simone Rubino - Takobi
+#  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': "MRP production filter lot",
+    'summary': "In production order production popup, "
+               "filter lots based on their location and availability",
+    'version': '12.0.1.0.0',
+    'category': 'Manufacturing',
+    'website': "https://github.com/OCA/manufacture/tree/"
+               "12.0/mrp_production_filter_lot",
+    'author': "Takobi, "
+              "Odoo Community Association (OCA)",
+    'license': 'AGPL-3',
+    'depends': [
+        'mrp',
+        'stock_picking_filter_lot',
+    ],
+    'auto_install': True,
+    'data': [
+        'wizards/mrp_product_produce_views.xml',
+    ],
+}
---
+++ mrp_production_filter_lot/readme/USAGE.rst
@@ -0,0 +1,5 @@
+To use this module:
+
+#. Produce a production order containing raw materials that are tracked by lots.
+#. In the production popup you will notice that the lots available
+   to select are the lots in the source location of the raw materials that have any availability.
