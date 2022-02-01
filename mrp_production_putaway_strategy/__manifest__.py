bc8a8acfd545f61c9c3e17b3395b44938c0dda55
Joan Mateu Jordi <joan.mateu@forgeflow.com>
2022-02-01 13:46:49 +0100

    [15.0][MIG] mrp_production_putaway_strategy: Migration to 15.0

--- mrp_production_putaway_strategy/__manifest__.py
+++ mrp_production_putaway_strategy/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "MRP Production Putaway Strategy",
     "summary": "Applies putaway strategies to manufacturing orders for "
     "finished products.",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "ForgeFlow, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/manufacture",
     "category": "Manufacture",
