f3a77abe636fb2a49b4ec421fde57feb9bfbfbfb
Manuel Calero <manuelcalerosolis@gmail.com>
2022-02-15 10:16:12 +0100

    [ADD] mrp_cost_lot: black, isort

---
+++ mrp_cost_lot/__manifest__.py
@@ -0,0 +1,22 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "MRP Cost Lot",
+    "version": "14.0.1.0.0",
+    "category": "Manufacturing",
+    "author": "Xtendoo, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/manufacture",
+    "license": "AGPL-3",
+    "maintainers": [
+        "manuelcalero@xtendoo.es",
+    ],
+    "development_status": "Beta",
+    "data": [
+        "views/stock_view.xml",
+    ],
+    "depends": [
+        "stock",
+        "mrp",
+    ],
+    "installable": True,
+}
