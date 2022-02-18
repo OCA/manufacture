44b6c9f11afd08fd59c3ed1f461f655df6411aad
Lois Rilo <lois.rilo@forgeflow.com>
2022-02-18 08:55:20 +0100

    [15.0][MIG] mrp_multi_level_estimate

--- mrp_multi_level_estimate/wizards/mrp_multi_level.py
+++ mrp_multi_level_estimate/wizards/mrp_multi_level.py
@@ -1,4 +1,4 @@
-# Copyright 2019-20 ForgeFlow S.L. (http://www.forgeflow.com)
+# Copyright 2019-22 ForgeFlow S.L. (http://www.forgeflow.com)
 # - Lois Rilo <lois.rilo@forgeflow.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
