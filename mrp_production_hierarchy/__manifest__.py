97abf9fc22900cee30a3cec777127f6e64421166
Sulivan Lominchar <slominchar@archeti.com>
2022-03-28 10:52:09 -0400

    [13.0] mrp_production_hierarchy: black, isort, prettier

--- mrp_production_hierarchy/__manifest__.py
+++ mrp_production_hierarchy/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 ABF OSIELL <http://osiell.com>
 # Copyright 2019 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -12,10 +11,6 @@
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": [
-        "mrp",
-    ],
-    "data": [
-        "views/mrp_production.xml",
-    ],
+    "depends": ["mrp",],
+    "data": ["views/mrp_production.xml",],
 }
--- mrp_production_hierarchy/models/mrp_production.py
+++ mrp_production_hierarchy/models/mrp_production.py
@@ -1,23 +1,29 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 ABF OSIELL <http://osiell.com>
 # Copyright 2019 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 
 
 class MrpProduction(models.Model):
-    _inherit = 'mrp.production'
+    _inherit = "mrp.production"
 
     root_id = fields.Many2one(
-        'mrp.production', u"Root order",
-        index=True, ondelete='restrict', readonly=True)
+        "mrp.production", u"Root order", index=True, ondelete="restrict", readonly=True
+    )
     parent_id = fields.Many2one(
-        'mrp.production', u"Parent order",
-        index=True, ondelete='restrict', readonly=True)
+        "mrp.production",
+        u"Parent order",
+        index=True,
+        ondelete="restrict",
+        readonly=True,
+    )
     child_ids = fields.One2many(
-        'mrp.production', 'parent_id', u"Child orders",
-        domain=[('state', '!=', 'cancel')])
+        "mrp.production",
+        "parent_id",
+        u"Child orders",
+        domain=[("state", "!=", "cancel")],
+    )
 
     @api.multi
     def _generate_moves(self):
@@ -27,7 +33,7 @@ class MrpProduction(models.Model):
         """
         for prod in self:
             # Set the initial root production order ID
-            if not prod.env.context.get('root_mrp_production_id'):
+            if not prod.env.context.get("root_mrp_production_id"):
                 prod = prod.with_context(root_mrp_production_id=self.id)
             # Set the parent production order ID
             prod = prod.with_context(parent_mrp_production_id=self.id)
@@ -39,23 +45,30 @@ class MrpProduction(models.Model):
         self.ensure_one()
         if self.child_ids:
             return {
-                'domain': [('root_id', '=', self.id)],
-                'name': _(u"Hierarchy"),
-                'view_mode': 'tree',
-                'res_model': 'mrp.production',
-                'views': [
-                    (self.env.ref(
-                        'mrp_production_hierarchy.mrp_production_tree_view').id,
-                        'tree'),
-                    (self.env.ref(
-                        'mrp_production_hierarchy.mrp_production_form_view').id,
-                        'form')
+                "domain": [("root_id", "=", self.id)],
+                "name": _(u"Hierarchy"),
+                "view_mode": "tree",
+                "res_model": "mrp.production",
+                "views": [
+                    (
+                        self.env.ref(
+                            "mrp_production_hierarchy.mrp_production_tree_view"
+                        ).id,
+                        "tree",
+                    ),
+                    (
+                        self.env.ref(
+                            "mrp_production_hierarchy.mrp_production_form_view"
+                        ).id,
+                        "form",
+                    ),
                 ],
-                'target': 'current',
-                'type': 'ir.actions.act_window',
-                'context': dict(
+                "target": "current",
+                "type": "ir.actions.act_window",
+                "context": dict(
                     self.env.context,
                     search_default_group_by_root_id=True,
-                    search_default_group_by_parent_id=True),
+                    search_default_group_by_parent_id=True,
+                ),
             }
         return False
