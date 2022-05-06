614dd2349ccc1d0b1ed5d0efd5242ddea7716f02
i1 <ivan@cetmix.com>
2022-05-06 13:19:06 +0200

    [14.0][ADD] Subcontractor Partner Management

---
+++ mrp_subcontracting_partner_management/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Subcontracting Partner Management",
+    "version": "14.0.1.0.0",
+    "summary": "Subcontracting Partner Management",
+    "author": "Ooops404, Cetmix, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "category": "Inventory",
+    "website": "https://github.com/OCA/manufacture",
+    "depends": ["purchase_stock", "mrp_subcontracting", "sale_stock"],
+    "external_dependencies": {},
+    "demo": [],
+    "data": [
+        "views/res_partner.xml",
+    ],
+    "qweb": [],
+    "installable": True,
+    "application": False,
+}
---
+++ mrp_subcontracting_partner_management/readme/USAGE.rst
@@ -0,0 +1,4 @@
+* Select a partner of type "Company"
+* Enable the "Subcontractor" checkbox
+* New entities are created or existing are used if were created previously
+* When disabled all associated enties will be archived
