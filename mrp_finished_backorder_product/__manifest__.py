eda0d7ef1dbe3f4fe7569088400b13ed376f37af
Joan Mateu Jordi <joan.mateu@forgeflow.com>
2022-05-13 12:54:07 +0200

    [ADD][15.0] Add module of mrp_finished_products

---
+++ mrp_finished_backorder_product/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 ForgeFlow S.L. (https://www.forgeflow.com)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "MRP Finished Backorder Product",
+    "version": "15.0.1.0.0",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "summary": "Be able to see the summary of the finished manufactured orders ",
+    "website": "https://github.com/OCA/manufacture",
+    "category": "Manufacturing",
+    "depends": ["mrp"],
+    "data": ["views/mrp_production_views.xml"],
+    "license": "LGPL-3",
+    "installable": True,
+    "development_status": "Beta",
+}
