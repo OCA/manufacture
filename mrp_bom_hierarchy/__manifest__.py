7f99da2b27fe20ea4956e1ccec3676d3d16a1eff
Miquel Raïch <miquel.raich@forgeflow.com>
2022-06-16 16:05:29 +0200

    [MIG/REF] mrp_bom_hierarchy: Refactor/Migration to 14.0

--- mrp_bom_hierarchy/__manifest__.py
+++ mrp_bom_hierarchy/__manifest__.py
@@ -1,35 +1,18 @@
-##############################################################################
-#
-#    Copyright (C) 2015 Eficent (<http://www.eficent.com/>)
-#              <contact@eficent.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2015-22 ForgeFlow S.L. (https://www.forgeflow.com)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
 {
-    "name": "MRP BOM Hierarchy",
-    "version": "1.0",
-    "author": "Eficent, Odoo Community Association (OCA)",
+    "name": "MRP BoM Hierarchy",
+    "summary": "Make it easy to navigate through BoM hierarchy.",
+    "version": "14.0.1.0.0",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
     "category": "Manufacturing",
     "depends": ["mrp"],
     "website": "https://github.com/OCA/manufacture",
     "license": "AGPL-3",
-    "demo": [],
     "data": [
         "view/mrp.xml",
     ],
-    "test": [],
     "installable": True,
     "auto_install": False,
 }
