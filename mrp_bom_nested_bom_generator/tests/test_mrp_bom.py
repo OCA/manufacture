ee569bb1117954d64d3ee91a98435d5824e09e85
geomer198 <geomer198@gmail.com>
2022-08-14 21:18:42 +0300

    [FIX] Changed tests.

--- mrp_bom_nested_bom_generator/tests/test_mrp_bom.py
+++ mrp_bom_nested_bom_generator/tests/test_mrp_bom.py
@@ -11,51 +11,67 @@ class TestMrpBom(TestNestedBomCase):
         self.assertEqual(
             temp_nested_bom._name,
             "mrp.nested.bom",
-            msg="Model name must be equal 'mrp.nested.bom'",
+            msg="Model name must be equal to 'mrp.nested.bom'",
         )
         self.assertEqual(temp_nested_bom.ids, [], msg="Ids field must be empty list")
         self.assertNotEqual(
-            type(temp_nested_bom.id), int, msg="Id field type must be not equal int"
+            type(temp_nested_bom.id), int, msg="Id field type must be not int type"
         )
         self.assertEqual(
             temp_nested_bom.bom_product_tmpl_id,
             self.product_template_pinocchio,
-            msg="Parent product must be equal 'Pinocchio' product template",
+            msg="Parent product must be equal to 'Pinocchio' product template",
         )
         self.assertEqual(
             temp_nested_bom.product_tmpl_id,
             self.product_template_pinocchio,
-            msg="Product template must be equal 'Pinocchio' product template",
+            msg="Product template must be equal to 'Pinocchio' product template",
         )
         self.assertEqual(
-            temp_nested_bom.product_qty, 1, msg="Product Qty must be equal 1"
+            temp_nested_bom.product_qty, 1, msg="Product Qty must be equal to one"
         )
         self.assertEqual(
             temp_nested_bom.uom_id,
             self.product_template_pinocchio.uom_id,
-            msg="Uom must br equal 'Pinocchio' product uom",
+            msg="Uom must br equal to 'Pinocchio' product uom",
         )
 
     def test_group_by_stage(self):
         p0, p1, p2 = list(self.mrp_bom_pinocchio.nested_bom_ids)
         stages = list(self.mrp_bom_pinocchio.group_by_stage())
-        self.assertEqual(len(stages), 3, msg="Stages count must be equal 3")
+        self.assertEqual(len(stages), 3, msg="Stages count must be equal to three")
         f_stage, s_stage, t_stage = stages
         self.assertEqual(
             f_stage[0].product_tmpl_id,
             self.product_template_pinocchio,
-            msg="First stage product template must be equal 'Pinocchio' product template",
+            msg="First stage product template must be equal to 'Pinocchio' product template",
         )
         self.assertEqual(f_stage[1], p0)
         self.assertEqual(s_stage, (p0, p1))
         self.assertEqual(t_stage, (p1, p2))
 
     def test_action_generate_nested_boms_invalid(self):
+        result = self.mrp_bom_pinocchio.action_generate_nested_boms()
+        self.assertTrue(result, msg="Result must be True")
+        result = self.mrp_bom_pinocchio.action_generate_nested_boms()
+        self.assertFalse(result, msg="Result must be False")
         self.mrp_bom_pinocchio.nested_bom_ids.unlink()
         with self.assertRaises(
             UserError, msg="Function must be raises exception UserError"
         ):
             self.mrp_bom_pinocchio.action_generate_nested_boms()
+
+    def test_action_generate_nested_boms_valid(self):
+        result = self.mrp_bom_pinocchio_mrp.action_generate_nested_boms()
+        self.assertTrue(result, msg="Function result must be True")
+        self.assertEqual(
+            len(self.product_template_wood.bom_ids),
+            2,
+            msg="Duplicates count mrp for product template must be equal to two",
+        )
+        self.mrp_bom_pinocchio_mrp.write({"nested_bom_updated": False})
+        result = self.mrp_bom_pinocchio_mrp.action_generate_nested_boms()
+        self.assertFalse(result, msg="Function result must be False")
 
     def test_create_boms_valid(self):
         MrpBom = self.env["mrp.bom"]
@@ -65,7 +81,9 @@ class TestMrpBom(TestNestedBomCase):
         product_tmpl_ids |= self.product_template_pinocchio
         self.mrp_bom_pinocchio.create_boms()
         mrp_bom_ids = MrpBom.search([("product_tmpl_id", "in", product_tmpl_ids.ids)])
-        self.assertEqual(len(mrp_bom_ids), 3, msg="MRP BOM count must be equal 3")
+        self.assertEqual(
+            len(mrp_bom_ids), 3, msg="MRP BOM count must be equal to three"
+        )
 
     def test_create_boms_invalid(self):
         product_tmpl_ids = self.mrp_bom_pinocchio.nested_bom_ids.mapped(
@@ -77,7 +95,9 @@ class TestMrpBom(TestNestedBomCase):
 
         self.mrp_bom_pinocchio.create_boms()
         child_bom_ids = self.mrp_bom_pinocchio.child_bom_ids
-        self.assertEqual(len(child_bom_ids), 0, msg="MRP BOM count must be equal 0")
+        self.assertEqual(
+            len(child_bom_ids), 0, msg="MRP BOM count must be equal to zero"
+        )
 
     def test_unlink_existing_bom(self):
         status = self.mrp_bom_pinocchio.unlink_existing_bom()
@@ -85,3 +105,17 @@ class TestMrpBom(TestNestedBomCase):
         self.mrp_bom_pinocchio.action_generate_nested_boms()
         status = self.mrp_bom_pinocchio.unlink_existing_bom()
         self.assertTrue(status, msg="Function result must be True")
+
+    def test_action_open_parent_bom(self):
+        self.mrp_bom_pinocchio.action_generate_nested_boms()
+        mrp_bom_ids = self.mrp_bom_pinocchio | self.mrp_bom_pinocchio_mrp
+        with self.assertRaises(ValueError):
+            mrp_bom_ids.action_open_parent_bom()
+        first_child_bom = self.mrp_bom_pinocchio.child_bom_ids[0]
+        action_dict = first_child_bom.action_open_parent_bom()
+        res_id = action_dict.get("res_id", False)
+        self.assertEqual(
+            self.mrp_bom_pinocchio.id,
+            res_id,
+            msg="Res id must be equal to {}".format(self.mrp_bom_pinocchio.id),
+        )
--- mrp_bom_nested_bom_generator/tests/test_mrp_nested_bom.py
+++ mrp_bom_nested_bom_generator/tests/test_mrp_nested_bom.py
@@ -34,7 +34,7 @@ class TestMrpNestedBom(TestNestedBomCase):
         self.assertEqual(
             len(self.mrp_nested_bom_wood.attribute_ids),
             0,
-            msg="Attributes count must be equal 0",
+            msg="Attributes count must be equal to zero",
         )
         self.mrp_nested_bom_wood.write(
             {"product_tmpl_id": self.product_template_log.id}
@@ -43,20 +43,20 @@ class TestMrpNestedBom(TestNestedBomCase):
         self.assertEqual(
             len(self.mrp_nested_bom_wood.attribute_ids),
             1,
-            msg="Attributes count must be equal 1",
+            msg="Attributes count must be equal to one",
         )
 
     def test_compute_attribute_aggregated_ids(self):
         self.assertEqual(
             len(self.mrp_nested_bom_log.attribute_aggregated_ids),
             2,
-            msg="Count elements must be equal 2",
+            msg="Count elements must be equal to two",
         )
         self.product_template_pinocchio_mrp.attribute_line_ids.unlink()
         self.assertEqual(
             len(self.mrp_nested_bom_log.attribute_aggregated_ids),
             1,
-            msg="Count elements must be equal 1",
+            msg="Count elements must be equal to one",
         )
 
     def test_create_product(self):
@@ -70,14 +70,14 @@ class TestMrpNestedBom(TestNestedBomCase):
         self.assertEqual(
             product_template.name,
             correct_name,
-            msg="Product template name must be equal 'Pinocchio #5'",
+            msg="Product template name must be equal to 'Pinocchio #5'",
         )
         result_product_template_id = MrpNestedBom.create_product(bom)
         product_template = self.get_product(result_product_template_id)
         self.assertEqual(
             product_template.name,
             correct_name,
-            msg="Product template name must be equal 'Pinocchio #1'",
+            msg="Product template name must be equal to 'Pinocchio #1'",
         )
 
         self.env["mrp.nested.bom"].create(
@@ -94,7 +94,7 @@ class TestMrpNestedBom(TestNestedBomCase):
         self.assertEqual(
             product_template.name,
             correct_name,
-            msg="Product template name must be equal 'Pinocchio #6'",
+            msg="Product template name must be equal to 'Pinocchio #6'",
         )
         self.env["mrp.nested.bom"].create(
             {
@@ -110,7 +110,7 @@ class TestMrpNestedBom(TestNestedBomCase):
         self.assertEqual(
             product_template.name,
             correct_name,
-            msg="Product template name must be equal 'Pinocchio #7'",
+            msg="Product template name must be equal to 'Pinocchio #7'",
         )
 
     def test_create_invalid(self):
@@ -127,49 +127,49 @@ class TestMrpNestedBom(TestNestedBomCase):
         self.assertEqual(
             result.product_tmpl_id.name,
             "Log #1",
-            msg="Product template name must be equal 'Log #1'",
+            msg="Product template name must be equal to 'Log #1'",
         )
 
     def test_prepare_bom_product_tmpl_attribute_ids(self):
         result = self.mrp_nested_bom_wood._prepare_bom_product_tmpl_attribute_ids()
-        self.assertEqual(result, set(), msg="Result must be equal empty set")
+        self.assertEqual(result, set(), msg="Result must be empty set")
         self.mrp_nested_bom_wood.attribute_ids = [(4, self.product_attribute_size.id)]
         result = self.mrp_nested_bom_wood._prepare_bom_product_tmpl_attribute_ids()
         self.assertEqual(
             result,
             set(self.product_attribute_size.ids),
-            msg="Result must be equal attribute 'Size' {id}",
+            msg="Result must be equal to attribute 'Size' {id}",
         )
 
     def test_find_product_template_attributes(self):
         result = self.mrp_nested_bom_log._find_product_template_attributes([])
-        self.assertEqual(len(result), 0, msg="Attributes count must be equal 0")
+        self.assertEqual(len(result), 0, msg="Attributes count must be equal to zero")
         result = self.mrp_nested_bom_log._find_product_template_attributes(
             self.product_attribute_size.ids
         )
-        self.assertEqual(len(result), 0, msg="Attributes count must be equal 0")
+        self.assertEqual(len(result), 0, msg="Attributes count must be equal to zero")
         result = self.mrp_nested_bom_log._find_product_template_attributes(
             [
                 self.product_attribute_size.id,
                 self.product_attribute_custom.id,
             ]
         )
-        self.assertEqual(len(result), 1, msg="Attributes count must be equal 1")
+        self.assertEqual(len(result), 1, msg="Attributes count must be equal to one")
         self.assertEqual(
             result,
             self.product_attribute_custom,
-            msg="Result must be equal 'Custom' product attribute",
+            msg="Result must be equal to 'Custom' product attribute",
         )
         result = self.mrp_nested_bom_log._find_product_template_attributes(
             [
                 self.product_attribute_custom.id,
             ]
         )
-        self.assertEqual(len(result), 1, msg="Attributes count must be equal 1")
+        self.assertEqual(len(result), 1, msg="Attributes count must be equal to one")
         self.assertEqual(
             result,
             self.product_attribute_custom,
-            msg="Result must be equal 'Custom' product attribute",
+            msg="Result must be equal to 'Custom' product attribute",
         )
 
     def test_prepare_product_attribute(self):
@@ -179,12 +179,12 @@ class TestMrpNestedBom(TestNestedBomCase):
         self.assertEqual(
             len(self.product_template_log.attribute_line_ids),
             1,
-            msg="Product Template Attributes count must be equal 1",
+            msg="Product Template Attributes count must be equal to one",
         )
         self.assertEqual(
             len(self.product_template_wood.attribute_line_ids),
             0,
-            msg="Product Template Attributes count must be equal 0",
+            msg="Product Template Attributes count must be equal to zero",
         )
         mrp_nested_ids = self.env["mrp.nested.bom"].browse(
             [self.mrp_nested_bom_log.id, self.mrp_nested_bom_wood.id]
@@ -193,10 +193,10 @@ class TestMrpNestedBom(TestNestedBomCase):
         self.assertEqual(
             len(self.product_template_log.attribute_line_ids),
             1,
-            msg="Product Template Attributes count must be equal 1",
+            msg="Product Template Attributes count must be equal to one",
         )
         self.assertEqual(
             len(self.product_template_wood.attribute_line_ids),
             1,
-            msg="Product Template Attributes count must be equal 1",
+            msg="Product Template Attributes count must be equal to one",
         )
