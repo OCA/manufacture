64d32f7476abf5d14900ddc843ab55efd882f642
Ivàn Todorovich <ivan.todorovich@gmail.com>
2022-10-25 15:41:00 -0300

    [MIG] mrp_bom_attribute_match: Migration to 15.0
    Notes:
    
    * `_get_product2bom` renamed to `_bom_find`
    * Test case `test_manufacturing_order_2`:
      * No longer check for "Add some materials to consume before marking this MO as to do.",
        as it's already supported in core since https://github.com/odoo/odoo/commit/bf5e1debf

--- mrp_bom_attribute_match/models/mrp_production.py
+++ mrp_bom_attribute_match/models/mrp_production.py
@@ -5,15 +5,14 @@ class MrpProduction(models.Model):
     _inherit = "mrp.production"
 
     def action_confirm(self):
-        res = super(MrpProduction, self).action_confirm()
-        if self and self.bom_id:
-            for bom_line in self.bom_id.bom_line_ids:
-                if bom_line.component_template_id:
-                    # product_id was set in mrp.bom.explode for correct flow. Need to remove it.
-                    bom_line.product_id = False
+        res = super().action_confirm()
+        for bom_line in self.bom_id.bom_line_ids:
+            if bom_line.component_template_id:
+                # product_id was set in mrp.bom.explode for correct flow. Need to remove it.
+                bom_line.product_id = False
         return res
 
     def write(self, vals):
-        for bl in self.bom_id.bom_line_ids.filtered(lambda x: x.component_template_id):
+        for bl in self.bom_id.bom_line_ids.filtered("component_template_id"):
             bl.check_component_attributes()
-        return super(MrpProduction, self).write(vals)
+        return super().write(vals)
