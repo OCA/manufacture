a612859ea97d532f965c65b41683f377de263558
ThiagoMForgeFlow <thiago.mulero@forgeflow.com>
2022-11-11 09:29:29 +0100

    [14.0][ADD] mrp_unbuild_valuation_layer_link
    [14.0][FIX] mrp_unbuild_valuation_layer_link: add assertTrue to the test

---
+++ mrp_unbuild_valuation_layer_link/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Valuation layers for unbuild orders",
+    "version": "14.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "Manufacture",
+    "summary": "Unbuild orders display the connected valuation layers",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/manufacture",
+    "depends": ["mrp_account"],
+    "data": ["views/mrp_unbuild_views.xml"],
+    "installable": True,
+}
