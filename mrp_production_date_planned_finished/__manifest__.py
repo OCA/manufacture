cb702701063805d32f349786642bfdeb6a8881bf
Ivàn Todorovich <ivan.todorovich@gmail.com>
2022-12-13 10:51:55 -0300

    [ADD] mrp_production_date_planned_finished

---
+++ mrp_production_date_planned_finished/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Camptocamp SA (https://www.camptocamp.com).
+# @author Iván Todorovich <ivan.todorovich@camptocamp.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "MRP Production Date Planned Finished",
+    "summary": "Allows to plan production from the desired finish date",
+    "version": "15.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainers": ["ivantodorovich"],
+    "website": "https://github.com/OCA/manufacture",
+    "license": "AGPL-3",
+    "category": "Manufacturing",
+    "depends": ["mrp"],
+    "data": ["views/mrp_production.xml"],
+}
