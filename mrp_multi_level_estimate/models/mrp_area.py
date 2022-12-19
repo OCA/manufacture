d98b536e6d98cb720f10171750a1a0991672f0d0
Lois Rilo <lois.rilo@forgeflow.com>
2022-12-19 16:10:02 +0100

    [REW] mrp_multi_level_estimate: remove unneeded dependency
    Remove unneeded dependency to date_range and relicense to LGPL.

--- mrp_multi_level_estimate/models/mrp_area.py
+++ mrp_multi_level_estimate/models/mrp_area.py
@@ -1,6 +1,6 @@
 # Copyright 2022 ForgeFlow S.L. (http://www.forgeflow.com)
 # - Lois Rilo Antelo <lois.rilo@forgeflow.com>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from odoo import fields, models
 
