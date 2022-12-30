0a7e0514f4918edbf13aea302dd340233e2c6c5e
DavidJForgeFlow <david.jimenez@forgeflow.com>
2022-12-30 11:14:49 +0100

    [14.0][ADD] mrp_component_operation_scrap_reason

---
+++ mrp_component_operation_scrap_reason/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "MRP Component Operation Scrap Reason",
+    "version": "14.0.1.0.0",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "summary": "Allows to pass a reason to scrap with MRP component operation",
+    "website": "https://github.com/OCA/manufacture",
+    "category": "Manufacturing/Manufacturing",
+    "license": "AGPL-3",
+    "depends": ["scrap_reason_code", "mrp_component_operation"],
+    "data": [
+        "wizards/mrp_component_operate_wizard.xml",
+    ],
+    "installable": True,
+    "auto-install": True,
+}
