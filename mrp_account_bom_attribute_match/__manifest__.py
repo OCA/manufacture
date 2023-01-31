495410b3987fafcf9a205db235f250b1e263efb2
Ivàn Todorovich <ivan.todorovich@gmail.com>
2023-01-31 10:32:24 -0300

    [ADD] mrp_account_bom_attribute_match

---
+++ mrp_account_bom_attribute_match/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Camptocamp SA (https://www.camptocamp.com).
+# @author Iván Todorovich <ivan.todorovich@camptocamp.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "MRP Account BOM Attribute Match",
+    "summary": "Glue module between `mrp_account` and `mrp_bom_attribute_match`",
+    "version": "15.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainers": ["ivantodorovich"],
+    "website": "https://github.com/OCA/manufacture",
+    "license": "AGPL-3",
+    "category": "Manufacturing",
+    "depends": ["mrp_account", "mrp_bom_attribute_match"],
+    "auto_install": True,
+}
