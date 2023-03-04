5914a5a5fdefe65b8b5e58af4a78a8d8b2f4a7cf
Ruchir Shukla <ruchir@bizzappdev.com>
2023-03-04 16:51:24 +0530

    [MIG][15.0]repair_type:Migration to 15.0

--- repair_type/__manifest__.py
+++ repair_type/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Repair Type",
-    "version": "14.0.1.0.2",
+    "version": "15.0.0.1.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/manufacture",
     "summary": "Repair type",
