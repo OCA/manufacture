d65f3d035369b256519fdab992b4480ab0eb70a0
Hardik Suthar <hsuthar@opensourceintegrators.com>
2023-03-09 12:11:27 +0530

    [FIX] Pre-Commit

--- mrp_workorder_cancel/models/mrp_workorder.py
+++ mrp_workorder_cancel/models/mrp_workorder.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2023 Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import models, _
+from odoo import _, models
 from odoo.exceptions import UserError
 
 
