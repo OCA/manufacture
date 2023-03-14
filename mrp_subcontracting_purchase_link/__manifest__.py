45344aca023a8fb6244c98126409e258a254eb81
Jordi Ballester Alomar <jordi.ballester@forgeflow.com>
2023-03-14 20:16:01 +0100

    [IMP] mrp_subcontracting_purchase_link: add post init hook to populate the PO line on previous manufacturing orders

--- mrp_subcontracting_purchase_link/__manifest__.py
+++ mrp_subcontracting_purchase_link/__manifest__.py
@@ -5,9 +5,10 @@
     "version": "16.0.1.0.0",
     "category": "Manufacturing",
     "license": "LGPL-3",
-    "author": "Quartile Limited, Odoo Community Association (OCA)",
+    "author": "Quartile Limited, ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/manufacture",
     "depends": ["purchase", "mrp_subcontracting"],
     "data": ["views/purchase_order_views.xml", "views/mrp_production_views.xml"],
+    "post_init_hook": "post_init_hook",
     "installable": True,
 }
--- mrp_subcontracting_purchase_link/readme/CONTRIBUTORS.rst
+++ mrp_subcontracting_purchase_link/readme/CONTRIBUTORS.rst
@@ -2,3 +2,8 @@
 
   * Yoshi Tashiro <tashiro@quartile.co>
   * Tim Lai <tl@quartile.com>
+
+* `ForgeFlow <https://www.forgeflow.com>`__:
+
+  * Thiago Mulero <thuago.mulero@forgeflow.com>
+  * Jordi Ballester <jordi.ballester@forgeflow.com>
