9fc202e5bb91fa80d2cb1aa6634a2148a787db1f
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-03-27 13:10:57 +0200

    [REF] mrp_attachment_mgmt: Avoid code duplication for attachment view
    - Put the method on product, and call it from all the places.
    - Remove excessive method fragmentation w/o gain.
    - Replace action read by proper `_for_xml_id` call.

--- mrp_attachment_mgmt/models/mrp_workorder.py
+++ mrp_attachment_mgmt/models/mrp_workorder.py
@@ -1,4 +1,5 @@
 # Copyright 2021 Tecnativa - Víctor Martínez
+# Copyright 2023 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import _, models
 from odoo.exceptions import UserError
@@ -6,20 +7,6 @@ from odoo.exceptions import UserError
 
 class MrpWorkorder(models.Model):
     _inherit = "mrp.workorder"
-
-    def _action_see_workorder_attachments_products(self, products):
-        domain = [
-            "|",
-            "&",
-            ("res_model", "=", "product.product"),
-            ("res_id", "in", products.ids),
-            "&",
-            ("res_model", "=", "product.template"),
-            ("res_id", "in", products.mapped("product_tmpl_id").ids),
-        ]
-        action = self.env.ref("base.action_attachment").read()[0]
-        action.update({"domain": domain})
-        return action
 
     def action_see_workorder_attachments(self):
         error = []
@@ -33,6 +20,4 @@ class MrpWorkorder(models.Model):
             raise UserError(
                 _("%d Product(s) without drawing:\n%s") % (len(error), "\n".join(error))
             )
-        return self._action_see_workorder_attachments_products(
-            self.mapped("product_id")
-        )
+        return self.product_id._action_show_attachments()
