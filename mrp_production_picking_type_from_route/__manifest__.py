c7e580adc5114f56349e29883c8c0fc4b3fd6f2e
Lois Rilo <lois.rilo@forgeflow.com>
2023-03-28 17:08:49 +0200

    [ADD] mrp_production_picking_type_from_route

---
+++ mrp_production_picking_type_from_route/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "MRP Production Picking Type From Route",
+    "summary": "Updates the operation type creating MO based on the product",
+    "version": "14.0.1.0.0",
+    "category": "Manufacture",
+    "website": "https://github.com/OCA/manufacture",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "depends": ["mrp"],
+    "installable": True,
+}
