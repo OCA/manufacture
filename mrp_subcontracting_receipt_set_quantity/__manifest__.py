652c1b7809898cce9e1bedf3859c7d492ba4c3b6
Aungkokolin1997 <lin@quartile.co>
2023-04-04 21:11:49 +0630

    [ADD] mrp_subcontracting_receipt_set_quantity

---
+++ mrp_subcontracting_receipt_set_quantity/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "MRP Subcontracting Receipt Set Quantity",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited, Odoo Community Association (OCA)",
+    "category": "Manufacturing",
+    "website": "https://github.com/OCA/manufacture",
+    "depends": ["mrp_subcontracting"],
+    "license": "AGPL-3",
+    "installable": True,
+}
