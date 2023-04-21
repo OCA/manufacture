f29b79bc509b153ab2482c71b6464113f37ac493
Aungkokolin1997 <aungkokolin23397@gmail.com>
2023-04-21 13:47:52 +0630

    [MIG] mrp_subcontracting_no_negative: Migration to 16.0

--- mrp_subcontracting_no_negative/__manifest__.py
+++ mrp_subcontracting_no_negative/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
     "name": "MRP Subcontracting (no negative components)",
-    "version": "15.0.0.1.0",
+    "version": "16.0.1.0.0",
     "development_status": "Alpha",
     "license": "AGPL-3",
     "author": "Camptocamp, Odoo Community Association (OCA)",
@@ -11,7 +11,6 @@
     "website": "https://github.com/OCA/manufacture",
     "category": "Manufacturing",
     "depends": [
-        # core
         "mrp_subcontracting",
     ],
     "data": [],
