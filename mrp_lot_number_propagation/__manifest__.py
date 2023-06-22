483032eea1c20d1b9795e6f7612a5584e0a8567d
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2023-06-22 17:49:51 +0200

    [MIG] mrp_lot_number_propagation: Migration to 16.0

--- mrp_lot_number_propagation/__manifest__.py
+++ mrp_lot_number_propagation/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
     "name": "MRP Serial Number Propagation",
-    "version": "15.0.0.3.0",
+    "version": "16.0.1.0.0",
     "development_status": "Alpha",
     "license": "AGPL-3",
     "author": "Camptocamp, Odoo Community Association (OCA)",
