2176a40e0efad0e2e381f6aea4837dcdff4fad48
Michael Tietz <mtietz@mt-software.de>
2023-08-07 12:05:33 +0200

    [ADD] mrp_bom_find_ignore: Mute _bom_find by adding 'ignore_bom_find' to the context _bom_find will return an empty record set

---
+++ mrp_bom_find_ignore/__manifest__.py
@@ -0,0 +1,15 @@
+# # Copyright 2023 Michael Tietz (MT Software) <mtietz@mt-software.de>
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "MRP Bom Find Ignore",
+    "summary": "Adding 'ignore_bom_find' to the context "
+    "will let '_bom_find' return an empty record set",
+    "version": "14.0.1.0.0",
+    "category": "Manufacture",
+    "website": "https://github.com/OCA/manufacture",
+    "author": "MT Software, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "depends": ["mrp"],
+    "data": [],
+    "maintainers": ["mt-software-de"],
+}
---
+++ mrp_bom_find_ignore/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Adding 'ignore_bom_find' to the context will let '_bom_find' return an empty record set
