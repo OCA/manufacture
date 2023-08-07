bc736e3903e0185128b0cc7a123f57e44c9c97c1
Michael Tietz <mtietz@mt-software.de>
2023-08-07 12:05:33 +0200

    [ADD] mrp_helper: Add methods shared between various mrp modules

---
+++ mrp_helper/__manifest__.py
@@ -0,0 +1,14 @@
+# # Copyright 2023 Michael Tietz (MT Software) <mtietz@mt-software.de>
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "MRP Helper",
+    "summary": "Add methods shared between various mrp modules",
+    "version": "14.0.1.0.0",
+    "category": "Manufacture",
+    "website": "https://github.com/OCA/manufacture",
+    "author": "MT Software, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "depends": ["mrp"],
+    "data": [],
+    "maintainers": ["mt-software-de"],
+}
