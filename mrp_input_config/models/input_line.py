9a593f087d7d38d42a92859742e69d5a6a0037df
David Beal <david.beal@akretion.com>
2023-08-14 18:55:59 +0200

    ADD module mrp_input_config

---
+++ mrp_input_config/models/input_line.py
@@ -0,0 +1,24 @@
+from odoo import fields, models
+
+
+class Inputline(models.Model):
+    _name = "input.line"
+    _description = "Input data for bom configuration"
+
+    name = fields.Char(required=True)
+    sequence = fields.Integer()
+    bom_id = fields.Many2one(
+        comodel_name="mrp.bom", required=True, domain=lambda s: s.bom_id.variable
+    )
+    config_id = fields.Many2one(comodel_name="input.config", required=True)
+    count = fields.Integer(default=1)
+    comment = fields.Char()
+
+    def ui_clone(self):
+        self.ensure_one()
+        vals = {"name": "%s copy" % self.name}
+        self.copy(vals)
+
+    def ui_configure(self):
+        # TODO
+        pass
