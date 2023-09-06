52f8d92c66a39ff6d3258b315ccfd4d4446eb53f
Francois Poizat <francois.poizat@gmail.com>
2023-09-06 16:40:38 +0200

    IMPL adds smart button for input_line batch edition

--- mrp_bom_configurable/models/input_config.py
+++ mrp_bom_configurable/models/input_config.py
@@ -19,3 +19,18 @@ class InputConfig(models.Model):
     )
     partner_id = fields.Many2one(comodel_name="res.partner")
     line_ids = fields.One2many(comodel_name="input.line", inverse_name="config_id")
+    line_count = fields.Integer(string="Lines", compute="_compute_line_count")
+
+    def _compute_line_count(self):
+        for rec in self:
+            rec.line_count = len(rec.line_ids)
+
+    def show_lines(self):
+        self.ensure_one()
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Lines",
+            "view_mode": "tree",
+            "res_model": "input.line",
+            "domain": [("config_id", "=", self.id)],
+        }
