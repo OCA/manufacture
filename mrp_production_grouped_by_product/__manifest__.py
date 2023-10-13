7680f41142cb07bc87cac04e1cf8965e331edbc5
Sibert Aerts <sibert.aerts@gmail.com>
2023-10-13 14:36:44 +0200

    [FIX] Fix edge case date bug

--- mrp_production_grouped_by_product/__manifest__.py
+++ mrp_production_grouped_by_product/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Production Grouped By Product",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
     "category": "MRP",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/manufacture",
