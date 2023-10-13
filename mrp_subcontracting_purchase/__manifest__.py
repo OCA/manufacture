89fd156c0170d72d17a58b0b3391971c8b351ffd
geomer198 <geomer198@gmail.com>
2023-10-13 13:57:03 +0400

    [MIG] mrp_subcontracting_purchase: Adapt to OCA guidelines + initial work

--- mrp_subcontracting_purchase/__manifest__.py
+++ mrp_subcontracting_purchase/__manifest__.py
@@ -1,19 +1,20 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Purchase and Subcontracting Management',
-    'version': '0.1',
-    'category': 'Manufacturing/Purchase',
-    'description': """
+    "name": "Purchase and Subcontracting Management",
+    "version": "14.0.1.0.0",
+    "category": "Manufacturing/Purchase",
+    "summary": """
         This bridge module adds some smart buttons between Purchase and Subcontracting
     """,
-    'depends': ['mrp_subcontracting', 'purchase'],
-    'data': [
-        'views/purchase_order_views.xml',
-        'views/stock_picking_views.xml',
+    "author": "Odoo S.A., Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/manufacture",
+    "depends": ["mrp_subcontracting", "purchase"],
+    "data": [
+        "views/purchase_order_views.xml",
+        "views/stock_picking_views.xml",
     ],
-    'installable': True,
-    'auto_install': True,
-    'license': 'LGPL-3',
+    "installable": True,
+    "auto_install": True,
+    "license": "LGPL-3",
 }
--- mrp_subcontracting_purchase/models/stock_picking.py
+++ mrp_subcontracting_purchase/models/stock_picking.py
@@ -1,40 +1,49 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields, api, _
+from odoo import _, api, fields, models
 
 
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
     subcontracting_source_purchase_count = fields.Integer(
-        "Number of subcontracting PO Source", compute='_compute_subcontracting_source_purchase_count',
-        help="Number of subcontracting Purchase Order Source")
+        "Number of subcontracting PO Source",
+        compute="_compute_subcontracting_source_purchase_count",
+        help="Number of subcontracting Purchase Order Source",
+    )
 
-    @api.depends('move_lines.move_dest_ids.raw_material_production_id')
+    @api.depends("move_lines.move_dest_ids.raw_material_production_id")
     def _compute_subcontracting_source_purchase_count(self):
         for picking in self:
-            picking.subcontracting_source_purchase_count = len(picking._get_subcontracting_source_purchase())
+            picking.subcontracting_source_purchase_count = len(
+                picking._get_subcontracting_source_purchase()
+            )
 
     def action_view_subcontracting_source_purchase(self):
         purchase_order_ids = self._get_subcontracting_source_purchase().ids
         action = {
-            'res_model': 'purchase.order',
-            'type': 'ir.actions.act_window',
+            "res_model": "purchase.order",
+            "type": "ir.actions.act_window",
         }
         if len(purchase_order_ids) == 1:
-            action.update({
-                'view_mode': 'form',
-                'res_id': purchase_order_ids[0],
-            })
+            action.update(
+                {
+                    "view_mode": "form",
+                    "res_id": purchase_order_ids[0],
+                }
+            )
         else:
-            action.update({
-                'name': _("Source PO of %s", self.name),
-                'domain': [('id', 'in', purchase_order_ids)],
-                'view_mode': 'tree,form',
-            })
+            action.update(
+                {
+                    "name": _("Source PO of %s", self.name),
+                    "domain": [("id", "in", purchase_order_ids)],
+                    "view_mode": "tree,form",
+                }
+            )
         return action
 
     def _get_subcontracting_source_purchase(self):
-        moves_subcontracted = self.move_lines.move_dest_ids.raw_material_production_id.move_finished_ids.move_dest_ids.filtered(lambda m: m.is_subcontract)
+        moves_subcontracted = self.move_lines.move_dest_ids.raw_material_production_id.move_finished_ids.move_dest_ids.filtered(  # noqa
+            lambda m: m.is_subcontract
+        )
         return moves_subcontracted.purchase_line_id.order_id
