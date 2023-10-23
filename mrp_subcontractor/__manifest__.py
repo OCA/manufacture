38801bd06be19850a8adba10c4e5c65e284192ea
Benoit <benoit.guillot@akretion.com>
2023-10-23 10:29:53 +0200

    [ADD] module mrp_subcontractor

---
+++ mrp_subcontractor/__manifest__.py
@@ -0,0 +1,13 @@
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "MRP subcontractor",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Manufacturing",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/manufacture",
+    "depends": ["mrp"],
+    "data": ["views/mrp_bom.xml"],
+    "installable": True,
+}
