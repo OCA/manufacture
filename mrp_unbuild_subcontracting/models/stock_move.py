97b40869c149417ee4d546268e1d68bc642481ba
BernatPForgeFlow <bernat.puig@forgeflow.com>
2024-01-03 17:00:22 +0100

    [FIX] mrp_unbuild_subcontracting: Adapt logic for partial receipts in 16.0

--- mrp_unbuild_subcontracting/models/stock_move.py
+++ mrp_unbuild_subcontracting/models/stock_move.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-from odoo import _, api, models
+from odoo import _, models
 from odoo.exceptions import UserError
 from odoo.tools.float_utils import float_is_zero
 
@@ -32,24 +32,6 @@ class StockMove(models.Model):
                 ):
                     raise UserError(_("To subcontract, use a planned transfer."))
                 subcontract_details_per_picking[move.picking_id].append((move, bom))
-                move.write(
-                    {
-                        "is_subcontract": True,
-                    }
-                )
-                # CHECK ME: Partial receipt of subcontracting process
-                # and refund change move_orig_ids adding all
-                # subcontracting origin moves, because
-                # that we try to restore and keep consistent data structure
-                # https://github.com/odoo/odoo/blob/b57c1332251b1553f98b9ad16b45a47d4101ffb2/addons/stock/wizard/stock_picking_return.py#L151-L153  # noqa: B950
-                if (
-                    len(move.mapped("move_orig_ids.move_orig_ids.production_id")) > 1
-                    and move.origin_returned_move_id
-                    and move.origin_returned_move_id.id in move.move_orig_ids.ids
-                ):
-                    move.write(
-                        {"move_orig_ids": [(6, 0, move.origin_returned_move_id.ids)]}
-                    )
                 move_to_not_merge |= move
             for picking, subcontract_details in subcontract_details_per_picking.items():
                 picking._subcontracted_produce_unbuild(subcontract_details)
@@ -68,23 +50,3 @@ class StockMove(models.Model):
             merge=merge, merge_into=merge_into
         )
         return result
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        for val in vals_list:
-            if val.get("move_dest_ids", False) and val.get("production_id", False):
-                if (
-                    self.env["mrp.production"]
-                    .browse(val.get("production_id", False))
-                    .bom_id.type
-                    == "subcontract"
-                ):
-                    # When we have partial receive move_orig_ids
-                    # keep first subcontracting manufacturing order moves link
-                    # should be avoided this situation, to refund cases
-                    self.browse(val.get("move_dest_ids", False)[0][1]).write(
-                        {
-                            "move_orig_ids": [(5, 0, 0)],
-                        }
-                    )
-        return super().create(vals_list)
