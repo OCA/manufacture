8614f833501d849e09fe0094f66bbf723d47e1b1
Akim Juillerat <akim.juillerat@camptocamp.com>
2024-01-18 18:56:39 +0100

    Add module mrp_repair_reuse_component

---
+++ mrp_repair_reuse_component/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    "name": "MRP Repair Reuse Component",
+    "summary": "Allow to reuse component in manufacturing after removal in repair",
+    "version": "14.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Uncategorized",
+    "website": "https://github.com/OCA/manufacture",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainers": ["grindtildeath"],
+    "license": "AGPL-3",
+    "depends": [
+        "mrp",
+        "repair",
+    ],
+}
