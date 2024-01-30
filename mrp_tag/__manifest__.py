769c345f2d6457189ede6ba9414dc2ac005e2a6e
SilvioC2C <silvio.gregorini@camptocamp.com>
2024-01-30 14:22:15 +0100

    [MIG] mrp_tag: Migration to 16.0

--- mrp_tag/__manifest__.py
+++ mrp_tag/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "MRP Tags",
     "summary": "Allows to add multiple tags to Manufacturing Orders",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/manufacture",
     "category": "Purchases",
