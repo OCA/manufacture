2f1c38c3437d0c3b7b84ababd14e6a3d7c5596d8
Víctor Martínez <victor.martinez@tecnativa.com>
2024-02-28 13:16:04 +0100

    [FIX] mrp_restrict_lot: Change SavepointCase to TransactionCase

--- mrp_restrict_lot/tests/test_manufaturing_lot.py
+++ mrp_restrict_lot/tests/test_manufaturing_lot.py
@@ -1,9 +1,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestRestrictLot(SavepointCase):
+class TestRestrictLot(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
