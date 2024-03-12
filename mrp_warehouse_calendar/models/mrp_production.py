5a2d423fee204ba53edcd86a4db67789ee54183e
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2024-03-12 11:51:32 +0530

    [MIG] mrp_warehouse_calendar: Migration to 17.0

--- mrp_warehouse_calendar/models/mrp_production.py
+++ mrp_warehouse_calendar/models/mrp_production.py
@@ -7,25 +7,31 @@ from odoo import api, models
 class MrpProduction(models.Model):
     _inherit = "mrp.production"
 
-    @api.depends("company_id", "date_planned_start", "is_planned", "product_id")
-    def _compute_date_planned_finished(self):
-        res = super(MrpProduction, self)._compute_date_planned_finished()
-        productions = self.filtered(lambda p: p.date_planned_start and not p.is_planned)
+    @api.depends(
+        "company_id",
+        "date_start",
+        "is_planned",
+        "product_id",
+        "workorder_ids.duration_expected",
+    )
+    def _compute_date_finished(self):
+        res = super()._compute_date_finished()
+        productions = self.filtered(lambda p: p.date_start and not p.is_planned)
         for production in productions:
             warehouse = self.picking_type_id.warehouse_id
             if warehouse.calendar_id:
-                if production.product_id.produce_delay:
-                    production.date_planned_finished = warehouse.calendar_id.plan_days(
-                        +1 * production.product_id.produce_delay + 1,
-                        production.date_planned_start,
+                if production.bom_id.produce_delay:
+                    production.date_finished = warehouse.calendar_id.plan_days(
+                        +1 * production.bom_id.produce_delay + 1,
+                        production.date_start,
                     )
                 if production.company_id.manufacturing_lead:
-                    production.date_planned_finished = warehouse.calendar_id.plan_days(
+                    production.date_finished = warehouse.calendar_id.plan_days(
                         +1 * production.company_id.manufacturing_lead + 1,
-                        production.date_planned_finished,
+                        production.date_finished,
                     )
                 production.move_finished_ids = [
-                    (1, m.id, {"date": production.date_planned_finished})
+                    (1, m.id, {"date": production.date_finished})
                     for m in production.move_finished_ids
                 ]
         return res
@@ -33,11 +39,11 @@ class MrpProduction(models.Model):
     @api.returns("self", lambda value: value.id)
     def copy(self, default=None):
         mo = super().copy(default=default)
-        dt_planned = mo.date_planned_start
+        dt_planned = mo.date_start
         warehouse = mo.picking_type_id.warehouse_id
-        if warehouse.calendar_id and mo.product_id.produce_delay:
+        if warehouse.calendar_id and mo.bom_id.produce_delay:
             date_expected = warehouse.calendar_id.plan_days(
-                +1 * self.product_id.produce_delay + 1, dt_planned
+                +1 * self.bom_id.produce_delay + 1, dt_planned
             )
-            mo.date_planned_finished = date_expected
+            mo.date_finished = date_expected
         return mo
