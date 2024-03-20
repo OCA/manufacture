755292561eab228a693b6228f231249aa1351e43
Quentin Dupont <perso@quentindupont.fr>
2024-03-20 12:31:47 +0100

    [ADD] mrp_bom_select_product_variant

---
+++ mrp_bom_select_product_variant/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2024 - Today: GRAP (http://www.grap.coop)
+# @author: Quentin Dupont (quentin.dupont@grap.coop)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "MRP BoM Select Product Variant",
+    "summary": "Favors Product variant selection for BOM creation.",
+    "version": "16.0.1.0.0",
+    "author": "GRAP, Odoo Community Association (OCA)",
+    "category": "Manufacturing",
+    "depends": ["mrp"],
+    "website": "https://github.com/OCA/manufacture",
+    "license": "AGPL-3",
+    "data": [
+        "view/mrp_bom.xml",
+    ],
+    "installable": True,
+}
---
+++ mrp_bom_select_product_variant/readme/DESCRIPTION.rst
@@ -0,0 +1,8 @@
+This module is designed so that the user selects the product variant when
+creating or updating a Bill of Material.
+When selecting a product variant, the linked product template is
+automatically selected.
+
+Tree and filter view are also changed : you see and search with product variant.
+
+.. figure:: ../static/description/mrp_bom_select_product_variant.gif
