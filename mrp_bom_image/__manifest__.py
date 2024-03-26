bacbaf928f2ffab3b9e966bc541cfae9ca437c23
Quentin Dupont <perso@quentindupont.fr>
2024-03-26 15:47:14 +0100

    [16.0][ADD] mrp_bom_image

---
+++ mrp_bom_image/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2024 - Today: GRAP (http://www.grap.coop)
+# @author: Quentin Dupont (quentin.dupont@grap.coop)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "MRP BoM Image",
+    "summary": "Add product Images to BoM",
+    "version": "16.0.1.0.0",
+    "category": "Manufacture",
+    "author": "GRAP, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/manufacture",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "views/view_mrp_bom.xml",
+    ],
+    "installable": True,
+}
