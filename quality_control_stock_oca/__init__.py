ff1d6bc76e1c9942977b39c0ee5bae0c4621a244
Antoni Marroig Campomar <amarroig@apsl.net>
2024-05-14 15:42:37 +0200

    [MIG] quality_control_stock_oca: Migration to 17.0

--- quality_control_stock_oca/__init__.py
+++ quality_control_stock_oca/__init__.py
@@ -3,12 +3,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import models
-from odoo import api, SUPERUSER_ID
 
 
-def post_init_hook(cr, registry):
+def post_init_hook(env):
     # Create QC triggers
-    env = api.Environment(cr, SUPERUSER_ID, {})
     picking_type_ids = env["stock.picking.type"].sudo().search([])
     for picking_type_id in picking_type_ids:
         picking_type_id.sudo()._create_qc_trigger()
