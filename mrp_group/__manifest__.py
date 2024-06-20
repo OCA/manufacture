7a084f9a309e54365757a441c891d8ad89aa8b07
Bibin <bibinraj143182@gmail.com>
2024-06-20 19:15:14 +0530

    changes in py,xml files

--- mrp_group/__manifest__.py
+++ mrp_group/__manifest__.py
@@ -3,7 +3,7 @@
     "name": "MRP Group",
     "version": "17.0.0.0.1",
     "category": "Manufacturing",
-    "summary": "This module simplifies production management by organizing BOMs and MOs into groups, streamlining complex production processes. It helps identify and group multiple BOMs and MOs involved in producing a finished product, making production management more efficient.",
+    "summary": "This module simplifies production management by organizing BOMs and MOs into groups",
     "website": "https://github.com/OCA/manufacture",
     "author": "ZestyBeanz Technologies,Odoo Community Association (OCA)",
     "maintainer": "Odoo Community Association (OCA)",
