b321d837d5d921076a52cbc5a29c741e87f3f0d8
Bibin <bibinraj143182@gmail.com>
2024-06-20 19:21:32 +0530

    changes in py,xml files

--- mrp_group/models/mrp_group.py
+++ mrp_group/models/mrp_group.py
@@ -10,10 +10,11 @@ class MrpGroup(models.Model):
     _inherit = ["mail.thread"]
     _description = "Mrp Group"
     
+    def _get_default_color(self):
+        return randint(1, 11)
+    
     name = fields.Char(string="Name", tracking=True)
     color = fields.Integer(string="Color", default=_get_default_color)
     
     _sql_constraints = [("unique_name", "unique(name)", "Group name should be unique!")]
     
-    def _get_default_color(self):
-        return randint(1, 11)
