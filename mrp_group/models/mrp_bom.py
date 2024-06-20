924f1e2438f97a60692720ef4c77274d54982bd4
Bibin <bibinraj143182@gmail.com>
2024-06-20 14:59:52 +0530

    changes in py,xml files

--- mrp_group/models/mrp_bom.py
+++ mrp_group/models/mrp_bom.py
@@ -4,5 +4,5 @@ from odoo import fields, models
 class MrpBom(models.Model):
     _inherit = "mrp.bom"
     
-    mrp_group_ids = fields.Many2many("mrp.group", string="MRP Groups") 
-    
+    mrp_group_ids = fields.Many2many("mrp.group", string="MRP Group") 
+
