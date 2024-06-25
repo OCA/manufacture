95d670d3ad60b2887b1ad5e874b9a41e133a8928
ZestyBeanz-Odoo <84498208+ZestyBeanz-Odoo@users.noreply.github.com>
2024-06-25 12:47:30 +0530

    Delete mrp_group directory

--- mrp_group/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import mrp_group
-from . import mrp_bom
-from . import mrp_production
--- mrp_group/models/mrp_bom.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class MrpBom(models.Model):
-    _inherit = "mrp.bom"
-
-    mrp_group_ids = fields.Many2many("mrp.group", string="MRP Group")
