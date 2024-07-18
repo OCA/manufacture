4b6e650e082dc2a0e65b381d67bb05e139a655db
Alfredo <alfredodelafuente@avanzosc.es>
2024-07-18 08:37:45 +0200

    [MIG] mrp_bom_version: Migration to 16.0

--- mrp_bom_version/__init__.py
+++ mrp_bom_version/__init__.py
@@ -1,12 +1,10 @@
-# -*- coding: utf-8 -*-
-# (c) 2015 Oihane Crucelaegui - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
 from . import models
 
 
-def set_active_bom_active_state(cr, registry):
+def _post_install_set_active_bom_active_state(cr, registry):
     """Set those active BoMs to state 'active'"""
-    cr.execute("""UPDATE mrp_bom
+    cr.execute(
+        """UPDATE mrp_bom
                      SET state = 'active'
-                   WHERE active = True""")
+                   WHERE active = True"""
+    )
