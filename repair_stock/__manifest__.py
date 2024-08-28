4ce8882498b9365f640a0cf5f3d28abadd88910f
DavidJForgeFlow <david.jimenez@forgeflow.com>
2024-08-28 09:22:42 +0200

    [ADD] repair_stock

---
+++ repair_stock/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Repair Stock",
+    "version": "14.0.1.0.0",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "category": "Repair",
+    "website": "https://github.com/OCA/manufacture",
+    "depends": ["repair_stock_move"],
+    "data": [
+        "views/repair_order_view.xml",
+    ],
+    "license": "AGPL-3",
+}
