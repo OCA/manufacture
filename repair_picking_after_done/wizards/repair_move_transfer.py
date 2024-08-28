1b254f8965e95d5ba24e24eced40195b860e7e3f
DavidJForgeFlow <david.jimenez@forgeflow.com>
2024-08-28 09:25:36 +0200

    [REF] repair_picking_after_done: adding repair_stock as dependency.

--- repair_picking_after_done/wizards/repair_move_transfer.py
+++ repair_picking_after_done/wizards/repair_move_transfer.py
@@ -46,6 +46,7 @@ class MrpInventoryProcure(models.TransientModel):
             "picking_type_id": self._get_picking_type().id,
             "product_uom_qty": self.quantity,
             "product_uom": self.repair_order_id.move_id.product_uom.id,
+            "repair_id": self.repair_order_id.id,
         }
 
     def action_create_transfer(self):
@@ -69,4 +70,4 @@ class MrpInventoryProcure(models.TransientModel):
             stock_move.move_line_ids[0]._assign_production_lot(
                 self.repair_order_id.lot_id
             )
-        self.repair_order_id.write({"picking_ids": [(4, picking.id)]})
+        self.repair_order_id._compute_picking_ids()
