a90ab95717d10106374cc9b9b37647b6b8dfc189
Kay K. Cross <kaycross@nomail.eu>
2024-10-18 11:55:17 +0200

    [MIG] mrp_bom_version: Migration to 16.0

--- mrp_bom_version/tests/test_mrp_bom_version.py
+++ mrp_bom_version/tests/test_mrp_bom_version.py
@@ -1,26 +1,27 @@
 # (c) 2015 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-import openerp.tests.common as common
+import odoo.tests as common
 
 
 class TestMrpBomVersion(common.TransactionCase):
-    def setUp(self):
-        super(TestMrpBomVersion, self).setUp()
-        self.parameter_model = self.env["ir.config_parameter"]
-        self.bom_model = self.env["mrp.bom"].with_context(test_mrp_bom_version=True)
-        self.company = self.env.ref("base.main_company")
+    @classmethod
+    def setUpClass(cls):
+        super(TestMrpBomVersion, cls).setUpClass()
+        cls.parameter_model = cls.env["ir.config_parameter"].sudo()
+        cls.bom_model = cls.env["mrp.bom"].with_context(test_mrp_bom_version=True)
+        cls.company = cls.env.ref("base.main_company")
         vals = {
-            "company_id": self.company.id,
-            "product_tmpl_id": self.env.ref(
+            "company_id": cls.company.id,
+            "product_tmpl_id": cls.env.ref(
                 "product.product_product_11_product_template"
             ).id,
             "bom_line_ids": [
-                (0, 0, {"product_id": self.env.ref("product.product_product_5").id}),
-                (0, 0, {"product_id": self.env.ref("product.product_product_6").id}),
+                (0, 0, {"product_id": cls.env.ref("product.product_product_5").id}),
+                (0, 0, {"product_id": cls.env.ref("product.product_product_6").id}),
             ],
         }
-        self.mrp_bom = self.bom_model.create(vals)
+        cls.mrp_bom = cls.bom_model.create(vals)
 
     def test_mrp_bom(self):
         self.assertEqual(
@@ -49,7 +50,7 @@ class TestMrpBomVersion(common.TransactionCase):
         self.assertFalse(self.mrp_bom.active, "Check must be False")
 
     def test_mrp_bom_back2draft_active(self):
-        self.parameter_model.create({"key": "active.draft", "value": True})
+        self.parameter_model.set_param("mrp_bom_version.active_draft", True)
         self.mrp_bom.button_activate()
         self.mrp_bom.button_draft()
         self.assertTrue(self.mrp_bom.active, "Check must be True, as set in parameters")
@@ -65,7 +66,11 @@ class TestMrpBomVersion(common.TransactionCase):
             "historical",
             "Incorrect state, it must have been historified",
         )
-        new_boms = self.bom_model.search([("parent_bom", "=", self.mrp_bom.id)])
+        new_boms = self.bom_model.with_context(active_test=False).search(
+            [
+                ("parent_bom", "=", self.mrp_bom.id),
+            ]
+        )
         for new_bom in new_boms:
             self.assertEqual(
                 new_bom.version,
