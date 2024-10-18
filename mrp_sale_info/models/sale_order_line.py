ac2b4ef10f4d27c30de5eed444adad075361ae86
Simone Rubino <simone.rubino@aion-tech.it>
2024-10-18 15:07:20 +0200

    [IMP] mrp_sale_info: Optimize computation

--- mrp_sale_info/models/sale_order_line.py
+++ mrp_sale_info/models/sale_order_line.py
@@ -1,5 +1,9 @@
 # Copyright 2024 Camptocamp SA
+# Copyright 2024 Simone Rubino - Aion Tech
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+import operator
+from functools import reduce
+
 from odoo import api, fields, models
 
 
@@ -14,19 +18,23 @@ class SaleOrderLine(models.Model):
 
     @api.depends(lambda sol: sol._created_prod_move_deps())
     def _compute_created_production_ids(self):
+        move_to_production_ids = dict()
+        for move in self.move_ids:
+            productions = move._get_orig_created_production_ids()
+            productions |= productions.procurement_group_id.mrp_production_ids
+            move_to_production_ids[move] = productions
+
         for line in self:
             delivery_moves = line.move_ids
-            mrp_production_ids = set()
-            for move in delivery_moves:
-                mrp_production_ids.update(move._get_orig_created_production_ids().ids)
-            # Consider MO Backorders for serial numbers
-            mrp_production_ids.update(
-                self.env["mrp.production"]
-                .browse(mrp_production_ids)
-                .mapped("procurement_group_id.mrp_production_ids")
-                .ids
+            productions = reduce(
+                operator.or_,
+                [
+                    move_to_production_ids[delivery_move]
+                    for delivery_move in delivery_moves
+                ],
+                self.env["mrp.production"].browse(),
             )
-            line.created_production_ids = [fields.Command.set(list(mrp_production_ids))]
+            line.created_production_ids = productions
 
     def _created_prod_move_deps(self):
         fnames = [
