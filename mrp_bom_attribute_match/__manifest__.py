9b6757a94da7443817b9d063902c570934383fd8
ferran-S73 <ferran@studio73.es>
2024-10-22 13:02:59 +0200

    [17.0][MIG] mrp_bom_attribute_match

--- mrp_bom_attribute_match/__manifest__.py
+++ mrp_bom_attribute_match/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "BOM Attribute Match",
-    "version": "15.0.1.1.1",
+    "version": "17.0.1.0.0",
     "category": "Manufacturing",
     "author": "Ilyas, Ooops, Odoo Community Association (OCA)",
     "summary": "Dynamic BOM component based on product attribute",
@@ -10,4 +10,5 @@
     "data": [
         "views/mrp_bom_views.xml",
     ],
+    "installable": True,
 }
--- mrp_bom_attribute_match/reports/mrp_report_bom_structure.py
+++ mrp_bom_attribute_match/reports/mrp_report_bom_structure.py
@@ -2,13 +2,27 @@
 # @author Iván Todorovich <ivan.todorovich@camptocamp.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import Command, models
+from odoo import Command, api, models
 
 
 class ReportBomStructure(models.AbstractModel):
     _inherit = "report.mrp.report_bom_structure"
 
-    def _get_bom_lines(self, bom, bom_quantity, product, line_id, level):
+    @api.model
+    def _get_bom_data(
+        self,
+        bom,
+        warehouse,
+        product=False,
+        line_qty=False,
+        bom_line=False,
+        level=0,
+        parent_bom=False,
+        parent_product=False,
+        index=0,
+        product_info=False,
+        ignore_stock=False,
+    ):
         # OVERRIDE to fill in the `line.product_id` if a component template is used.
         # To avoid a complete override, we HACK the bom by replacing it with a virtual
         # record, and modifying it's lines on-the-fly.
@@ -31,14 +45,24 @@ class ReportBomStructure(models.AbstractModel):
                     line.product_id = line_product
             if to_ignore_line_ids:
                 bom.bom_line_ids = [Command.unlink(id) for id in to_ignore_line_ids]
-        components, total = super()._get_bom_lines(
-            bom, bom_quantity, product, line_id, level
+        data = super()._get_bom_data(
+            bom,
+            warehouse,
+            product=product,
+            line_qty=line_qty,
+            bom_line=bom_line,
+            level=level,
+            parent_bom=parent_bom,
+            parent_product=parent_product,
+            index=index,
+            product_info=product_info,
+            ignore_stock=ignore_stock,
         )
         # Replace any NewId value by the real record id
         # Otherwise it's evaluated as False in some situations, and it may cause issues
         if has_template_lines:
-            for component in components:
+            for component in data.get("components", []):
                 for key, value in component.items():
                     if isinstance(value, models.NewId):
                         component[key] = value.origin
-        return components, total
+        return data
