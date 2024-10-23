2dda1a86ff963d71ac62ccc0c4b89e76d8ea3632
Víctor Martínez <victor.martinez@tecnativa.com>
2024-10-23 09:54:54 +0200

    [IMP] mrp_subcontracting_skip_no_negative: Show negative stock error of subcontracting component
    TT50668

--- mrp_subcontracting_skip_no_negative/models/stock_move.py
+++ mrp_subcontracting_skip_no_negative/models/stock_move.py
@@ -1,7 +1,10 @@
 # Copyright 2023 Quartile Limited
+# Copyright 2024 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models
+from odoo import _, models
+from odoo.exceptions import ValidationError
+from odoo.tools import config, float_compare
 
 
 class StockMove(models.Model):
@@ -31,6 +34,41 @@ class StockMove(models.Model):
             ):
                 continue
             moves_with_no_check -= move
+            # If you have not been able to allocate previously it is because there is
+            # no stock, therefore it will leave the stock negative, we deduct the
+            # quantity checking the components and show the corresponding error.
+            test_condition = (
+                config["test_enable"] and self.env.context.get("test_stock_no_negative")
+            ) or not config["test_enable"]
+            if not test_condition:
+                continue
+            qty_precision = self.env["decimal.precision"].precision_get(
+                "Product Unit of Measure"
+            )
+            for p_move in unassigned_productions.move_raw_ids.filtered(
+                lambda x: x.state != "assigned"
+                and not x.product_id.allow_negative_stock
+                and not x.product_id.categ_id.allow_negative_stock
+                and not x.location_id.allow_negative_stock
+            ):
+                product = p_move.product_id.sudo()
+                location = p_move.location_id
+                location_qty = product.with_context(location=location.id).free_qty
+                new_qty = location_qty - p_move.product_uom_qty
+                if float_compare(new_qty, 0, precision_digits=qty_precision) == -1:
+                    raise ValidationError(
+                        _(
+                            "You cannot validate this stock operation because the "
+                            "stock level of the component product '{name}' would become "
+                            "negative ({qty}) on the stock location '{location}' and "
+                            "negative stock is not allowed for this product and/or "
+                            "location."
+                        ).format(
+                            name=product.display_name,
+                            qty=new_qty,
+                            location=location.complete_name,
+                        )
+                    )
         res = super(StockMove, self - moves_with_no_check)._action_done(
             cancel_backorder=cancel_backorder
         )
