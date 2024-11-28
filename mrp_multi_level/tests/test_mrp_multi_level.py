35c9a734bcc9cf07aa2dc6c151ecfaf0ad042ee8
Lois Rilo <lois.rilo@forgeflow.com>
2024-11-28 16:49:38 +0100

    [FIX] mrp_multi_level: adapt tests to recent upstream changes.
    After https://github.com/odoo/odoo/commit/1ed6dbeae531dff07a16d50457a16c51db184e40
    we need to explicitly set routes in the propoer order becouse while
    in the cache there are not yet sorted by sequence (it requires flush
    to the database, which we cannot do in a test).

--- mrp_multi_level/tests/test_mrp_multi_level.py
+++ mrp_multi_level/tests/test_mrp_multi_level.py
@@ -444,8 +444,18 @@ class TestMrpMultiLevel(TestMrpMultiLevelCommon):
         self.fp_4.route_ids = [(4, self.env.ref("mrp.route_warehouse0_manufacture").id)]
         product_mrp_area._compute_supply_method()
         self.assertEqual(product_mrp_area.supply_method, "manufacture")
+        # because of the issue discussed here https://github.com/odoo/odoo/pull/188846
+        # we need to apply routes explicitly in the proper order (by sequence)
         self.fp_4.route_ids = [
-            (4, self.env.ref("purchase_stock.route_warehouse0_buy").id)
+            (
+                6,
+                0,
+                (
+                    self.env.ref("stock.route_warehouse0_mto")
+                    + self.env.ref("purchase_stock.route_warehouse0_buy")
+                    + self.env.ref("mrp.route_warehouse0_manufacture")
+                ).ids,
+            )
         ]
         product_mrp_area._compute_supply_method()
         self.assertEqual(product_mrp_area.supply_method, "buy")
