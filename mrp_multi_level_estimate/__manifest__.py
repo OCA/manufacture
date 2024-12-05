c54f2421503411cc8afd7f686e713d4a9d234911
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2024-12-05 10:20:45 +0530

    [MIG] mrp_multi_level_estimate: Migration to 18.0

--- mrp_multi_level_estimate/__manifest__.py
+++ mrp_multi_level_estimate/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "MRP Multi Level Estimate",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "development_status": "Production/Stable",
     "license": "LGPL-3",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
