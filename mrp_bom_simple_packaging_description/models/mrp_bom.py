93a623d1d8cd780a9f29433362dd9bc89376c8b6
Quentin Dupont <quentin.dupont@grap.coop>
2024-12-20 16:30:10 +0100

    [16.0][ADD] mrp_bom_simple_packaging_description

---
+++ mrp_bom_simple_packaging_description/models/mrp_bom.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2024 - Today: GRAP (http://www.grap.coop)
+# @author: Quentin DUPONT (quentin.dupont@grap.coop)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class MrpBom(models.Model):
+    _inherit = "mrp.bom"
+
+    description_packaging = fields.Char(
+        string="Packaging description",
+        help="Describe your Packaging. E.g. : Small Packet 250g.",
+    )
---
+++ mrp_bom_simple_packaging_description/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+Adds a text field in BoM for packaging description.
+
+.. figure:: ../static/description/mrp_bom_simple_description_packaging.jpeg
