331a0d126b41c35fc0b1e87c2d545a576972a72b
ArnauCForgeFlow <arnau.cruz@forgeflow.com>
2025-03-27 15:56:21 +0100

    [FIX] mrp_production_serial_matrix: increase line_ids limit

--- mrp_production_serial_matrix/__manifest__.py
+++ mrp_production_serial_matrix/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "MRP Production Serial Matrix",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.1.0",
     "category": "Manufacturing",
     "license": "AGPL-3",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
