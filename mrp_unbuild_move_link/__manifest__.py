aa789caa6a63f32136d2b30dfe4c91c284747629
Andreu Orensanz <andreu.orensanz@forgeflow.com>
2025-05-06 09:41:30 +0200

    [MIG] mrp_unbuild_move_link: Migration to 18.0

--- mrp_unbuild_move_link/__manifest__.py
+++ mrp_unbuild_move_link/__manifest__.py
@@ -2,10 +2,11 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Stock moves of manufacturing orders added to unbuild orders",
-    "version": "16.0.1.0.1",
+    "version": "18.0.1.0.0",
     "license": "LGPL-3",
     "category": "Manufacture",
-    "summary": "Link the stock moves of manufacturing orders to the respective unbuild orders",
+    "summary": """Link the stock moves of manufacturing orders to the
+    respective unbuild orders""",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/manufacture",
     "depends": ["mrp_account"],
