80db19437e3ce5fa2c26a4e8a3b3b6f5daf57af4
Jordi Ballester Alomar <jordi.ballester@forgeflow.com>
2025-07-07 16:35:43 +0200

    fixup!

--- mrp_bom_line_formula_applicability/models/mrp_production.py
+++ mrp_bom_line_formula_applicability/models/mrp_production.py
@@ -7,20 +7,21 @@ from odoo import models
 class MRPProduction(models.Model):
     _inherit = "mrp.production"
 
-    def _compute_move_raw_ids(self):
-        res = super()._compute_move_raw_ids()
-        for production in self:
-            moves_to_remove = self.env["stock.move"]
-            for move in production.move_raw_ids:
-                if move.bom_line_id and move.bom_line_id.applicability_formula:
-                    applicability = move.bom_line_id._eval_applicability_formula(
-                        move.product_id,
-                        move.product_uom,
-                        move.product_uom_qty,
-                        production,
-                        operation_id=move.operation_id,
+    def _get_moves_raw_values(self):
+        initial_moves = super()._get_moves_raw_values()
+        final_moves = []
+        for move in initial_moves:
+            if move.get("bom_line_id"):
+                bom_line = self.env["mrp.bom.line"].browse(move["bom_line_id"])
+                if bom_line.applicability_formula:
+                    applicability = bom_line._eval_applicability_formula(
+                        move["product_id"],
+                        move["product_uom"],
+                        move["product_uom_qty"],
+                        self,
+                        operation_id=move.get("operation_id"),
                     )
                     if not applicability:
-                        moves_to_remove |= move
-            production.move_raw_ids -= moves_to_remove
-        return res
+                        continue
+            final_moves.append(move)
+        return final_moves
