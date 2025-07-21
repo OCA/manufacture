361e322268dab0ceec6571e397b00dd186c4dda9
Rémi - Le Filament <30716308+remi-filament@users.noreply.github.com>
2025-07-21 13:25:10 +0200

    [IMP] quality_control_oca: remove unused object_id on qt_test

--- quality_control_oca/models/qc_test.py
+++ quality_control_oca/models/qc_test.py
@@ -17,14 +17,6 @@ class QcTest(models.Model):
     _description = "Quality control test"
     _inherit = "mail.thread"
 
-    def object_selection_values(self):
-        return set()
-
-    @api.onchange("type")
-    def onchange_type(self):
-        if self.type == "generic":
-            self.object_id = False
-
     active = fields.Boolean(default=True)
     name = fields.Char(required=True, translate=True)
     test_lines = fields.One2many(
@@ -33,16 +25,7 @@ class QcTest(models.Model):
         string="Questions",
         copy=True,
     )
-    object_id = fields.Reference(
-        string="Reference object",
-        selection="object_selection_values",
-    )
     fill_correct_values = fields.Boolean(string="Pre-fill with correct values")
-    type = fields.Selection(
-        [("generic", "Generic"), ("related", "Related")],
-        required=True,
-        default="generic",
-    )
     category = fields.Many2one(comodel_name="qc.test.category")
     company_id = fields.Many2one(
         comodel_name="res.company",
