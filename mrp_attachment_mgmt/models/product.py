385ed83b6730a70f47aed830e9e271ea56863b1d
dhx <dh.tx.dev@dhx.at>
2025-09-20 19:54:26 +0000

    [IMP] mrp_attachment_mgmt: use product.document search view for preview
    For product.template and product.product using the product_document_search view
    allows us to show attachment previews

--- mrp_attachment_mgmt/models/product.py
+++ mrp_attachment_mgmt/models/product.py
@@ -1,7 +1,7 @@
 # Copyright 2022 Tecnativa - Víctor Martínez
 # Copyright 2023 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import fields, models
+from odoo import _, fields, models
 
 
 class ProductTemplate(models.Model):
@@ -21,9 +21,21 @@ class ProductTemplate(models.Model):
             ("res_model", "=", "product.template"),
             ("res_id", "in", self.ids),
         ]
-        action = self.env["ir.actions.actions"]._for_xml_id("base.action_attachment")
-        action.update({"domain": domain})
-        return action
+        context = {
+            "default_res_model": "product.template",
+            "default_res_id": len(self.ids) == 1 and self.id or None,
+            "default_company_id": self.company_id.id,
+        }
+        return {
+            "name": _("Attachments"),
+            "domain": domain,
+            "res_model": "product.document",
+            "type": "ir.actions.act_window",
+            "view_mode": "kanban,list,form",
+            "target": "current",
+            "context": context,
+            "search_view_id": self.env.ref("product.product_document_search").ids,
+        }
 
 
 class ProductProduct(models.Model):
@@ -48,6 +60,18 @@ class ProductProduct(models.Model):
             ("res_model", "=", "product.template"),
             ("res_id", "in", self.product_tmpl_id.ids),
         ]
-        action = self.env["ir.actions.actions"]._for_xml_id("base.action_attachment")
-        action.update({"domain": domain})
-        return action
+        context = {
+            "default_res_model": "product.product",
+            "default_res_id": len(self.ids) == 1 and self.id or None,
+            "default_company_id": self.company_id.id,
+        }
+        return {
+            "name": _("Attachments"),
+            "domain": domain,
+            "res_model": "product.document",
+            "type": "ir.actions.act_window",
+            "view_mode": "kanban,list,form",
+            "target": "current",
+            "context": context,
+            "search_view_id": self.env.ref("product.product_document_search").ids,
+        }
