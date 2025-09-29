379f54889604820392ad41914bedb9e0dbd7b788
Akim Juillerat <akim.juillerat@camptocamp.com>
2025-09-29 20:37:20 +0200

    [FIX] mrp_lot_number_propagation: Raise error
    If no component reserved for mass produce, we want to display a
    nice error message.

--- mrp_lot_number_propagation/wizard/mrp_batch_produce_propagate.py
+++ mrp_lot_number_propagation/wizard/mrp_batch_produce_propagate.py
@@ -1,6 +1,7 @@
 # Copyright 2025 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 from odoo import fields, models
+from odoo.exceptions import UserError
 
 
 class MrpBatchProducePropagate(models.TransientModel):
@@ -24,13 +25,19 @@ class MrpBatchProducePropagate(models.TransientModel):
                 .create({})
             )
             propagate_move = production._get_propagating_component_move()
-            batch_produce_wizard.production_text = "\n".join(
-                [
-                    f"{ml.lot_id.name}{batch_produce_wizard.component_separator}{ml.lot_id.name}"
-                    for ml in propagate_move.move_line_ids
-                    if ml.lot_id
-                ]
-            )
+            production_text_lines = [
+                f"{ml.lot_id.name}{batch_produce_wizard.component_separator}{ml.lot_id.name}"
+                for ml in propagate_move.move_line_ids
+                if ml.lot_id
+            ]
+            if not production_text_lines:
+                raise UserError(
+                    self.env._(
+                        "Cannot mass produce without any propagating component line "
+                        "having serial number defined."
+                    )
+                )
+            batch_produce_wizard.production_text = "\n".join(production_text_lines)
             batch_produce_wizard.with_context(
                 lot_propagation=True
             )._production_text_to_object(mark_done=mark_done)
