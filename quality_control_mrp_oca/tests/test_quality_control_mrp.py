8e1879bedeed3d2bc6e9e76e92988795df2ebc0b
Alfredo <alfredodelafuente@avanzosc.es>
2025-11-11 12:22:47 +0100

    [18.0][MIG] quality_control_mrp_oca: Migration to 18.0

--- quality_control_mrp_oca/tests/test_quality_control_mrp.py
+++ quality_control_mrp_oca/tests/test_quality_control_mrp.py
@@ -2,7 +2,7 @@
 # Copyright 2018 Simone Rubino - Agile Business Group
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import Form
+from odoo.tests import Form
 
 from odoo.addons.quality_control_oca.tests.test_quality_control import (
     TestQualityControlOcaBase,
