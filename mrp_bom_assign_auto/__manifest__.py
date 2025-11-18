184b535e97c9e4004dfb545fad583fcf7dedf148
eduezerouali-tecnativa <eduardo.ezerouali@tecnativa.com>
2025-11-18 14:49:23 +0100

    [ADD] mrp_bom_assing_auto: New module

---
+++ mrp_bom_assign_auto/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2025 Tecnativa - Eduardo Ezerouali
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    "name": "MRP BOM Assign Auto",
+    "version": "15.0.1.0.0",
+    "category": "Manufacturing",
+    "summary": "Auto select th first BoM that has all components available",
+    "website": "https://github.com/OCA/manufacture",
+    "author": "Tecnativa," "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["mrp"],
+    "data": [
+        "views/res_config_settings_views.xml",
+    ],
+}
