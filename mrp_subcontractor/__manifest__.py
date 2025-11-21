70a3bf59c3de292774a00fee837b823c6590832d
Francois Poizat <francois.poizat@gmail.com>
2025-11-21 12:02:28 +0100

    [MIG] mrp_subcontractor: Migration to 18.0

--- mrp_subcontractor/__manifest__.py
+++ mrp_subcontractor/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "MRP subcontractor",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "category": "Manufacturing",
     "author": "Akretion,Odoo Community Association (OCA)",
