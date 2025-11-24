fdda041703c4e2295d7a22d53bc0ddfec4869170
Kev-Roche <kevin.roche@akretion.com>
2025-11-24 14:40:01 +0100

    [16.0][FIX] fix compute_kit_quantities

--- mrp_bom_version/tests/test_mrp_bom_version.py
+++ mrp_bom_version/tests/test_mrp_bom_version.py
@@ -85,3 +85,78 @@ class TestMrpBomVersion(common.TransactionCase):
             self.assertEqual(
                 new_bom.state, "draft", "New version must be created in 'draft' state"
             )
+
+    def test_historical_bom_still_used_in_picking(self):
+        self.mrp_bom.button_activate()
+        kit_product = self.mrp_bom.product_tmpl_id.product_variant_id
+
+        picking = self.env["stock.picking"].create(
+            {
+                "partner_id": self.env.ref("base.res_partner_1").id,
+                "picking_type_id": self.env.ref("stock.picking_type_out").id,
+                "location_id": self.env.ref("stock.stock_location_stock").id,
+                "location_dest_id": self.env.ref("stock.stock_location_customers").id,
+            }
+        )
+        bom_line_5 = self.mrp_bom.bom_line_ids.filtered(
+            lambda l: l.product_id == self.env.ref("product.product_product_5")
+        )
+        bom_line_6 = self.mrp_bom.bom_line_ids.filtered(
+            lambda l: l.product_id == self.env.ref("product.product_product_6")
+        )
+
+        self.env["stock.move"].create(
+            {
+                "name": "Kit Move – compo 5",
+                "product_id": self.env.ref("product.product_product_5").id,
+                "product_uom_qty": 1,
+                "product_uom": kit_product.uom_id.id,
+                "picking_id": picking.id,
+                "location_id": picking.location_id.id,
+                "location_dest_id": picking.location_dest_id.id,
+                "bom_line_id": bom_line_5.id,
+            }
+        )
+        self.env["stock.move"].create(
+            {
+                "name": "Kit Move – compo 6",
+                "product_id": self.env.ref("product.product_product_6").id,
+                "product_uom_qty": 1,
+                "product_uom": kit_product.uom_id.id,
+                "picking_id": picking.id,
+                "location_id": picking.location_id.id,
+                "location_dest_id": picking.location_dest_id.id,
+                "bom_line_id": bom_line_6.id,
+            }
+        )
+
+        picking.action_assign()
+        picking.button_validate()
+
+        filters = {
+            "incoming_moves": lambda m: m.id in picking.move_ids.ids,
+            "outgoing_moves": lambda m: m.id not in picking.move_ids.ids,
+        }
+
+        qty = picking.move_ids._compute_kit_quantities(
+            product_id=kit_product,
+            kit_qty=1,
+            kit_bom=self.mrp_bom,
+            filters=filters,
+        )
+        self.assertEqual(qty, 1.0)
+
+        self.mrp_bom.button_new_version()
+        new_bom = self.bom_model.with_context(active_test=False).search(
+            [
+                ("previous_bom_id", "=", self.mrp_bom.id),
+            ],
+            limit=1,
+        )
+        qty2 = picking.move_ids._compute_kit_quantities(
+            product_id=kit_product,
+            kit_qty=1,
+            kit_bom=new_bom,
+            filters=filters,
+        )
+        self.assertEqual(qty2, 1.0)
