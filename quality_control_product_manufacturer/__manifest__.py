de1a1ec683460c736c7a521c291ee106a99dcb68
Quirino Leone <quirinoleone95@gmail.com>
2025-12-12 11:52:26 +0100

    [MIG] quality_control_product_manufacturer: Migration to 18.0

--- quality_control_product_manufacturer/__manifest__.py
+++ quality_control_product_manufacturer/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Quality Control Product Manufacturer",
-    "version": "16.0.1.1.0",
+    "version": "18.0.1.0.0",
     "category": "Quality Control",
     "license": "AGPL-3",
     "summary": "Provides information related to Manufacturer under Inspection",
