9c7289ae8e8d87a6620b8c16985092fd32076172
Daniel Reis <dreis@opensourceintegrators.com>
2025-12-19 08:12:41 +0000

    [MIG] mrp_bom_version: Migration to 17.0

--- mrp_bom_version/__manifest__.py
+++ mrp_bom_version/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "MRP - BoM version",
     "summary": "BoM versioning",
-    "version": "18.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "OdooMRP team,"
     "AvanzOSC,"
