469639eefb3d4a9480ea6c7104a85866f462bce7
Max He <max@example.com>
2026-01-17 11:41:23 +0800

    清理代码，移除不再使用的hooks和相关字段，简化BOM行数据处理逻辑，更新视图以反映最新的字段依赖关系

--- mrp_bom_attribute_match/__init__.py
+++ mrp_bom_attribute_match/__init__.py
@@ -1,6 +1,2 @@
 from . import models
 from . import reports
-from . import hooks
-
-# Export the post_init_hook function to the module level
-from .hooks import _post_init_hook
--- mrp_bom_attribute_match/__manifest__.py
+++ mrp_bom_attribute_match/__manifest__.py
@@ -4,14 +4,11 @@
     "category": "Manufacturing",
     "author": "Ilyas, Ooops, Odoo Community Association (OCA)",
     "summary": "Dynamic BOM component based on product attribute",
-    "depends": ["mrp", "mrp_plm"],
+    "depends": ["mrp"],
     "license": "AGPL-3",
     "website": "https://github.com/OCA/manufacture",
     "data": [
         "views/mrp_bom_views.xml",
-        "views/mrp_plm_views.xml",
     ],
     "installable": True,
-    "auto_install": True,
-    "post_init_hook": "_post_init_hook",
 }
