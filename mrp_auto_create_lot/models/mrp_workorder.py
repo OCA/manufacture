41b56d50eb87645600aa961ed295b5f97861b1a2
Marina Alapont <marina.alapont@forgeflow.com>
2026-02-04 09:02:22 +0100

    [IMP] mrp_auto_create_lot: pre-commit stuff

--- mrp_auto_create_lot/models/mrp_workorder.py
+++ mrp_auto_create_lot/models/mrp_workorder.py
@@ -5,12 +5,14 @@ from odoo import api, models
 
 
 class MrpWorkorder(models.Model):
-    _inherit = 'mrp.workorder'
+    _inherit = "mrp.workorder"
 
     @api.multi
     def record_production(self):
         if not self.final_lot_id and self.product_id.auto_create_lot:
-            self.final_lot_id = self.env['stock.production.lot'].create({
-                'product_id': self.product_id.id,
-            })
+            self.final_lot_id = self.env["stock.production.lot"].create(
+                {
+                    "product_id": self.product_id.id,
+                }
+            )
         return super().record_production()
