a179a4d53fc5f0b116cd68d200fca160744af269
Daniel Reis <dreis@opensourceintegrators.com>
2026-02-20 18:42:20 +0000

    [ADD] mrp_bom_lock_used: prevent used BOMs to be changed

---
+++ mrp_bom_lock_used/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2026 Open Source Integrators
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "MRP BOM Lock Used",
+    "version": "17.0.1.0.0",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "summary": "Prevents BOM modifications when active production orders"
+    " exist and blocks MO confirmation when BOM is outdated",
+    "website": "https://github.com/OCA/manufacture",
+    "category": "Manufacturing",
+    "depends": ["mrp"],
+    "license": "LGPL-3",
+    "installable": True,
+}
