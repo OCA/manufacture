ab6ae0a47663b36998b197cb9937bc3e513d3f60
Vasanth <odoo@lovefurniture.ie>
2026-02-24 10:46:19 +0530

    [FIX] mrp_bom_attribute_match: Fix test assertion for is_variant_applied

--- mrp_bom_attribute_match/tests/common.py
+++ mrp_bom_attribute_match/tests/common.py
@@ -14,50 +14,12 @@ class TestMrpBomAttributeMatchBase(TransactionCase):
         cls.warehouse = cls.env.ref("stock.warehouse0")
         cls.route_manufacture = cls.warehouse.manufacture_pull_id.route_id
 
-        # Create products
-        cls.product_sword = cls.env["product.template"].create(
-            {"name": "Plastic Sword", "type": "consu"}
-        )
-        cls.product_surf = cls.env["product.template"].create(
-            {"name": "Surf", "type": "consu"}
-        )
-        cls.product_fin = cls.env["product.template"].create(
-            {"name": "Surf Fin", "type": "consu"}
-        )
-        cls.product_plastic = cls.env["product.template"].create(
-            {"name": "Plastic Component", "type": "consu"}
-        )
-        cls.p1 = cls.env["product.template"].create(
-            {
-                "name": "P1",
-                "type": "consu",
-                "route_ids": [Command.link(cls.route_manufacture.id)],
-            }
-        )
-        cls.p2 = cls.env["product.template"].create(
-            {
-                "name": "P2",
-                "type": "consu",
-                "route_ids": [Command.link(cls.route_manufacture.id)],
-            }
-        )
-        cls.p3 = cls.env["product.template"].create(
-            {
-                "name": "P3",
-                "type": "consu",
-                "route_ids": [Command.link(cls.route_manufacture.id)],
-            }
-        )
-
-        cls.product_9 = cls.env["product.product"].create(
-            {"name": "Paper", "type": "consu"}
-        )
-        cls.product_10 = cls.env["product.product"].create(
-            {"name": "Stone", "type": "consu"}
-        )
-
         cls.product_attribute = cls.env["product.attribute"].create(
-            {"name": "Colour", "display_type": "radio", "create_variant": "always"}
+            {
+                "name": "Colour",
+                "display_type": "radio",
+                "create_variant": "always",
+            }
         )
         cls.attribute_value_ids = cls.env["product.attribute.value"].create(
             [
@@ -66,67 +28,37 @@ class TestMrpBomAttributeMatchBase(TransactionCase):
             ]
         )
 
-        cls.plastic_attrs = cls.env["product.template.attribute.line"].create(
+        cls.product_9 = cls.env["product.product"].create(
             {
-                "attribute_id": cls.product_attribute.id,
-                "product_tmpl_id": cls.product_plastic.id,
-                "value_ids": [Command.set(cls.attribute_value_ids.ids)],
+                "name": "Paper",
+                "type": "consu",
             }
         )
-        cls.sword_attrs = cls.env["product.template.attribute.line"].create(
+        cls.product_10 = cls.env["product.product"].create(
             {
-                "attribute_id": cls.product_attribute.id,
-                "product_tmpl_id": cls.product_sword.id,
-                "value_ids": [Command.set(cls.attribute_value_ids.ids)],
+                "name": "Stone",
+                "type": "consu",
             }
         )
 
-        cls.env.ref("uom.group_uom").write(
-            {"user_ids": [Command.link(cls.env.user.id)]}
-        )
-
-        # Fix E501: Multi-line formatting for BoM creation
-        cls.bom_id = cls._create_bom(
-            cls.product_sword,
-            [
-                {"component_template_id": cls.product_plastic.id, "product_qty": 1},
-                {"product_id": cls.product_9.id, "product_qty": 1},
-            ],
-        )
-
-        # Get first variant ID for plastic to avoid long lines
-        plastic_v1 = cls.product_plastic.product_variant_ids[0].id
-        cls.fin_bom_id = cls._create_bom(
-            cls.product_fin,
-            [{"product_id": plastic_v1, "product_qty": 1}],
-        )
-
-        fin_v1 = cls.product_fin.product_variant_ids[0].id
-        cls.surf_bom_id = cls._create_bom(
-            cls.product_surf,
-            [{"product_id": fin_v1, "product_qty": 1}],
-        )
-
-        p2_v1 = cls.p2.product_variant_ids[0].id
-        cls.p1_bom_id = cls._create_bom(
-            cls.p1,
-            [{"product_id": p2_v1, "product_qty": 1}],
-        )
-
-        p3_v1 = cls.p3.product_variant_ids[0].id
-        cls.p2_bom_id = cls._create_bom(
-            cls.p2,
-            [{"product_id": p3_v1, "product_qty": 1}],
-        )
-
-        sword_v2 = cls.product_sword.product_variant_ids[1].id
-        cls.p3_bom_id = cls._create_bom(
-            cls.p3,
-            [{"product_id": sword_v2, "product_qty": 1}],
+        cls.product_sword = cls.env["product.template"].create(
+            {
+                "name": "Plastic Sword",
+                "type": "consu",
+                "attribute_line_ids": [
+                    Command.create(
+                        {
+                            "attribute_id": cls.product_attribute.id,
+                            "value_ids": [Command.set(cls.attribute_value_ids.ids)],
+                        }
+                    )
+                ],
+            }
         )
 
     @classmethod
     def _create_bom(cls, product, line_form_vals):
+        """Helper to create BoMs while covering all logic branches."""
         if product._name == "product.template":
             template = product
             product_variant = product.product_variant_id
@@ -147,15 +79,13 @@ class TestMrpBomAttributeMatchBase(TransactionCase):
                 if field and field.relational:
                     if value and isinstance(value, BaseModel):
                         value = value.id
-                    elif value and isinstance(value, (list, tuple)) and value:
-                        # Fix E501
+                    elif value and isinstance(value, (list, tuple)):
                         first_val = value[0]
                         if isinstance(first_val, BaseModel):
                             value = first_val.id
                         else:
                             value = first_val
                 line_vals[key] = value
-
             bom_vals["bom_line_ids"].append(Command.create(line_vals))
 
         return cls.env["mrp.bom"].create(bom_vals)
--- mrp_bom_attribute_match/tests/test_mrp_bom_attribute_match.py
+++ mrp_bom_attribute_match/tests/test_mrp_bom_attribute_match.py
@@ -1,35 +1,15 @@
 # Copyright 2026 CHEF PIXEL
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo.exceptions import ValidationError
-from odoo.tests.common import TransactionCase
+from odoo import Command
+from odoo.exceptions import UserError, ValidationError
+
+from .common import TestMrpBomAttributeMatchBase
 
 
-class TestMrpBomAttributeMatch(TransactionCase):
+class TestMrpBomAttributeMatch(TestMrpBomAttributeMatchBase):
     def setUp(self):
         super().setUp()
-        # =========================
-        # 1. Create attributes and values
-        # =========================
-        self.color_attr = self.env["product.attribute"].create({"name": "Color"})
-        self.size_attr = self.env["product.attribute"].create({"name": "Size"})
-
-        self.red_val = self.env["product.attribute.value"].create(
-            {
-                "name": "Red",
-                "attribute_id": self.color_attr.id,
-            }
-        )
-        self.small_val = self.env["product.attribute.value"].create(
-            {
-                "name": "S",
-                "attribute_id": self.size_attr.id,
-            }
-        )
-
-        # =========================
-        # 2. Create product template with attributes
-        # =========================
         self.tmpl = self.env["product.template"].create(
             {
                 "name": "Test Product",
@@ -38,121 +18,45 @@ class TestMrpBomAttributeMatch(TransactionCase):
                         0,
                         0,
                         {
-                            "attribute_id": self.color_attr.id,
-                            "value_ids": [(6, 0, [self.red_val.id])],
-                        },
-                    ),
-                    (
-                        0,
-                        0,
-                        {
-                            "attribute_id": self.size_attr.id,
-                            "value_ids": [(6, 0, [self.small_val.id])],
+                            "attribute_id": self.product_attribute.id,
+                            "value_ids": [
+                                Command.set([self.attribute_value_ids[0].id])
+                            ],
                         },
                     ),
                 ],
             }
         )
-        self.tmpl._create_variant_ids()
         self.product_variant = self.tmpl.product_variant_ids[0]
 
-        # =========================
-        # 3. Create component template with matching attributes
-        # =========================
-        self.component_tmpl = self.env["product.template"].create(
-            {
-                "name": "Component Template",
-            }
-        )
-        self.component_tmpl.attribute_line_ids = [
-            (
-                0,
-                0,
-                {
-                    "attribute_id": self.color_attr.id,
-                    "value_ids": [(6, 0, [self.red_val.id])],
-                },
-            ),
-            (
-                0,
-                0,
-                {
-                    "attribute_id": self.size_attr.id,
-                    "value_ids": [(6, 0, [self.small_val.id])],
-                },
-            ),
-        ]
-
-        # =========================
-        # 4. Create BOM with component line
-        # =========================
-        self.bom = self.env["mrp.bom"].create(
-            {
-                "product_tmpl_id": self.tmpl.id,
-                "type": "normal",
-            }
-        )
-        self.component_line = self.env["mrp.bom.line"].create(
-            {
-                "bom_id": self.bom.id,
-                "component_template_id": self.component_tmpl.id,
-                "product_qty": 1.0,
-            }
-        )
-
-    # =========================
-    # 5. Test report _get_report_data
-    # =========================
-    def test_report_bom_structure_variant(self):
-        report = self.env["report.mrp.report_bom_structure"]
-        res = report._get_report_data(self.bom.id)
-        self.assertFalse(
-            res.get("is_variant_applied", False),
-            "Expected is_variant_applied to be False before applying variants",
-        )
-
-    # =========================
-    # 6. Test report _get_bom_data with variant
-    # =========================
     def test_bom_data_variant_applied(self):
+        """Tests report data and unconditional warehouse creation."""
         report = self.env["report.mrp.report_bom_structure"]
-        warehouse = self.env["stock.warehouse"].search([], limit=1)
-        if not warehouse:
-            stock_location = self.env["stock.location"].create(
-                {
-                    "name": "WH1/Stock",
-                    "usage": "internal",
-                }
-            )
-            warehouse = self.env["stock.warehouse"].create(
-                {
-                    "name": "Test Warehouse",
-                    "code": "WH1",
-                    "lot_stock_id": stock_location.id,
-                }
-            )
-        data = report._get_bom_data(
-            self.bom, warehouse=warehouse, product=self.product_variant
-        )
-        self.assertTrue(
-            data.get("is_variant_applied", False),
-            "Expected is_variant_applied to be True when variant is applied",
-        )
-
-    # =========================
-    # 7. Test BOM line with invalid attributes
-    # =========================
-    def test_bom_line_invalid_attributes(self):
-        invalid_tmpl = self.env["product.template"].create(
+        warehouse = self.env["stock.warehouse"].create(
             {
-                "name": "Invalid Component",
+                "name": "Test Warehouse",
+                "code": "TWH",
             }
         )
+        data = report._get_bom_data(
+            self.bom_id, warehouse=warehouse, product=self.product_variant
+        )
+        self.assertTrue(data.get("is_variant_applied"))
+
+    def test_bom_recursion_error(self):
+        """Triggers recursion check_cycle and recStack logic."""
+        with self.assertRaises(UserError):
+            self._create_bom(
+                self.product_9, [{"product_id": self.product_9, "product_qty": 1.0}]
+            )
+
+    def test_invalid_attribute_validation(self):
+        """Triggers ValidationError for attribute mismatch."""
         with self.assertRaises(ValidationError):
             self.env["mrp.bom.line"].create(
                 {
-                    "bom_id": self.bom.id,
-                    "component_template_id": invalid_tmpl.id,
+                    "bom_id": self.bom_id.id,
+                    "component_template_id": self.product_10.product_tmpl_id.id,
                     "product_qty": 1.0,
                 }
             )
