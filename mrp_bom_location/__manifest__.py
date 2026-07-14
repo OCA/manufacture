db67381c3a50433e334e9f4da4a6d7c9f6c71d85
ManfredNelvin <manfrednelvin@gmail.com>
2026-07-14 12:42:18 +0530

    [IMP] mrp_bom_location: pre-commit auto fixes

--- mrp_bom_location/__manifest__.py
+++ mrp_bom_location/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "MRP BOM Location",
     "summary": "Adds location field to Bill of Materials and its components.",
-    "version": "18.0.1.1.2",
+    "version": "19.0.1.0.0",
     "category": "Manufacture",
     "website": "https://github.com/OCA/manufacture",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
